2cc366c498873884e9a00392bdca04dc71423d28
Eduardo <eduardolucasmaia@hotmail.com>
2016-07-19 15:33:35 -0300

    Adição controller web service

---
+++ SgqSystem.Tests/Controllers/WebServiceControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SgqSystem.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SgqSystem.Controllers.Tests
+{
+    [TestClass()]
+    public class WebServiceControllerTests
+    {
+        [TestMethod()]
+        public void GetTelaTesteTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
