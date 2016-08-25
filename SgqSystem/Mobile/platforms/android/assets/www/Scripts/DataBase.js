777d7de6cbea129f3872a7d658206b046d9fc55b
Jelsafa <Jelsafa@SERVERGRT>
2016-08-25 17:20:04 -0300

    Insercao da pasta mobile como a integracao mais atualizada

---
+++ SgqSystem/Mobile/platforms/android/assets/www/Scripts/DataBase.js
@@ -0,0 +1,132 @@
+﻿// Wait for Cordova to load
+document.addEventListener('deviceready', onDeviceReady, false);
+
+// Cordova is ready
+function onDeviceReady() {
+
+    //PeriodHTMLDAO.dropTable();
+    //SystemDAO.getDB();
+    //PeriodHTMLDAO.createTable();
+    //PeriodHTMLDAO.insertHTML();
+    //PeriodHTMLDAO.updateHTML(1);
+    //PeriodHTMLDAO.selectTable(list);
+    //PeriodHTMLDAO.appendHTML();
+    $('#platform').text( device.platform );
+    createFile();
+}
+
+var SystemDAO = {
+    getDB: function () {
+        return openDatabase('SGQUSA', '1.0', 'WebSQL Database', 50 * 1024 * 1024);
+    }
+}
+
+var PeriodHTMLDAO = {
+    createTable: function () {
+        SystemDAO.getDB().transaction(function (tx) {
+            tx.executeSql("CREATE TABLE IF NOT EXISTS PeriodHTML (Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Result TEXT, InsertDate TEXT, SyncDate TEXT, Shift INTEGER, Period INTEGER, Sync BOOL)");
+        });
+    },
+
+    dropTable: function () {
+        SystemDAO.getDB().transaction(function (tx) {
+            tx.executeSql('DROP TABLE IF EXISTS PeriodHTML');
+        });
+    },
+
+    insertHTML: function (result) {
+        SystemDAO.getDB().transaction(function (tx) {
+            var date = new Date();
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
+            var day = date.getDate();
+            var hour = date.getHours();
+            var minute = date.getMinutes();
+            var second = date.getSeconds();
+            var yyyymmddhhmm = year + ("0" + month).slice(-2) + "" + ("0" + day).slice(-2) + "" + ("0" + hour).slice(-2) + "" + ("0" + minute).slice(-2) + "" + ("0" + second).slice(-2);
+            var dt = year + "/" + ("0" + month).slice(-2) + "/" + ("0" + day).slice(-2) + " " + ("0" + hour).slice(-2) + ":" + ("0" + minute).slice(-2) + ":" + ("0" + second).slice(-2);
+
+            var shift = $('.App').attr('shift');
+            //var period = $('.App').attr('period');
+
+            tx.executeSql("INSERT INTO PeriodHTML (Result, InsertDate, Shift, Period, Sync) VALUES ('" + result + "', '" + dt + "', '" + $('.App').attr('shift') +"', '" + $('.App').attr('period') + "', 0) ", null, null, null);
+        });
+    },
+
+    updateHTML: function (id) {
+        SystemDAO.getDB().transaction(function (tx) {
+            var date = new Date();
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
+            var day = date.getDate();
+            var hour = date.getHours();
+            var minute = date.getMinutes();
+            var second = date.getSeconds();
+            var yyyymmddhhmm = year + ("0" + month).slice(-2) + "" + ("0" + day).slice(-2) + "" + ("0" + hour).slice(-2) + "" + ("0" + minute).slice(-2) + "" + ("0" + second).slice(-2);
+            var dt = year + "/" + ("0" + month).slice(-2) + "/" + ("0" + day).slice(-2) + " " + ("0" + hour).slice(-2) + ":" + ("0" + minute).slice(-2) + ":" + ("0" + second).slice(-2);
+
+            var shift = $('.App').attr('shift');
+            var period = $('.App').attr('period');
+            var contentHtml = $('.App').html();
+
+            tx.executeSql('UPDATE PeriodHTML SET Result = ?, InsertDate = ?, Shift = ?, Period = ? WHERE Id = ? ', [contentHtml, dt, 1, 1, 1], onSuccess, onError);
+
+        });
+    },
+
+    selectTable: function (callback) {
+        return SystemDAO.getDB().transaction(function (tx) {
+            tx.executeSql('select * from PeriodHTML', [],
+            function (tx, results) {
+                return callback(results.rows, results.rows.length - 1);
+            });
+        });
+
+    },
+
+    appendHTML: function () {
+        return SystemDAO.getDB().transaction(function (tx) {
+            tx.executeSql('select * from PeriodHTML', [],
+            function (tx, results) {
+                if (results.rows[results.rows.length - 1] != undefined) {
+                    $('.App').empty();
+                    appendDevice(results.rows[results.rows.length - 1].ValueHtml, $('.App'));
+                    //alert(results.rows.length);
+                }
+            });
+        });
+    },
+
+    showHTML: function (id) {
+        return SystemDAO.getDB().transaction(function (tx) {
+            tx.executeSql('select * from PeriodHTML', [],
+            function (tx, results) {
+                if (results.rows[id].ValueHtml != undefined) {
+                    console.log(results.rows[id].ValueHtml);
+                }
+            });
+        });
+    }
+};
+
+function list(value, last) {
+    //$('.App').empty();
+    if (value[last].ValueHtml != undefined) {
+        $('.App').html(value[last].ValueHtml);
+    }
+    // $('.App').append(value[last].ValueHtml);
+    showLevel01();
+    console.log(value); 
+}
+
+function onSuccess(transaction, resultSet) {
+    console.log('Query completed: ' + JSON.stringify(resultSet));
+    $('#countlines').text( JSON.stringify(resultSet));
+}
+
+function onError(transaction, error) {
+    console.log('Query failed: ' + error.message);
+    $('#countlines').text( error.message);
+}
+
+//onDeviceReady();
---
+++ SgqSystem/Mobile/platforms/windows/www/Scripts/correctiveaction.js
@@ -0,0 +1,278 @@
+﻿var Utils = {
+    GerarData: function () {
+        var date = new Date();
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+        var hours = date.getHours();
+        //var ddMMyyyyhhmmss = ("0" + day).slice(-2) + "/" + ("0" + month).slice(-2) + "/" + year + " " + ("0" + hours).slice(-2) + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2);
+        // return ddMMyyyyhhmmss;
+        var MMddyyyyhhmmss = ("0" + month).slice(-2) + "/" + ("0" + day).slice(-2) + "/" + year + " " + ("0" + hours).slice(-2) + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2);
+        return MMddyyyyhhmmss;
+    },
+};
+
+var storage = window.localStorage;
+
+var idAcaoCorretiva = 0;
+
+var idSlaughterLogado = 0;
+var idTechinicalLogado = 0;
+var timeSlaughterLogado = null;
+var timeTechinicalLogado = null;
+var dateExecute = Utils.GerarData();
+var dateStart = Utils.GerarData();
+
+$("#period").text(storage.getItem("periodo"));
+$("#auditor").text(storage.getItem("userId"));
+$("#datetime").text(dateExecute);
+$("#starttime").text(dateStart);
+
+$("#auditText").text(storage.getItem("indicatorName"));
+
+var AcaoCorretiva = {
+
+    abrirModalLoginSlaughter: function () {
+        $('#correctiveActionModal').css('z-index', '9997');
+        $('#modalLoginSlaughter').removeClass('hide');
+        var ca = $('#modalLoginSlaughter');
+        ca.fadeIn("fast");
+    },
+
+    abrirModalLoginTecnical: function () {
+        $('#modalLoginTechinical').removeClass('hide');
+        var ca = $('#modalLoginTechinical');
+        ca.fadeIn("fast");
+    },
+
+    logarUsuarioSlaughter: function () {
+
+        Geral.esconderMensagem("#modalLoginSlaughter");
+
+        var obj = {
+            SlaughterPassword: $("#slaughterPassword").val(),
+            SlaughterLogin: $("#slaughterLogin").val()
+        };
+
+        $.ajax({
+            data: obj,
+            url: '../' + '../api/CorrectiveAction/LogarUsuarioSlaughter',
+            type: 'POST',
+            success: function (data) {
+                if (data.Mensagem != null) {
+                    Geral.exibirMensagemAlerta(data.Mensagem, "", "#modalLoginSlaughter");
+                }
+                else {
+                    idSlaughterLogado = data.Retorno.Id
+                    $("#slaughter").val(data.Retorno.Name);
+                    $("#slaughterDatetime").val(Utils.GerarData());
+                    timeSlaughterLogado = Utils.GerarData();
+                    var $temp = $('#modalLoginSlaughter');
+                    $temp.modal('hide');
+
+                }
+            }
+        });
+
+    },
+
+    logarUsuarioTechnical: function () {
+
+        Geral.esconderMensagem("#modalLoginTechinical");
+
+        var obj = {
+            TechnicalPassword: $("#techinicalPassword").val(),
+            TechnicalLogin: $("#techinicalLogin").val()
+        };
+
+        $.ajax({
+            data: obj,
+            url: '../' + '../api/CorrectiveAction/LogarUsuarioTechnical',
+            type: 'POST',
+            success: function (data) {
+                if (data.Mensagem != null) {
+                    Geral.exibirMensagemAlerta(data.Mensagem, "", "#modalLoginTechinical");
+                }
+                else {
+                    idTechinicalLogado = data.Retorno.Id;
+                    $("#techinical").val(data.Retorno.Name);
+                    $("#techinicalDatetime").val(Utils.GerarData());
+                    timeTechinicalLogado = Utils.GerarData();
+                    var $temp = $('#modalLoginTechinical');
+                    $temp.modal('hide');
+                }
+            }
+        });
+
+    },
+
+    enviarAcaoCorretiva: function () {
+
+        Geral.esconderMensagem("#correctiveActionModal");
+        
+        var listDefects = new Array();
+
+        var level01Id = parseInt($('.level01.selected').attr('id'));
+
+        if (!$('.level01.selected').length) {
+
+            level01Id = parseInt($('.btnCorrectiveAction.selected').parents('.row').children('.level01').attr('id'));
+        }
+
+        $('.level02List .level02Group[level01id=' + level01Id + '] .level02[limitexceeded]').each(function (e) {
+
+            var level02 = $(this);
+            var level02Id = parseInt(level02.attr('id'));
+            var level02Name = level02.children('span.levelName').text();
+
+            $('.level03Group[level01id=' + level01Id + '] .level03').each(function (e) {
+
+                var level03 = $(this);
+                var level02errorlimit = parseInt(level02.attr('levelerrorlimit'));
+                var level03Defects = level02.attr('level03' + level03.attr('id'));
+                var level03Id = parseInt(level03.attr('id'));
+                var level03Name = level03.children('.row').children('div').html();
+
+
+                if (level03.children('.row').children('div').children('span.response').length) {
+                    if (level03Defects == 1) {
+                        level03Defects = 0
+                    }
+                    else {
+                        level03Defects = 1;
+                    }
+                }
+
+                if (level03Defects >= level02errorlimit && level03Defects > 0) {
+
+                    var tempDefects = {
+                        CorrectiveActionId: 0,
+                        AuditLevel01Id: level01Id,
+                        AuditLevel02Id: level02Id,
+                        AuditLevel03Id: level03Id,
+                        Defects: level03Defects
+                    };
+
+                    listDefects.push(tempDefects);
+
+                }
+            });
+        });
+
+
+
+
+        var obj = {
+            CorrectiveAction: {
+                Id: idAcaoCorretiva,
+                DateExecuteFarmatado: dateExecute,
+                AuditorId: 2,
+                ShiftId: 1,
+                AuditLevel01Id: level01Id,
+                StartTimeFarmatado: dateStart,
+                PeriodId: 1,
+                DescriptionFailure: $("#DescriptionFailure").val(),
+                ImmediateCorrectiveAction: $("#ImmediateCorrectiveAction").val(),
+                ProductDisposition: $("#ProductDisposition").val(),
+                PreventativeMeasure: $("#PreventativeMeasure").val(),
+                SlaughterId: idSlaughterLogado,
+                NameSlaughter: $("#slaughter").val(),
+                DateTimeSlaughterFarmatado: timeSlaughterLogado,
+                TechinicalId: idTechinicalLogado,
+                NameTechinical: $("#techinical").val(),
+                DateTimeTechinicalFarmatado: timeTechinicalLogado,
+                CorrectiveActionLevels: listDefects
+            }
+        };
+
+       // console.log(obj);
+
+        $.ajax({
+            data: obj,
+            url: '../' + '../api/CorrectiveAction/SalvarAcaoCorretiva',
+            type: 'POST',
+            success: function (data) {
+                if (data.Mensagem != null) {
+                    Geral.exibirMensagemAlerta(data.Mensagem, "", "#correctiveActionModal");
+                }
+                else {
+                    window.location.href = '/newColeta/Index.html';
+                }
+            }
+        });
+
+    },
+
+    verificarAcaoCorretivaIncompleta: function () {
+
+        var obj = {
+            CorrectiveAction: {
+                Auditor: storage.getItem("userId"),
+                Shift: 1,
+                AuditLevel1: storage.getItem("indicatorId"),
+                AuditLevel2: storage.getItem("indicatorId"),
+                AuditLevel3: storage.getItem("indicatorId"),
+                Period: storage.getItem("periodo")
+            }
+        };
+
+        $.ajax({
+            data: obj,
+            url: '../' + '../api/CorrectiveAction/VerificarAcaoCorretivaIncompleta',
+            type: 'POST',
+            success: function (data) {
+
+               // console.log(data);
+
+                if (data != null) {
+
+                    if (data.Id != 0) {
+
+                        idAcaoCorretiva = data.Id;
+
+                        dateExecute = data.DateExecuteFarmatado;
+                        $("#datetime").text(dateExecute);
+
+                        dateStart = data.StartTimeFarmatado;
+                        $("#starttime").text(dateStart);
+
+                        storage.setItem("userId", data.Auditor);
+                        $("#auditor").text(data.Auditor);
+
+                        $("#shift").text('1');
+
+                        storage.setItem("indicatorId", data.AuditLevel1);
+                        storage.setItem("indicatorId", data.AuditLevel2);
+                        storage.setItem("indicatorId", data.AuditLevel3);
+
+                        storage.setItem("periodo", data.Period);
+                        $("#period").text(data.Period);
+
+                        $("#DescriptionFailure").val(data.DescriptionFailure);
+                        $("#ImmediateCorrectiveAction").val(data.ImmediateCorrectiveAction);
+                        $("#ProductDisposition").val(data.ProductDisposition);
+                        $("#PreventativeMeasure").val(data.PreventativeMeasure);
+
+                        idSlaughterLogado = data.Slaughter;
+                        timeSlaughterLogado = data.DateTimeSlaughterFarmatado;
+                        $("#slaughterDatetime").val(timeSlaughterLogado);
+                        $("#slaughter").val(data.NameSlaughter);
+
+
+                        idTechinicalLogado = data.Techinical;
+                        timeTechinicalLogado = data.DateTimeTechinicalFarmatado;
+                        $("#techinicalDatetime").val(timeTechinicalLogado);
+                        $("#techinical").val(data.NameTechinical);
+
+                    }
+                }
+            }
+        });
+
+    },
+
+};
+
+//AcaoCorretiva.verificarAcaoCorretivaIncompleta();
