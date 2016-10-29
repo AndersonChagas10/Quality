7d46b8902f2b0e00b23553d889bb368ac1165e80
Tony <Tony@TONY-NB>
2016-10-29 08:41:13 -0200

    Atualizações Level3Level2

---
+++ DTO/DTO/Params/ParLevel3Level2DTO.cs
@@ -0,0 +1,17 @@
+﻿using DTO.BaseEntity;
+
+namespace DTO.DTO.Params
+{
+    public class ParLevel3Level2DTO : EntityBase
+    {
+        public int ParLevel2_Id { get; set; }
+        public int ParLevel3_Id { get; set; }
+        public int ParLevel3Group_Id { get; set; }
+        public decimal Weight { get; set; }
+        public bool Active { get; set; } = true;
+
+        public ParLevel2DTO ParLevel2 { get; set; }
+        public ParLevel3DTO ParLevel3 { get; set; }
+        public ParLevel3GroupDTO ParLevel3Group { get; set; }
+    }
+}
