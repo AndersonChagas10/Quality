a6f192ffc8080147ab84032a1ee9137a5a3a0fab
celsogea@hotmail.com <celsogea@hotmail.com>
2016-11-11 12:24:43 -0200

    Atualização Panel Tipo HTP

--- Data/Repositories/ParLevel3Repository.cs
+++ Data/Repositories/ParLevel3Repository.cs
@@ -27,7 +27,7 @@ namespace Data.Repositories
 
         public List<ParLevel3Level2> GetLevel3VinculadoLevel2(int idLevel1)
         {
-            var query = string.Format("select * from ParLevel3Level2 p32 INNER JOIN ParLevel3Level2Level1 p321 ON p321.ParLevel3Level2_Id = p32.Id WHERE p321.ParLevel1_Id = {0}", idLevel1);
+            var query = string.Format("select * from ParLevel3Level2 p32 INNER JOIN ParLevel3Level2Level1 p321 ON p321.ParLevel3Level2_Id = p32.Id LEFT JOIN ParLevel3Group pg ON pg.Id = p32.ParLevel3Group_Id     WHERE p321.ParLevel1_Id = {0}", idLevel1);
 
             return db.Database.SqlQuery<ParLevel3Level2>(query).ToList();
 
