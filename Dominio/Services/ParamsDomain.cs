5ad9f15463ce81843a88ba7083f9ab15ffd0c114
celsogea@hotmail.com <celsogea@hotmail.com>
2016-11-12 15:09:41 -0200

    Mais um commit do tatão, correção de par counter para local by tato.

--- Dominio/Services/ParamsDomain.cs
+++ Dominio/Services/ParamsDomain.cs
@@ -622,6 +622,8 @@ namespace Dominio.Services
             foreach (var ParLevel3Level2Level1Dto in retorno.listParLevel3Level2Level1Dto.Distinct())
             {
                 if (!retorno.listParLevel2Colleta.Any(r=> r.Id == ParLevel3Level2Level1Dto.ParLevel3Level2.ParLevel2.Id)) {
+                    ParLevel3Level2Level1Dto.ParLevel3Level2.ParLevel2.listParCounterXLocal = Mapper.Map<List<ParCounterXLocalDTO>>(_baseParCounterXLocal.GetAll().Where(r => r.ParLevel2_Id == ParLevel3Level2Level1Dto.ParLevel3Level2.ParLevel2.Id && r.IsActive == true).ToList());
+
                     retorno.listParLevel2Colleta.Add(ParLevel3Level2Level1Dto.ParLevel3Level2.ParLevel2);
                 }
                 var parLevel3Level2DoLevel2 = _repoParLevel3.GetLevel3VinculadoLevel2(retorno.Id);
