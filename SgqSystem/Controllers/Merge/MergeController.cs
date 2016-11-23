6f4dc0be91705b6cb34defe252e8ba35197aa086
celsogea@hotmail.com <celsogea@hotmail.com>
2016-11-23 20:50:06 -0200

    Implementação de merge div.

---
+++ SgqSystem/Controllers/Merge/MergeController.cs
@@ -0,0 +1,35 @@
+﻿using Dominio.Interfaces.Services;
+using SgqSystem.ViewModels;
+using System.Web.Mvc;
+
+namespace SgqSystem.Controllers
+{
+    public class MergeController : Controller
+    {
+
+        #region Construtor e atributos
+
+        public string UserSgqDTO { get; private set; }
+        private readonly IGetConsolidateDataCollectionDomain _getConsolidateDataCollectionDomain;
+        public ResultsDivViewModel ViewModel;
+
+        public MergeController(IGetConsolidateDataCollectionDomain getConsolidateDataCollectionDomain)
+        {
+            _getConsolidateDataCollectionDomain = getConsolidateDataCollectionDomain;
+
+            ViewModel = new ResultsDivViewModel()
+            {
+                consolidationLevel01 = _getConsolidateDataCollectionDomain.GetLastEntry()
+            };
+
+        }
+
+        #endregion
+
+        // GET: Merge
+        public ActionResult DivResults()
+        {
+            return PartialView("_DivResults", ViewModel);
+        }
+    }
+}
