3995ad56f5ce98f8029ee38384cdd9d6474cf45a
jelsafa <jelsafa.rb@hotmail.com>
2016-11-28 14:33:49 -0200

    CompanyStructure

---
+++ SgqSystem/Controllers/CompanyStructureController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SgqSystem.Controllers
+{
+    public class CompanyStructureController : Controller
+    {
+        // GET: CompanyStructure
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
