f4418bed2fe5559139c9f19b804b11b26619bca3
Gabriel Nunes <gabriel@grtsolucoes.com.br>
2016-12-10 17:12:29 -0200

    Novo menu de gestão

---
+++ SgqSystem/Controllers/GestaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SgqSystem.Controllers
+{
+    public class GestaoController : Controller
+    {
+        // GET: Gestao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
