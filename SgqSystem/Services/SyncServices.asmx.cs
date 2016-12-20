2a80bf9913368a08708bfcdaeba68047a1a7f54d
Gabriel Nunes <gabriel@grtsolucoes.com.br>
2016-12-20 08:52:39 -0200

    Espaço e pipe entre usuario e unidade

--- SgqSystem/Services/SyncServices.asmx.cs
+++ SgqSystem/Services/SyncServices.asmx.cs
@@ -2134,7 +2134,7 @@ namespace SgqSystem.Services
     {
         string foot = "<footer class=\"footer\">                                                                                                                                       " +
                       "   <p style=\"color:white; margin-left:16px; margin-right:16px; margin-top: 12px;\">                                                                      " +
-                      "       <span class=\"user\">Admin</span> - <span class=\"unit\">Colorado</span><span class=\"urlPrefix\"></span>                                          " +
+                      "       <span class=\"user\">Admin</span> - <span class=\"unit\">Colorado</span> | <span class=\"urlPrefix\"></span>                                          " +
                       "       <span class=\"status pull-right\"></span>                                                                                                          " +
                       "   </p>                                                                                                                                                   " +
                       "</footer>                                                                                                                                                 ";
