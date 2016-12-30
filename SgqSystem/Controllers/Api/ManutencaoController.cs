efc46d4c03609c9c13f4e28c923c7df59a4155f2
Gabriel Nunes <gabriel@grtsolucoes.com.br>
2016-12-30 19:11:31 -0200

    manutenção

--- SgqSystem/Controllers/Api/ManutencaoController.cs
+++ SgqSystem/Controllers/Api/ManutencaoController.cs
@@ -51,6 +51,52 @@ namespace SgqSystem.Controllers.Api
 
             return lista;
         }
+
+        [HttpPost]
+        [Route("getTabela2")]
+        public List<Pacote> getSelectTabela2()
+        {
+            
+            var lista = new List<Pacote>();
+            
+            using (var db = new SgqDbDevEntities())
+            {
+                var sql = "select distinct(ContaContabil) as Name from Manutencao WHERE MesAno BETWEEN '20150101' AND '20180101'AND TipoInformacao = 'CustoFixo' and Pacote in ('MANUTENÇÃO INDUSTRIAL/ PREDIAL') and EmpresaRegional in ('Reg 1 - GO/MG/BA') group by ContaContabil order by ContaContabil";
+
+                lista = db.Database.SqlQuery<Pacote>(sql).ToList();
+
+                foreach (var item in lista)
+                {
+                    sql = " select EmpresaSigla as Regional,";
+                    sql += " ROUND(SUM(DespesaOrcada), 0) AS Orçada,";
+                    sql += " ROUND(SUM(DespesaRealizada), 0) AS Realizada,";
+                    sql += " CASE WHEN SUM(DespesaOrcada) = 0 THEN 0 ELSE ROUND((SUM(DespesaRealizada) / SUM(DespesaOrcada) - 1) * 100, 0) END AS DesvioPorc,";
+                    sql += " ROUND(SUM(DespesaRealizada) - SUM(DespesaOrcada), 0) AS DesvioReal";
+                    sql += " from Manutencao";
+                    sql += " WHERE MesAno BETWEEN '20150101' AND '20180101'";
+                    sql += " AND TipoInformacao = 'CustoFixo'";
+                    sql += " and Pacote in ('MANUTENÇÃO INDUSTRIAL/ PREDIAL')";
+                    sql += " and EmpresaRegional in ('Reg 1 - GO/MG/BA')";
+                    sql += " and ContaContabil in ('" +item.Name+ "')";
+                    sql += " group by EmpresaSigla";
+                    sql += " order by EmpresaSigla asc;";
+
+                    item.ListaRegionais = db.Database.SqlQuery<Reg>(sql).ToList();
+
+                    sql = " select ContaContabil as Pacote, ROUND(SUM(DespesaOrcada), 0) AS Orçada,  ROUND(SUM(DespesaRealizada), 0) AS Realizada,";
+                    sql += "CASE WHEN SUM(DespesaOrcada) = 0 THEN 0 ELSE ROUND((SUM(DespesaRealizada) / SUM(DespesaOrcada) - 1) * 100, 0) END AS DesvioPorc,";
+                    sql += "ROUND(SUM(DespesaRealizada) - SUM(DespesaOrcada), 0) AS DesvioReal";
+                    sql += " from Manutencao ";
+                    sql += " WHERE MesAno BETWEEN '20150101' AND '20180101' AND TipoInformacao = 'CustoFixo'";
+                    sql += " and Pacote in ('MANUTENÇÃO INDUSTRIAL/ PREDIAL') and EmpresaRegional in ('Reg 1 - GO/MG/BA') and ContaContabil in ( '" + item.Name + "')  group by ContaContabil  order by ContaContabil asc; ";
+                    item.total = db.Database.SqlQuery<TotalPacote>(sql).FirstOrDefault();
+
+                }
+
+            }
+
+            return lista;
+        }
     }
 
     public class Pacote
--- SgqSystem/Controllers/Api/RelatorioDiarioApiController.cs
+++ SgqSystem/Controllers/Api/RelatorioDiarioApiController.cs
@@ -13,12 +13,17 @@ namespace SgqSystem.Controllers.Api
     {
         private List<RelDiarioGrafico1Result> _mock { get; set; }
         private List<RelDiarioGrafico1Result> _listGrafico1Level1 { get; set; }
+        private PanelResulPanel _mockPanelResultevel2Level3 { get; set; }
+
+        public RelatorioDiarioApiController() {
+            CriaMockGrafico1Level1();
+            CriaMockLevel1();
+        }
 
         [HttpPost]
         [Route("Grafico1")]
         public List<RelDiarioGrafico1Result> GetGrafico1Level1([FromBody] FormularioParaRelatorioViewModel form)
         {
-            CriaMockGrafico1Level1();
             //var query = "";
             //using (var db = new SgqDbDevEntities())
             //{
@@ -26,6 +31,21 @@ namespace SgqSystem.Controllers.Api
             //}
 
             return _mock;
+        }
+
+        [HttpPost]
+        [Route("GetPanelResultSet")]
+        public PanelResulPanel GetPanelResultSet([FromBody] FormularioParaRelatorioViewModel form)
+        {
+            //var query = "";
+            //using (var db = new SgqDbDevEntities())
+            //{
+            //    _listGrafico1Level1 = db.Database.SqlQuery<RelDiarioGrafico1Result>(query).ToList();
+            //}
+            //var mockRetorno = _mockPanelResultevel2Level3;
+            //mockRetorno.listResultSetLevel2 = _mockPanelResultevel2Level3.listResultSetLevel2.Where(r => r.level1_Id == form.level01Id).ToList();
+            //mockRetorno.listResultSetLevel3 = _mockPanelResultevel2Level3.listResultSetLevel3.Where(r => r.level2_Id == form.level02Id && r.level1_Id == form.level01Id).ToList();
+            return _mockPanelResultevel2Level3;
         }
 
         private void CriaMockGrafico1Level1()
@@ -37,7 +57,8 @@ namespace SgqSystem.Controllers.Api
                 NCProc = 90.8M,
                 Meta = 8.9M,
                 TotalNC = 13.74M,
-                TotalAv = 12.34M
+                TotalAv = 12.34M,
+                Id = 1
             });
             _mock.Add(new RelDiarioGrafico1Result()
             {
@@ -45,38 +66,178 @@ namespace SgqSystem.Controllers.Api
                 NCProc = 130.8M,
                 Meta = 5M,
                 TotalNC = 8M,
-                TotalAv = 12M
+                TotalAv = 12M,
+                Id = 2
             });
-            _mock.Add(new RelDiarioGrafico1Result()
+            //_mock.Add(new RelDiarioGrafico1Result()
+            //{
+            //    Level1Name = "Level1 teste 3",
+            //    NCProc = 20.8M,
+            //    Meta = 8.99M,
+            //    TotalNC = 13M,
+            //    TotalAv = 12M,
+            //    Id = 3
+            //});
+            //_mock.Add(new RelDiarioGrafico1Result()
+            //{
+            //    Level1Name = "Level1 teste 4",
+            //    NCProc = 2.8M,
+            //    Meta = 3.9M,
+            //    TotalNC = 103.74M,
+            //    TotalAv = 210.34M,
+            //    Id = 4
+            //});
+
+        }
+
+        private void CriaMockLevel1()
+        {
+            _mockPanelResultevel2Level3 = new PanelResulPanel();
+            _mockPanelResultevel2Level3.listResultSetLevel2 = new List<PanelResultevel2Level3>();
+            _mockPanelResultevel2Level3.listResultSetLevel3PorLevel1 = new List<PanelResultevel2Level3>();
+            _mockPanelResultevel2Level3.listResultSetLevel3 = new List<PanelResultevel2Level3>();
+
+            /*Level 2*/
+            _mockPanelResultevel2Level3.listResultSetLevel2.Add(new PanelResultevel2Level3()
             {
-                Level1Name = "Level1 teste 3",
-                NCProc = 20.8M,
-                Meta = 8.99M,
-                TotalNC = 13M,
-                TotalAv = 12M
+                Name = "Level2 teste 1",
+                NC = 13.74M,
+                Av = 12.34M,
+                Id = 12,
+                level1_Id = 1
+
             });
-            _mock.Add(new RelDiarioGrafico1Result()
+            _mockPanelResultevel2Level3.listResultSetLevel2.Add(new PanelResultevel2Level3()
             {
-                Level1Name = "Level1 teste 4",
-                NCProc = 2.8M,
-                Meta = 3.9M,
-                TotalNC = 103.74M,
-                TotalAv = 210.34M
+                Name = "Level2 teste 2",
+                NC = 60M,
+                Av = 80M,
+                Id = 23,
+                level1_Id = 1
             });
-           
+            _mockPanelResultevel2Level3.listResultSetLevel2.Add(new PanelResultevel2Level3()
+            {
+                Name = "Level2 teste 3",
+                NC = 40M,
+                Av = 49M,
+                Id = 34,
+                level1_Id = 2
+            });
+            _mockPanelResultevel2Level3.listResultSetLevel2.Add(new PanelResultevel2Level3()
+            {
+                Name = "Level2 teste 4",
+                NC = 103.74M,
+                Av = 210.34M,
+                Id = 45,
+                level1_Id = 2
+
+            });
+
+            /*Level 3*/
+            _mockPanelResultevel2Level3.listResultSetLevel3.Add(new PanelResultevel2Level3()
+            {
+                Name = "Level3 teste 1",
+                NC = 13.74M,
+                Av = 12.34M,
+                Id = 112,
+                level1_Id = 1,
+                level2_Id = 12
+            });
+            _mockPanelResultevel2Level3.listResultSetLevel3.Add(new PanelResultevel2Level3()
+            {
+                Name = "Level3 teste 2",
+                NC = 60M,
+                Av = 80M,
+                Id = 123,
+                level1_Id = 2,
+                level2_Id = 12
+            });
+
+            _mockPanelResultevel2Level3.listResultSetLevel3.Add(new PanelResultevel2Level3()
+            {
+                Name = "Level3 teste 3",
+                NC = 40M,
+                Av = 49M,
+                Id = 134,
+                level1_Id = 3,
+                level2_Id = 23
+            });
+            _mockPanelResultevel2Level3.listResultSetLevel3.Add(new PanelResultevel2Level3()
+            {
+                Name = "Level3 teste 4",
+                NC = 103.74M,
+                Av = 210.34M,
+                Id = 145,
+                level1_Id = 4,
+                level2_Id = 23
+            });
+
+            _mockPanelResultevel2Level3.listResultSetLevel3.Add(new PanelResultevel2Level3()
+            {
+                Name = "Level3 teste 3",
+                NC = 40M,
+                Av = 49M,
+                Id = 134,
+                level1_Id = 3,
+                level2_Id = 34
+            });
+            _mockPanelResultevel2Level3.listResultSetLevel3.Add(new PanelResultevel2Level3()
+            {
+                Name = "Level3 teste 4",
+                NC = 103.74M,
+                Av = 210.34M,
+                Id = 145,
+                level1_Id = 4,
+                level2_Id = 34
+            });
+
+            _mockPanelResultevel2Level3.listResultSetLevel3.Add(new PanelResultevel2Level3()
+            {
+                Name = "Level3 teste 3",
+                NC = 40M,
+                Av = 49M,
+                Id = 134,
+                level1_Id = 3,
+                level2_Id = 45
+            });
+            _mockPanelResultevel2Level3.listResultSetLevel3.Add(new PanelResultevel2Level3()
+            {
+                Name = "Level3 teste 4",
+                NC = 103.74M,
+                Av = 210.34M,
+                Id = 145,
+                level1_Id = 4,
+                level2_Id = 45
+            });
         }
     }
 
     public class RelDiarioGrafico1Result
     {
-        public RelDiarioGrafico1Result()
-        {
-        }
-
+        public int Id { get; internal set; }
         public string Level1Name { get; set; }
         public decimal Meta { get; set; }
         public decimal NCProc { get; set; }
         public decimal TotalAv { get; set; }
         public decimal TotalNC { get; set; }
     }
+
+    public class PanelResulPanel
+    {
+        public List<PanelResultevel2Level3> listResultSetLevel2 { get; set; }
+        public List<PanelResultevel2Level3> listResultSetLevel3PorLevel1 { get; set; }
+        public List<PanelResultevel2Level3> listResultSetLevel3 { get; set; }
+    }
+
+    public class PanelResultevel2Level3
+    {
+        internal int level1_Id;
+        internal int level2_Id;
+
+        public string Name { get; set; }
+        public decimal Av { get; set; }
+        public decimal NC { get; set; }
+        public int Id { get; internal set; }
+    }
+
 }
