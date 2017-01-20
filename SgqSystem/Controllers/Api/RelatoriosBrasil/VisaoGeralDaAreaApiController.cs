62a36abcffec48e367d497dbb9e3f1a4ca5dacec
celsogea@hotmail.com <celsogea@hotmail.com>
2017-01-20 12:04:56 -0200

    Coleta de Indicadores Técnicos

--- SgqSystem/Controllers/Api/RelatoriosBrasil/VisaoGeralDaAreaApiController.cs
+++ SgqSystem/Controllers/Api/RelatoriosBrasil/VisaoGeralDaAreaApiController.cs
@@ -3,8 +3,6 @@ using SgqSystem.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
