e0c4ba182b2649885069c3f382a65bfe0e95608b
celsogea@hotmail.com <celsogea@hotmail.com>
2017-01-21 10:42:53 -0200

    Atualização de totais para monitoramento PCC1b

--- SgqSystem/Controllers/Api/PCC1BController.cs
+++ SgqSystem/Controllers/Api/PCC1BController.cs
@@ -2,10 +2,7 @@
 using SgqSystem.Handlres;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace SgqSystem.Controllers.Api
@@ -40,13 +37,13 @@ namespace SgqSystem.Controllers.Api
             using (var db = new SgqDbDevEntities())
             {
 
-                string querySequencial =   
+                string querySequencial =
                         "SELECT                                                                                             " +
                         "\n IsNull(Max(Sequential), 0) AS Sequential,                                                       " +
                         "\n 0 AS Side from CollectionLevel2                                                                 " +
-                        "\n WHERE ParLevel1_Id=3 and ParLevel2_Id = "+ receive.ParLevel2 + " AND                            " +
-                        "\n UnitId="+receive.Unit+" AND                                                                     " +
-                        "\n CollectionDate BETWEEN '" + receive.Data + " 00:00:00' AND '"+ receive.Data + " 23:59:59'       ";
+                        "\n WHERE ParLevel1_Id=3 and ParLevel2_Id = " + receive.ParLevel2 + " AND                            " +
+                        "\n UnitId=" + receive.Unit + " AND                                                                     " +
+                        "\n CollectionDate BETWEEN '" + receive.Data + " 00:00:00' AND '" + receive.Data + " 23:59:59'       ";
 
                 _list = db.Database.SqlQuery<_PCC1B>(querySequencial).ToList();
 
@@ -54,14 +51,14 @@ namespace SgqSystem.Controllers.Api
                 {
                     pcc1b = _list[0];
 
-                    string queryBanda = 
+                    string queryBanda =
                         "SELECT                                                                                             " +
                         "\n IsNull(Max(Sequential), 0) AS Sequential,                                                       " +
                         "\n IsNull(Max(Side), 0) AS Side from CollectionLevel2                                              " +
                         "\n WHERE ParLevel1_Id=3 and ParLevel2_Id = " + receive.ParLevel2 + "                               " +
                         "\n AND UnitId=" + receive.Unit + " AND CollectionDate                                              " +
-                        "\n BETWEEN '" + receive.Data + " 00:00:00' AND '" + receive.Data + " 23:59:59'                     "+
-                        "\n AND Sequential = " + pcc1b.Sequential+"                                                         ";
+                        "\n BETWEEN '" + receive.Data + " 00:00:00' AND '" + receive.Data + " 23:59:59'                     " +
+                        "\n AND Sequential = " + pcc1b.Sequential + "                                                         ";
 
                     _list = db.Database.SqlQuery<_PCC1B>(queryBanda).ToList();
 
@@ -70,6 +67,38 @@ namespace SgqSystem.Controllers.Api
             }
 
             return pcc1b;
+        }
+
+        [HttpPost]
+        [Route("TotalNC")]
+        public ResultTotalNC TotalNC(_Receive receive)
+        {
+
+            List<_PCC1B> _list = new List<_PCC1B>();
+
+            using (var db = new SgqDbDevEntities())
+            {
+
+
+            }
+
+            return new ResultTotalNC() { ncDianteiro = 1, ncTraseiro = 2 };
+        }
+
+
+    }
+
+
+    public class ResultTotalNC
+    {
+        public int ncDianteiro { get; set; }
+        public int ncTraseiro { get; set; }
+        public int totalNc
+        {
+            get
+            {
+                return ncDianteiro + ncTraseiro;
+            }
         }
     }
 }
