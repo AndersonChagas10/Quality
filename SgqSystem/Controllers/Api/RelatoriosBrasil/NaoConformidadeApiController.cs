9c1f0c4be827a364cf0aef557e00c499b81e5625
Gabriel Nunes <gabriel@grtsolucoes.com.br>
2017-01-31 17:02:22 -0200

    Não conformidades

--- SgqSystem/Controllers/Api/RelatoriosBrasil/NaoConformidadeApiController.cs
+++ SgqSystem/Controllers/Api/RelatoriosBrasil/NaoConformidadeApiController.cs
@@ -19,14 +19,65 @@ namespace SgqSystem.Controllers.Api.RelatoriosBrasil
         [Route("GraficoUnidades")]
         public List<NaoConformidadeResultsSet> GraficoUnidades([FromBody] FormularioParaRelatorioViewModel form)
         {
-            _list = CriaMockGraficoUnidades();
+            //_list = CriaMockGraficoUnidades();
 
-            //var query = new NaoConformidadeResultsSet().Select(form._dataInicio, form._dataFim, form.unitId);
+            var query = "" +
+                "\n SELECT " +
+                
+                "\n Unidade " +
+                "\n ,ProcentagemNc as [proc] " +
+               
+                "\n ,nc " +
+                "\n ,av " +
+                "\n FROM " +
+                "\n ( " +
+                "\n     SELECT " +
+                "\n       Unidade " +
+                "\n     , Unidade_Id " +
+                "\n     , sum(av) as av " +
+                "\n     , sum(nc) as nc " +
+                "\n     , CASE WHEN sum(AV) IS NULL OR sum(AV) = 0 THEN 0 ELSE sum(NC) / sum(AV) * 100 END AS ProcentagemNc " +
 
-            //using (var db = new SgqDbDevEntities())
-            //{
-            //    _list = db.Database.SqlQuery<NaoConformidadeResultsSet>(query).ToList();
-            //}
+                "\n     FROM " +
+                "\n     ( " +
+                "\n         SELECT " +
+                "\n          IND.Id         AS level1_Id " +
+                "\n         , IND.Name       AS Level1Name " +
+                "\n         , UNI.Id         AS Unidade_Id " +
+                "\n         , UNI.Name       AS Unidade " +
+                "\n         , CASE " +
+                "\n         WHEN IND.ParConsolidationType_Id = 1 THEN WeiEvaluation " +
+                "\n         WHEN IND.ParConsolidationType_Id = 2 THEN WeiEvaluation " +
+                "\n         WHEN IND.ParConsolidationType_Id = 3 THEN EvaluatedResult " +
+                "\n         ELSE 0 " +
+                "\n        END AS Av " +
+                "\n         , CASE " +
+                "\n         WHEN IND.ParConsolidationType_Id = 1 THEN WeiDefects " +
+                "\n         WHEN IND.ParConsolidationType_Id = 2 THEN WeiDefects " +
+                "\n         WHEN IND.ParConsolidationType_Id = 3 THEN DefectsResult " +
+                "\n         ELSE 0 " +
+
+                "\n         END AS NC " +
+                "\n         , (SELECT TOP 1 PercentValue FROM ParGoal WHERE ParLevel1_Id = CL1.ParLevel1_Id AND(ParCompany_Id = CL1.UnitId OR ParCompany_Id IS NULL) ORDER BY ParCompany_Id DESC) AS Meta " +
+                "\n         FROM ConsolidationLevel1 CL1 " +
+                "\n         INNER JOIN ParLevel1 IND " +
+                "\n         ON IND.Id = CL1.ParLevel1_Id " +
+                "\n         INNER JOIN ParCompany UNI " +
+                "\n         ON UNI.Id = CL1.UnitId " +
+                "\n         WHERE CL1.ConsolidationDate BETWEEN '" + form._dataInicioSQL + "' AND '" + form._dataFimSQL + "'" +
+             
+                "\n     ) S1 " +
+
+                "\n     GROUP BY Unidade, Unidade_Id " +
+
+                "\n ) S2 " +
+                "\n WHERE nc > 0 " +
+                "\n ORDER BY 1 DESC";
+
+            using (var db = new SgqDbDevEntities())
+            {
+                _list = db.Database.SqlQuery<NaoConformidadeResultsSet>(query).ToList();
+            }
 
             return _list;
         }
@@ -242,5 +293,5 @@ public class NaoConformidadeResultsSet
     public decimal Nc { get; set; }
     public decimal Av { get; set; }
     public decimal Meta { get; set; }
-    public int Proc { get; internal set; }
+    public decimal Proc { get; internal set; }
 }
