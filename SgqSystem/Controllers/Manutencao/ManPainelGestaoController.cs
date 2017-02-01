c54409ab19e9f7522b4b1e40cb9f7949ce1ed7da
renan <renan@DESKTOP-JR62LHP>
2017-02-01 17:29:54 -0200

    Criaçao do ManColetaDados e Painel de Gestao

---
+++ SgqSystem/Controllers/Manutencao/ManPainelGestaoController.cs
@@ -0,0 +1,22 @@
+﻿using Dominio;
+using SgqSystem.Secirity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SgqSystem.Controllers.Manutencao
+{
+    [CustomAuthorize]
+    [OutputCache(Duration = 20, VaryByParam = "none")]
+    public class ManPainelGestaoController : BaseController
+    {
+        private SgqDbDevEntities db = new SgqDbDevEntities();
+
+        [FormularioPesquisa(filtraUnidadePorUsuario = true)]
+        public ActionResult PainelGestaoManutencao()
+        {
+            return View();
+        }
+
+
+    }
+}
