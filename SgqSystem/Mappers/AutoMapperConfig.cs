b5679ff7059ed093b9bfc8a3129f1032128bd2de
renan <renan@DESKTOP-JR62LHP>
2017-02-07 17:12:39 -0200

    Comentarios nos indicadores do painel de manutencao

--- SgqSystem/Mappers/AutoMapperConfig.cs
+++ SgqSystem/Mappers/AutoMapperConfig.cs
@@ -95,7 +95,7 @@ namespace SgqSystem.Mappers
                 x.AddProfile<ResultLevel3MapperProfile>();
                 x.AddProfile<RoleMapperProfile>();
                 x.AddProfile<DefectMapperProfile>();
-                x.AddProfile<UserSgqProfile>();
+                //x.AddProfile<UserSgqProfile>();
 
 
 
