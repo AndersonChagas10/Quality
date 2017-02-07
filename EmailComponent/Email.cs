641516ff706583f534a8255daa1fbdf26123582d
celsogea@hotmail.com <celsogea@hotmail.com>
2017-02-07 15:50:35 -0200

    Servico de email.

---
+++ EmailComponent/Email.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+using System.Runtime.Remoting.Messaging;
+
+namespace EmailComponent
+{
+    public class Email :  ConfigEmail
+    {
+        public string subject { get; set; }
+        public string toName { get; set; }
+        public string toEmail { get; set; }
+        public string messageBody { get; set; }
+        public string smtpServer { get; set; }
+        public NetworkCredential smtpCredentials { get; set; }
+
+        public bool SendEmail(string toEmail, string toName)
+        {
+            try
+            {
+                MailMessage Message = new MailMessage();
+                Message.IsBodyHtml = true;
+                Message.To.Add(new MailAddress(toEmail, toName));
+                Message.From = (new MailAddress(this.emailLogin, this.emailLoginName));
+                Message.Subject = this.subject;
+                Message.Body = this.messageBody;
+
+                SmtpClient sc = new SmtpClient();
+                sc.Host = this.smtpServer;
+                sc.Credentials = smtpCredentials;
+                sc.Port = this.port;
+
+                sc.EnableSsl = true; // <-- ATENÇÃO.
+
+                sc.Send(Message);
+            }
+            #pragma warning disable CS0168 // Variable is declared but never used
+            catch (Exception ex)
+            #pragma warning restore CS0168 // Variable is declared but never used
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public delegate bool SendEmailDelegate(string toEmail, string toName);
+
+        public void GetResultsOnCallback(IAsyncResult ar)
+        {
+            SendEmailDelegate del = (SendEmailDelegate)((AsyncResult)ar).AsyncDelegate;
+            try
+            {
+                bool result = del.EndInvoke(ar);
+            }
+            #pragma warning disable CS0168 // Variable is declared but never used
+            catch (Exception ex)
+            #pragma warning restore CS0168 // Variable is declared but never used
+            {
+                #pragma warning disable CS0219 // Variable is assigned but its value is never used
+                bool result = false;
+                #pragma warning restore CS0219 // Variable is assigned but its value is never used
+            }
+        }
+
+        public bool SendEmailAsync(string toEmail, string toName)
+        {
+            try
+            {
+                SendEmailDelegate dc = new SendEmailDelegate(this.SendEmail);
+                AsyncCallback cb = new AsyncCallback(this.GetResultsOnCallback);
+                IAsyncResult ar = dc.BeginInvoke(toEmail, toName, cb, null);
+            }
+            #pragma warning disable CS0168 // Variable is declared but never used
+            catch (Exception ex)
+            #pragma warning restore CS0168 // Variable is declared but never used
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+}
---
+++ EmailComponent/GetEmailIdsFromDB.cs
@@ -0,0 +1,70 @@
+﻿using Dominio.ADO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmailComponent
+{
+    public class GetEmailIdsFromDB
+    {
+
+        public string connectionString { get; set; }
+
+        public string getConfig
+        {
+            get
+            {
+                //MOCK
+                var query = "SELECT 'celsogea@hotmail.com' AS emailLogin    " +
+                            ", 'celsogea' AS emailLoginName                 " +
+                            ", 'smtp.live.com' AS host                      " +
+                            ", '587' AS port                                " +
+                            ", 'thebost1' AS pass";
+
+                return query;
+            }
+        }
+
+        public Email GetEmailConfig()
+        {
+            using (var db = new FactoryADO(connectionString))
+            {
+                var result = db.SearchQuery<Email>(getConfig).FirstOrDefault();
+                return result;
+            }
+        }
+
+        public List<Email> GetListMail()
+        {
+            var mock = MockEmailTesteAlerta();
+
+            using (var db = new FactoryADO(connectionString))
+            {
+                //var result = db.SearchQuery<Email>(getConfig).FirstOrDefault();
+            }
+
+            return mock;
+        }
+
+        private List<Email> MockEmailTesteAlerta()
+        {
+            //USAR GUARD SE NECESsARIO PARA MONTAR LISTA DEFINITIVA.
+            var _list = new List<Email>();
+
+            _list.Add(new Email()
+            {
+                subject = "TESTE Subject 1",
+                messageBody = "TESTE messageBody 1"
+            });
+
+            _list.Add(new Email()
+            {
+                subject = "TESTE Subject 2",
+                messageBody = "TESTE messageBody 2"
+            });
+
+            return _list;
+
+        }
+
+    }
+}
