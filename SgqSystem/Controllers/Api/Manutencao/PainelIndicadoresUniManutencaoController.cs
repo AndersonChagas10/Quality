dbc91115f9511f558d04b60cc5a38ef9fcb3d992
renan <renan@DESKTOP-JR62LHP>
2017-02-10 15:35:32 -0200

    Manutencao

--- SgqSystem/Controllers/Api/Manutencao/PainelIndicadoresUniManutencaoController.cs
+++ SgqSystem/Controllers/Api/Manutencao/PainelIndicadoresUniManutencaoController.cs
@@ -1,11 +1,7 @@
 ﻿using Dominio;
 using DTO.DTO.Manutencao;
-using System;
 using System.Collections.Generic;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace SgqSystem.Controllers.Api.Manutencao
