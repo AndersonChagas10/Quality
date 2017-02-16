38d7f08c3628afb167c6d00df22268e923c2820b
renan <renan@DESKTOP-JR62LHP>
2017-02-16 11:24:06 -0200

    Manuntecao Coleta

--- SgqSystem/Controllers/Api/Manutencao/ManDataCollectITsController.cs
+++ SgqSystem/Controllers/Api/Manutencao/ManDataCollectITsController.cs
@@ -99,6 +99,87 @@ namespace SgqSystem.Controllers.Api.Manutencao
             return Indicadores2;
         }
 
+        [HttpPost]
+        [Route("GetIndicadoresEdit")]
+        public List<Indicador> GetIndicadoresEdit(Obj3 obj3)
+        {
+            List<Indicador> Indicadores;
+            List<Indicador> Indicadores2 = new List<Indicador>();
+
+            string query = "SELECT DISTINCT DimName as Nome, Name as NomeReal FROM DimManColetaDados WHERE DimRealTarget = 'Real' and DimName is not null";
+
+            Indicadores = db.Database.SqlQuery<Indicador>(query).ToList();
+
+            //Busca valores do real e meta dos indicadores
+
+            //Pergunta se existe o Indicador na data
+
+            int nIndicadores = Indicadores.Count;
+
+            for (int i = 0; i < nIndicadores; i++)
+            {
+                query = "SELECT top 1 " + Indicadores[i].NomeReal + " as PerguntaIndicador FROM ManColetaDados WHERE Base_dateRef = '" + obj3.Date.ToString("yyyy-MM-dd") + "' AND " + Indicadores[i].NomeReal + " IS NOT NULL AND Base_parCompany_id = " + obj3.Unit;
+
+                Nullable<decimal> result = db.Database.SqlQuery<Nullable<decimal>>(query).FirstOrDefault();
+
+                if (result != null)
+                {
+                    Indicadores2.Add(Indicadores[i]);
+                }
+
+            }
+
+            nIndicadores = Indicadores2.Count;
+
+            for (int i = 0; i < nIndicadores; i++)
+            {
+                query = "SELECT top 1 " + Indicadores2[i].NomeReal + " as ValorReal FROM ManColetaDados WHERE Base_dateRef = '" + obj3.Date.ToString("yyyy-MM-dd") + "' AND " + Indicadores2[i].NomeReal + " IS NOT NULL AND Base_parCompany_id = " + obj3.Unit;
+                Indicadores2 = db.Database.SqlQuery<Indicador>(query).ToList();
+            }
+
+            return Indicadores2;
+        }
+
+        [HttpPost]
+        [Route("SaveEdit")]
+        public int SaveEdit(Obj obj)
+        {
+            string sql = "";
+
+            sql = "select Name as indicadorNome from DimManColetaDados where DimRealTarget = 'Real' and DimName is not null and DimName = '" + obj.descricao + "'";
+
+            var db1 = new SgqDbDevEntities();
+
+            List<Obj2> list = db1.Database.SqlQuery<Obj2>(sql).ToList();
+
+            obj.indicadorNome = list[0].indicadorNome;
+
+            sql = "";
+
+            sql = "INSERT INTO dbo.ManColetaDados " +
+            "(" +
+            "Base_parCompany_id " +
+            ",Base_dateAdd " +
+            ",Base_dateRef " +
+            ",Comentarios " +
+            "," + obj.indicadorNome +
+            ") " +
+            "VALUES " +
+            "(" +
+            "" + obj.parCompany + "," +
+            "'" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "'," +
+            "'" + obj.data.ToString("yyyy-MM-dd HH:mm:ss") + "'," +
+            "'" + obj.comentarios + "'," +
+            " replace('" + obj.quantidade + "',',','.')" + //BS: Alteração feita=> Troca de Virgula (,) por Ponto (.): Replace()
+            ")";
+
+            using (var db = new SgqDbDevEntities())
+            {
+                var d = db.Database.ExecuteSqlCommand(sql);
+                return d;
+            }
+
+        }
     }
 
     public class Obj
@@ -126,6 +207,7 @@ namespace SgqSystem.Controllers.Api.Manutencao
     {
         public string Nome { get; set; }
         public string NomeReal { get; set; }
+        public decimal? ValorReal { get; set; }     
     }
 
     public class Pergunta
