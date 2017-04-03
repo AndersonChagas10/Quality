1c7b84aaef316785b0272531dd6daecb8b7cf8b7
Torrer <ronaldo.torre@outlook.com>
2017-04-03 16:21:07 -0300

    Correção para .net 4.6 incluindo pcls

--- SgqSystem/Services/SGQDBContext_YTOARA.cs
+++ SgqSystem/Services/SGQDBContext_YTOARA.cs
@@ -1,59 +1,59 @@
-﻿//using System.Data.SqlClient;
-//using System.Linq;
-//using Dominio.YTOARA;
-//using Dapper;
-//using System.Collections.Generic;
+﻿using System.Data.SqlClient;
+using System.Linq;
+using Dominio.YTOARA;
+using Dapper;
+using System.Collections.Generic;
 
-//namespace SgqSystem.Services
-//{
-//    public class SGQDBContext_YTOARA
-//    {
-//        string conexao;
-//        SqlConnection db;
-//        string sql;
+namespace SgqSystem.Services
+{
+    public class SGQDBContext_YTOARA
+    {
+        string conexao;
+        SqlConnection db;
+        string sql;
 
-//        Estrutura estrutura;
-//        Elemento elemento;
+        Estrutura estrutura;
+        Elemento elemento;
 
-//        public SGQDBContext_YTOARA()
-//        {
-//            conexao = System.Configuration.ConfigurationManager.ConnectionStrings["DbContextSgqEUA"].ConnectionString;
+        public SGQDBContext_YTOARA()
+        {
+            conexao = System.Configuration.ConfigurationManager.ConnectionStrings["DbContextSgqEUA"].ConnectionString;
 
-//            db = new SqlConnection(conexao);
-//            estrutura = new Estrutura();
-//            elemento = new Elemento();
-//        }
-        
-//        public List<Estrutura> getAllEstrutura()
-//        {
-//            sql = "SELECT * FROM ESTRUTURA; ";
+            db = new SqlConnection(conexao);
+            estrutura = new Estrutura();
+            elemento = new Elemento();
+        }
 
-//            return db.Query<Estrutura>(sql).ToList();
-//        }
+        public List<Estrutura> getAllEstrutura()
+        {
+            sql = "SELECT * FROM ESTRUTURA; ";
 
-//        public List<Estrutura> getEstruturaByParam(string parametro, string value)
-//        {
-//            sql = "SELECT * FROM ESTRUTURA WHERE " + parametro + " " + value;
+            return db.Query<Estrutura>(sql).ToList();
+        }
 
-//            return db.Query<Estrutura>(sql).ToList();
-//        }
+        public List<Estrutura> getEstruturaByParam(string parametro, string value)
+        {
+            sql = "SELECT * FROM ESTRUTURA WHERE " + parametro + " " + value;
 
-//        public List<Elemento> getALLElemento()
-//        {
-//            sql = "SELECT S.* FROM ELEMENTO E " +
-//                  "INNER JOIN ELEMENTO S ON S.ID_ESTRUTURA = E.ID_ESTRUTURA;";
+            return db.Query<Estrutura>(sql).ToList();
+        }
 
-//            return db.Query<Elemento>(sql).ToList();
-//        }
+        public List<Elemento> getALLElemento()
+        {
+            sql = "SELECT S.* FROM ELEMENTO E " +
+                  "INNER JOIN ELEMENTO S ON S.ID_ESTRUTURA = E.ID_ESTRUTURA;";
 
-//        public List<Elemento> getElementoByParam(string parametro, string value)
-//        {
-//            sql = "SELECT S.* FROM ELEMENTO E "+
-//                  "INNER JOIN ELEMENTO S ON S.ID_ESTRUTURA = E.ID_ESTRUTURA" +
-//                  "WHERE " + parametro + " " + value;
+            return db.Query<Elemento>(sql).ToList();
+        }
 
-//            return db.Query<Elemento>(sql).ToList();
-//        }
+        public List<Elemento> getElementoByParam(string parametro, string value)
+        {
+            sql = "SELECT S.* FROM ELEMENTO E " +
+                  "INNER JOIN ELEMENTO S ON S.ID_ESTRUTURA = E.ID_ESTRUTURA" +
+                  "WHERE " + parametro + " " + value;
 
-//    }
-//}
+            return db.Query<Elemento>(sql).ToList();
+        }
+
+    }
+}
