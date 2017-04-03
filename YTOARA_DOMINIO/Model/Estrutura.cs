e61783d4b084b332af73172a20fff751f92c2803
Torrer <ronaldo.torre@outlook.com>
2017-04-03 14:16:44 -0300

    Inclusão do contexto e ajuste do dominio da YTOARA

--- YTOARA_DOMINIO/Model/Estrutura.cs
+++ YTOARA_DOMINIO/Model/Estrutura.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace YTOARA_DOMINIO
+﻿namespace Dominio.YTOARA
 {
     public class Estrutura
     {
