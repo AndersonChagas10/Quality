68d2ca80119197a60cb66a2d299d0ce687fc4fec
Torrer <ronaldo.torre@outlook.com>
2017-04-03 12:18:34 -0300

    Incluir DOMINIO da YTOARA

---
+++ YTOARA_DOMINIO/Model/Elemento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTOARA_DOMINIO
+{
+    public class Elemento
+    {
+        public int idelemento { get; set; }
+        public int idestrutura { get; set; }
+        public string nome { get; set; }
+
+        public Elemento()
+        {
+
+        }
+
+    }
+}
---
+++ YTOARA_DOMINIO/Model/Estrutura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTOARA_DOMINIO
+{
+    public class Estrutura
+    {
+        public int idestrurua { get; set; }
+        public int idpai { get; set; }
+        public string nome { get; set; }
+        public string sigla { get; set; }
+
+        public Estrutura()
+        {
+
+        }
+
+    }
+}
