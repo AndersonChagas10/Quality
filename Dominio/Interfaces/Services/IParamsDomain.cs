a81418d5ae0b7fc54ef8071360525515ac658bb6
Note-pc <celsogea@hotmail.com>
2017-04-04 12:21:07 -0300

    Correções repositório. Params

--- Dominio/Interfaces/Services/IParamsDomain.cs
+++ Dominio/Interfaces/Services/IParamsDomain.cs
@@ -22,12 +22,14 @@ namespace Dominio.Interfaces.Services
         ParamsDTO GetLevel3(int idParLevel3, int? idParLevel2);
         //ParLevel3Level2DTO AddVinculoL3L2(int idLevel2, int idLevel3, decimal peso);
         //ParLevel3Level2Level1DTO AddVinculoL1L2(int idLevel1, int idLevel2Level3);
-        List<ParLevel3Level2Level1DTO> AddVinculoL1L2(int idLevel1, int idLevel2, int idLevel3, int? userId);
+        List<ParLevel3Level2Level1DTO> AddVinculoL1L2(int idLevel1, int idLevel2, int idLevel3, int? userId, int? companyId = null);
         ParLevel3Level2DTO AddVinculoL3L2(int idLevel2, int idLevel3, decimal peso, int? groupLevel2);
         bool RemVinculoL1L2(int idLevel1, int idLevel2);
         bool VerificaShowBtnRemVinculoL1L2(int idLevel1, int idLevel2);
         bool SetRequiredCamposCabecalho(int id, int required);
         ParMultipleValues SetDefaultMultiplaEscolha(int idHeader, int idMultiple);
         ParLevel2XHeaderField AddRemoveParHeaderLevel2(ParLevel2XHeaderField parLevel2XHeaderField);
+
+       
     }
 }
