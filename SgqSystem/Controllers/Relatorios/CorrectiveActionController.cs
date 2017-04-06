95b8156eeea72e7b693f360e2067956043e4e6f5
renan <renan@DESKTOP-JR62LHP>
2017-04-06 09:42:43 -0300

    Corretive Action Controller

---
+++ SgqSystem/Controllers/Relatorios/CorrectiveActionController.cs
@@ -0,0 +1,19 @@
+﻿using Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SgqSystem.Controllers.Relatorios
+{
+    [CustomAuthorize]
+    public class CorrectiveActionController : BaseController
+    {
+        // GET: CorrectiveAction
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
