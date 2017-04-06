2bdc6c3662dd66a0b6782214d97f94013ee8f571
renan <renan@DESKTOP-JR62LHP>
2017-04-06 10:18:38 -0300

    Corrective Action

---
+++ SgqSystem/Controllers/Api/Reports/CorrectiveActionApiController.cs
@@ -0,0 +1,62 @@
+﻿using Dominio.Interfaces.Services;
+using DTO.DTO;
+using DTO.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SgqSystem.Controllers.Api.Reports
+{
+    [RoutePrefix("api/CorrectiveAction")]
+    public class CorrectiveActionApiController : ApiController
+    {
+        //private readonly ICorrectiveActionDomain _correctiveActionAppService;
+        //private readonly IUserDomain _userAppService;
+
+        //public CorrectiveActionApiController(ICorrectiveActionDomain correctiveActionAppService, IUserDomain userAppService)
+        //{
+        //    _correctiveActionAppService = correctiveActionAppService;
+        //    _userAppService = userAppService;
+        //}
+
+        //[Route("SalvarAcaoCorretiva")]
+        //[HttpPost]
+        //public GenericReturn<CorrectiveActionDTO> SalvarAcaoCorretiva([FromBody]CorrectiveActionViewModel model)
+        //{
+        //    return _correctiveActionAppService.SalvarAcaoCorretiva(model);
+        //}
+
+        //[Route("GetCorrectiveAction")]
+        //[HttpPost]
+        //public GenericReturn<List<CorrectiveActionDTO>> GetCorrectiveAction([FromBody]FormularioParaRelatorioViewModel model)
+        //{
+        //    return _correctiveActionAppService.GetCorrectiveAction(model);
+        //}
+
+        //[Route("LoginSlaughterTechinical")]
+        //[HttpPost]
+        //public GenericReturn<UserDTO> LoginSlaughterTechinical([FromBody]UserViewModel model)
+        //{
+        //    var result = _userAppService.AuthenticationLogin(model);
+        //    return result;
+        //}
+
+        //[Route("VerificarAcaoCorretivaIncompleta")]
+        //[HttpPost]
+        //public GenericReturn<CorrectiveActionDTO> VerificarAcaoCorretivaIncompleta([FromBody]CorrectiveActionViewModel model)
+        //{
+        //    var result = _correctiveActionAppService.VerificarAcaoCorretivaIncompleta(model);
+        //    return result;
+        //}
+
+        //[Route("GetCorrectiveActionById")]
+        //[HttpPost]
+        //public GenericReturn<CorrectiveActionDTO> GetCorrectiveActionById([FromBody]int id)
+        //{
+        //    return _correctiveActionAppService.GetCorrectiveActionById(id);
+        //}
+    }
+}
