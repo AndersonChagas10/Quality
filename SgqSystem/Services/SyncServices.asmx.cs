f5761def3b89be73770c9d7a6ba48e5ae37cd46b
Torrer <ronaldo.torre@outlook.com>
2017-04-11 15:35:01 -0300

    Ajuste de layout das colunas do tablet.

--- SgqSystem/Services/SyncServices.asmx.cs
+++ SgqSystem/Services/SyncServices.asmx.cs
@@ -3371,17 +3371,17 @@ namespace SgqSystem.Services
                 string headerCounter =
                                      html.div(
                                                outerhtml: "<b>" + CommonData.getResource("ev").Value.ToString() + " </b>",
-                                               classe: "col-xs-3",
+                                               classe: "col-xs-4",
                                                style: "text-align:center"
                                              ) +
                                      html.div(
                                                outerhtml: "<b>" + CommonData.getResource("sd").Value.ToString() + " </b>",
-                                               classe: "col-xs-3",
+                                               classe: "col-xs-4",
                                                style: "text-align:center"
                                               ) +
                                       html.div(
                                                outerhtml: "<b>" + CommonData.getResource("df").Value.ToString() + " </b>",
-                                               classe: "col-xs-3",
+                                               classe: "col-xs-4",
                                                style: "text-align:center"
                                              );
 
@@ -3416,24 +3416,22 @@ namespace SgqSystem.Services
                 string counters =
                                       html.div(
                                                 outerhtml: html.span(outerhtml: "0", classe: "evaluateCurrent") + html.span(outerhtml: " / ", classe: "separator") + html.span(outerhtml: evaluate.ToString(), classe: "evaluateTotal"),
-                                                classe: "col-xs-3",
+                                                classe: "col-xs-4",
                                                 style: "text-align:center"
                                               ) +
                                       html.div(
                                                 outerhtml: html.span(outerhtml: "0", classe: "sampleCurrent hide") + html.span(outerhtml: "0", classe: "sampleCurrentTotal") + html.span(outerhtml: " / ", classe: "separator") + html.span(outerhtml: sample.ToString(), classe: "sampleTotal hide") + html.span(outerhtml: totalSampleXEvaluate.ToString(), classe: "sampleXEvaluateTotal"),
-                                                classe: "col-xs-3",
+                                                classe: "col-xs-4",
                                                 style: "text-align:center"
                                               )+
                                        html.div(
                                                 outerhtml: html.span(outerhtml: "0", classe: "defects") + html.span(outerhtml: " / ", classe: "separator") + html.span(outerhtml: defect.ToString() , classe: "defectstotal"),
-                                                classe: "col-xs-3",
+                                                classe: "col-xs-4",
                                                 style: "text-align:center"
                                               );
 
                 
 
-                //
-                //+
                 //                        html.div(
                 //                                    outerhtml: html.span(outerhtml: "0", classe: "defectsLevel2"),
                 //                                    classe: "col-xs-3",
