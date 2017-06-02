e1705245e9177220fd6400829084d24f4d3030a3
renan <renan@ASUS-K45VM>
2017-06-02 14:02:53 -0300

    Relatorio Generico

--- SgqSystem/Controllers/Api/RelatorioGenericoApiController.cs
+++ SgqSystem/Controllers/Api/RelatorioGenericoApiController.cs
@@ -1,8 +1,12 @@
 ﻿using Dominio;
+using Newtonsoft.Json.Linq;
 using SgqSystem.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Dynamic;
 using System.Linq;
+using System.Reflection;
 using System.Web.Http;
 
 namespace SgqSystem.Controllers.Api
@@ -64,6 +68,55 @@ namespace SgqSystem.Controllers.Api
             return retorno;
         }
 
+
+        [HttpPost]
+        [Route("getTabela3")]
+        public dynamic getTabela3(JObject model)
+        {
+            var db = new SgqDbDevEntities();
+            var query = "SELECT TOP 10 STR(Id) as Id, Name as Nome, [Password] as Pass from UserSgq";
+            var retorno = QueryNinja(db, query);
+
+            return retorno;
+        }
+
+        //QueryGenerica para implementar
+        protected dynamic QueryNinja(DbContext db, string query)
+        {
+            db.Database.Connection.Open();
+            var cmd = db.Database.Connection.CreateCommand();
+            cmd.CommandText = query;
+            var reader = cmd.ExecuteReader();
+            List<JObject> datas = new List<JObject>();
+            List<JObject> columns = new List<JObject>();
+            dynamic retorno = new ExpandoObject();
+
+            while (reader.Read())
+            {
+                var row = new JObject();
+                for (int i = 0; i < reader.FieldCount; i++)
+                    row[reader.GetName(i)] = reader[i].ToString();
+
+                datas.Add(row);
+            }
+            
+
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                var col = new JObject();
+                col["title"] = col["data"] = reader.GetName(i);
+                columns.Add(col);
+            }
+
+            
+
+            retorno.datas = datas;
+            retorno.columns = columns;
+
+            return retorno;
+        }
+
+        //ApiRelatoriosController
     }
 
     public class PropriedadesGenericas
@@ -124,4 +177,5 @@ namespace SgqSystem.Controllers.Api
     {
         public string title { get; set; }
     }
+
 }
