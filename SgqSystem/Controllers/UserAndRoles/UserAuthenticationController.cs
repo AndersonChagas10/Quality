1c7629626d1fbe24d336a032486c8e2615745920
wesle <wesle@DESKTOP-T3K263B>
2017-06-16 18:21:06 -0300

    recupera Senha

--- SgqSystem/Controllers/UserAndRoles/UserAuthenticationController.cs
+++ SgqSystem/Controllers/UserAndRoles/UserAuthenticationController.cs
@@ -16,7 +16,7 @@ namespace SgqSystem.Controllers.Api
         private readonly IUserDomain _userDomain;
         private readonly IBaseDomain<UserSgq, UserDTO> _userBaseDomain;
         private IBaseDomain<UserSgq, UserSgqDTO> _userSgqDomain;
-        //private IBaseDomain<EmailContent, EmailContentDTO> _emailContent;
+        private IBaseDomain<EmailContent, EmailContentDTO> _emailContent;
 
         public UserAuthenticationController(IUserDomain userDomain, IBaseDomain<UserSgq, UserDTO> userBaseDomain, IBaseDomain<UserSgq, UserSgqDTO> userSgqDomain
             /*,IBaseDomain<EmailContent, EmailContentDTO> emailContent*/)
@@ -124,22 +124,18 @@ namespace SgqSystem.Controllers.Api
             email.To = user.Email;
             email.Subject = "Recuperação de Senha Sgq";
             email.Body = "Seu usuário: " + user.Name + "\n " + "sua Senha: " + hash;
+            email.AddDate = DateTime.Now;
+            email.Project = "SGQApp";
+            email.IsBodyHtml = false;
             using (var db = new SgqDbDevEntities())
             {
                 db.EmailContent.Add(email);
                 var ret = db.SaveChanges();
-                int r = ret;
-                if (ret == 1)
+                if (ret > 0)
                     return true;
                 else
                     return false;
             }
-            var retorno = _emailContent.AddOrUpdate(email);
-    var a = retorno;
-        if (retorno != null)
-            return true;
-        else
-            return false;
         }
     }
 }
