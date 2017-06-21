3ef832bd4de3a26493112bba77fe094e3eace48e
Note-pc <Note-pc@NOTE>
2017-06-21 12:41:24 -0300

    Email de corrective action, Melhorias PA

--- PlanoAcaoCore/Conn.cs
+++ PlanoAcaoCore/Conn.cs
@@ -2,23 +2,23 @@
 {
     public static class Conn
     {
-        //Local
-        public static string catalog { get { return "PlanoDeAcao"; } }
-        public static string dataSource { get { return @"SERVERGRT\MSSQLSERVER2014"; } }
-        public static string user { get { return "sa"; } }
-        public static string pass { get { return "1qazmko0"; } }
-        public static string catalog2 { get { return "dbGQualidade_JBS"; } }
-        public static string dataSource2 { get { return @"SERVERGRT\MSSQLSERVER2014"; } }
-        public static string user2 { get { return "sa"; } }
-        public static string pass2 { get { return "1qazmko0"; } }
+        ////Local
+        //public static string catalog { get { return "PlanoDeAcao"; } }
+        //public static string dataSource { get { return @"SERVERGRT\MSSQLSERVER2014"; } }
+        //public static string user { get { return "sa"; } }
+        //public static string pass { get { return "1qazmko0"; } }
+        //public static string catalog2 { get { return "dbGQualidade_JBS"; } }
+        //public static string dataSource2 { get { return @"SERVERGRT\MSSQLSERVER2014"; } }
+        //public static string user2 { get { return "sa"; } }
+        //public static string pass2 { get { return "1qazmko0"; } }
 
-        public static string selfRoot { get { return "http://localhost/PlanoDeAcaoMVC"; } }
+        //public static string selfRoot { get { return "http://localhost/PlanoDeAcaoMVC"; } }
 
-        public static string emailFrom = "celsogea@hotmail.com";
-        public static string emailPass = "Thebost1";
-        public static string emailSmtp = "smtp.live.com";
-        public static int emailPort = 587;
-        public static bool emailSSL = true;
+        //public static string emailFrom = "celsogea@hotmail.com";
+        //public static string emailPass = "Thebost1";
+        //public static string emailSmtp = "smtp.live.com";
+        //public static int emailPort = 587;
+        //public static bool emailSSL = true;
 
         ////Azure
         //public static string catalog { get { return "PlanoDeAcao"; } }
@@ -50,24 +50,24 @@
         //public static string user2 { get { return "grjqualidadedev"; } }
         //public static string pass2 { get { return "Mi3UpU0J35<_"; } }
 
-        ////Utilizar para JBS
-        ////Remoto
-        //public static string catalog { get { return "dbGQualidadeTeste"; } }
-        //public static string dataSource { get { return @"10.255.0.41"; } }
-        //public static string user { get { return "UserGQualidade"; } }
-        //public static string pass { get { return "grJsoluco3s"; } }
-        ////Remoto
-        //public static string catalog2 { get { return "dbGQualidade"; } }
-        //public static string dataSource2 { get { return @"10.255.0.16"; } }
-        //public static string user2 { get { return "UserGQualidade"; } }
-        //public static string pass2 { get { return "grJsoluco3s"; } }
+        //Utilizar para JBS
+        //Remoto
+        public static string catalog { get { return "dbGQualidadeTeste"; } }
+        public static string dataSource { get { return @"10.255.0.41"; } }
+        public static string user { get { return "UserGQualidade"; } }
+        public static string pass { get { return "grJsoluco3s"; } }
+        //Remoto
+        public static string catalog2 { get { return "dbGQualidade"; } }
+        public static string dataSource2 { get { return @"10.255.0.16"; } }
+        public static string user2 { get { return "UserGQualidade"; } }
+        public static string pass2 { get { return "grJsoluco3s"; } }
 
-        //public static string selfRoot { get { return "http://mtzsvmqsc/PlanoDeAcao/"; } }
+        public static string selfRoot { get { return "http://mtzsvmqsc/PlanoDeAcao/"; } }
 
-        //public static string emailFrom = "sgq@jbs.com.br";
-        //public static string emailPass = "B7pwGJD44SbY";
-        //public static string emailSmtp = "correio.jbs.com.br";
-        //public static int emailPort = 587;
-        //public static bool emailSSL = false;
+        public static string emailFrom = "sgq@jbs.com.br";
+        public static string emailPass = "B7pwGJD44SbY";
+        public static string emailSmtp = "correio.jbs.com.br";
+        public static int emailPort = 587;
+        public static bool emailSSL = false;
     }
 }
--- PlanoDeAcaoMVC/Controllers/Api/ApiRelatoriosController.cs
+++ PlanoDeAcaoMVC/Controllers/Api/ApiRelatoriosController.cs
@@ -1,8 +1,6 @@
 ﻿using DTO.Helpers;
 using Newtonsoft.Json.Linq;
-using PlanoAcaoCore;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web.Http;
 
 namespace PlanoDeAcaoMVC.Controllers.Api
@@ -10,61 +8,81 @@ namespace PlanoDeAcaoMVC.Controllers.Api
     [RoutePrefix("api/Relatorios")]
     public class ApiRelatoriosController : BaseApiController
     {
-        PlanoAcaoEF.PlanoDeAcaoEntities db;
+
+        private PlanoAcaoEF.PlanoDeAcaoEntities db;
+
         public ApiRelatoriosController()
         {
             db = new PlanoAcaoEF.PlanoDeAcaoEntities();
             db.Configuration.LazyLoadingEnabled = false;
             db.Configuration.AutoDetectChangesEnabled = false;
         }
+        
 
         //Retorna Categorias Series e Dados
-        [HttpGet]
+        [HttpPost]
         [Route("GetGrafico")]
-        public List<JObject> GetGrafico([FromBody] filtros filtro)
+        public List<JObject> GetGrafico(JObject filtro)
         {
-            //var query = "SELECT ACAO.* ,                                                                   " +
-            //            "\n  STA.*,                                                                        " +
-            //            "\n  UN.*,                                                                         " +
-            //            "\n  DPT.*,                                                                        " +
-            //            "\n  Q.*,                                                                          " +
-            //            "\n  CG.*,                                                                         " +
-            //            "\n  CMG.*,                                                                        " +
-            //            "\n  GC.*,                                                                          " +
-            //            "\n  PLANE.*                                                                          " +
-            //            "\n  FROM pa_acao ACAO                                                             " +
-            //            "\n  LEFT JOIN Pa_Unidade UN ON UN.Id = ACAO.Unidade_Id                            " +
-            //            "\n LEFT JOIN Pa_Departamento DPT ON DPT.Id = ACAO.Departamento_Id                 " +
-            //            "\n  LEFT JOIN Pa_Quem Q ON Q.Id = ACAO.Quem_Id                                    " +
-            //            "\n  LEFT JOIN Pa_CausaGenerica CG ON CG.Id = ACAO.CausaGenerica_Id                " +
-            //            "\n  LEFT JOIN Pa_ContramedidaGenerica CMG ON CMG.Id = ACAO.ContramedidaGenerica_Id" +
-            //            "\n  LEFT JOIN Pa_GrupoCausa GC ON GC.Id = ACAO.GrupoCausa_Id                      " +
-            //            "\n  LEFT JOIN Pa_Planejamento PLANE ON PLANE.Id = ACAO.Panejamento_Id "+ 
-            //            "\n LEFT JOIN Pa_Status STA ON STA.Id = ACAO.[Status]; ";
+            string dtInit;
+            string dtFim;
+            GetParamsPeloFiltro(filtro, out dtInit, out dtFim);
 
-            var query = "SELECT Acao.Id, Sta.Name AS [Status], ISNULL(Quem.Name, 'Não Atribuido') AS Responsavel" +
-                        "--, cast(Acao.QuandoInicio As Date) AS QuandoInicio" +
-                        "--, cast(Acao.QuandoFim As Date) AS QuandoFim" +
-                        "\n FROM Pa_Acao AS Acao                            " +
-                        "\n LEFT JOIN Pa_Status Sta ON Sta.Id = Acao.Status " +
-                        "\n LEFT JOIN Pa_Quem Quem ON Quem.Id = Acao.Quem_Id";
+            var query = "SELECT Acao.Id, Sta.Name AS[Status]," +
+            "\n  ISNULL(Quem.Name, 'Não possui Responsável') AS Responsavel," +
+            "\n  ISNULL(ContraMedGen.ContramedidaGenerica, 'Não possui Contramedida Genérica') AS 'Contramedida Genérica'," +
+            "\n  ISNULL(CausaGen.CausaGenerica, 'Não possui Causa Genérica') AS 'Causa Genérica'," +
+            "\n  ISNULL(GrpCausa.GrupoCausa, 'Não possui Grupo Causa') AS 'Grupo Causa'," +
+            "\n  ISNULL(Un.Description, 'Corporativo') AS 'Unidade'," +
+            "\n  ISNULL(IndicDir.Name, 'Não possui Indicadores Diretriz') AS 'Indicadores Diretriz'," +
+            "\n  ISNULL(DiretObj.Name, 'Não possui Diretriz / Objetivo') AS 'Diretriz / Objetivo'," +
+            "\n  ISNULL(Diretor.Name, 'Não possui Diretoria') AS 'Diretoria'," +
+            "\n  ISNULL(Dimens.Name, 'Não possui Dimensão') AS 'Dimensão'," +
+            "\n  ISNULL(Gere.Name, 'Não possui Gerência') AS 'Gerência'," +
+            "\n  ISNULL(Coord.Name, 'Não possui Coordenação') AS 'Coordenação'" +
+            "\n FROM Pa_Acao AS Acao" +
+            "\n LEFT JOIN Pa_Status Sta ON Sta.Id = Acao.Status" +
+            "\n LEFT JOIN Pa_Quem Quem ON Quem.Id = Acao.Quem_Id" +
+            "\n LEFT JOIN Pa_ContramedidaGenerica ContraMedGen ON ContraMedGen.Id = Acao.ContramedidaGenerica_Id" +
+            "\n LEFT JOIN Pa_CausaGenerica CausaGen ON CausaGen.Id = Acao.CausaGenerica_Id" +
+            "\n LEFT JOIN Pa_GrupoCausa GrpCausa ON GrpCausa.Id = Acao.GrupoCausa_Id" +
+            "\n LEFT JOIN Pa_Unidade Un ON Un.Id = Acao.Unidade_Id" +
+            "\n LEFT JOIN Pa_Planejamento PlanTatico ON PlanTatico.Id = Acao.Panejamento_Id" +
+            "\n LEFT JOIN Pa_Planejamento PlanEstrategy ON PlanEstrategy.Id = PlanTatico.Estrategico_Id" +
+            "\n LEFT JOIN Pa_IndicadoresDiretriz IndicDir ON IndicDir.Id = PlanEstrategy.IndicadoresDiretriz_Id" +
+            "\n LEFT JOIN Pa_Objetivo DiretObj ON DiretObj.Id = PlanEstrategy.Objetivo_Id" +
+            "\n LEFT JOIN Pa_Diretoria Diretor ON Diretor.Id = PlanEstrategy.Diretoria_Id" +
+            "\n LEFT JOIN Pa_Dimensao Dimens ON Dimens.Id = PlanEstrategy.Dimensao_Id" +
+            "\n LEFT JOIN Pa_Gerencia Gere ON Gere.Id = PlanTatico.Gerencia_Id" +
+            "\n LEFT JOIN Pa_Coordenacao Coord ON Coord.Id = PlanTatico.Coordenacao_Id" +
+            "\n WHERE Acao.AddDate BETWEEN ('" + dtInit + "') AND('" + dtFim + "')";
 
             dynamic results = QueryNinja(db, query);
 
             return results;
-            //return retorno;
+        }
+
+        private void GetParamsPeloFiltro(JObject filtro, out string dtInit, out string dtFim)
+        {
+            dynamic filtroObj = filtro;
+            string startDate = filtroObj.startDate;
+            string endDate = filtroObj.endDate;
+            dtInit = Guard.ParseDateToSqlV2(startDate, Guard.CultureCurrent.BR).ToString("yyyy-MM-dd 00:00:00");
+            dtFim = Guard.ParseDateToSqlV2(endDate, Guard.CultureCurrent.BR).ToString("yyyy-MM-dd 23:59:59");
         }
 
         //Retorna apenas Series e Dados
         [HttpPost]
         [Route("GraficoPie")]
-        public List<JObject> GraficoPie([FromBody] filtros filtro)
+        public List<JObject> GraficoPie(JObject filtro)
         {
-            var dataInicio = Guard.ParseDateToSqlV2(filtro.dataInicio, Guard.CultureCurrent.BR).ToString("yyyyMMdd");
-            var dataFim = Guard.ParseDateToSqlV2(filtro.dataFim, Guard.CultureCurrent.BR).ToString("yyyyMMdd");
+            string dtInit;
+            string dtFim;
+            GetParamsPeloFiltro(filtro, out dtInit, out dtFim);
+
             var query = "SELECT COUNT(s.id) AS y, s.Name AS name" +
                         "\n FROM pa_acao acao INNER JOIN pa_status s ON acao.Status = s.Id" +
-                        "\n WHERE acao.AddDate BETWEEN ('" + dataInicio + " 00:00:00') AND('" + dataFim + "  23:59:59')" +
+                        "\n WHERE acao.AddDate BETWEEN ('" + dtInit + "') AND('" + dtFim + "')" +
                         "\n GROUP BY s.Id, s.Name";
             dynamic resultPie = QueryNinja(db, query);
 
