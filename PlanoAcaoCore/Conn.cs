8da0e418930bd9737ecfea1d985ce40b6a1db023
Note-pc <Note-pc@NOTE>
2017-06-23 16:44:00 -0300

    PA alterações e grafico indicadores.

--- PlanoAcaoCore/Conn.cs
+++ PlanoAcaoCore/Conn.cs
@@ -2,17 +2,26 @@
 {
     public static class Conn
     {
-        ////Local
+        //CONFIG COMMUN PARA TODOS
+        public static int sessionTimer = 60;
+        public static bool isSgqIntegrado = true;
+
+
+        //Config para dev SRT
+        //Local Db
         //public static string catalog { get { return "PlanoDeAcao"; } }
         //public static string dataSource { get { return @"SERVERGRT\MSSQLSERVER2014"; } }
         //public static string user { get { return "sa"; } }
         //public static string pass { get { return "1qazmko0"; } }
+
+        //Remoto SGQ
         //public static string catalog2 { get { return "dbGQualidade_JBS"; } }
         //public static string dataSource2 { get { return @"SERVERGRT\MSSQLSERVER2014"; } }
         //public static string user2 { get { return "sa"; } }
         //public static string pass2 { get { return "1qazmko0"; } }
 
-        //public static string selfRoot { get { return "http://localhost/PlanoDeAcaoMVC"; } }
+        //public static string selfRoot { get { return "http://192.168.25.200/PlanoAcao/"; } }
+        //public static string SgqHost { get { return "http://192.168.25.200/sgqbr/api/User/AuthenticationLogin"; } }
 
         //public static string emailFrom = "celsogea@hotmail.com";
         //public static string emailPass = "Thebost1";
@@ -20,17 +29,22 @@
         //public static int emailPort = 587;
         //public static bool emailSSL = true;
 
+
         ////Azure
+        //Local DB
         //public static string catalog { get { return "PlanoDeAcao"; } }
         //public static string dataSource { get { return @"servergrtpa.database.windows.net"; } }
         //public static string user { get { return "grt"; } }
         //public static string pass { get { return "1qazmko0#"; } }
+
+        //Remoto SGQ
         //public static string catalog2 { get { return "SGQ_YTOARA"; } }
         //public static string dataSource2 { get { return @"servergrt.database.windows.net"; } }
         //public static string user2 { get { return "grt"; } }
         //public static string pass2 { get { return "1qazmko0#"; } }
 
         //public static string selfRoot { get { return "http://server20129141.cloudapp.net/PlanoDeAcao"; } }
+        //public static string SgqHost { get { return "http://server20129141.cloudapp.net"; } }
 
         //public static string emailFrom = "celsogea@hotmail.com";
         //public static string emailPass = "Thebost1";
@@ -51,18 +65,20 @@
         //public static string pass2 { get { return "Mi3UpU0J35<_"; } }
 
         //Utilizar para JBS
-        //Remoto
+        //DB Local
         public static string catalog { get { return "dbGQualidadeTeste"; } }
         public static string dataSource { get { return @"10.255.0.41"; } }
         public static string user { get { return "UserGQualidade"; } }
         public static string pass { get { return "grJsoluco3s"; } }
-        //Remoto
+        //Remoto SGQ
         public static string catalog2 { get { return "dbGQualidade"; } }
         public static string dataSource2 { get { return @"10.255.0.16"; } }
         public static string user2 { get { return "UserGQualidade"; } }
         public static string pass2 { get { return "grJsoluco3s"; } }
 
         public static string selfRoot { get { return "http://mtzsvmqsc/PlanoDeAcao/"; } }
+        public static string SgqHost { get { return "http://mtzsvmqsc/sgq/api/User/AuthenticationLogin"; } }
+
 
         public static string emailFrom = "sgq@jbs.com.br";
         public static string emailPass = "B7pwGJD44SbY";
--- PlanoDeAcaoMVC/Controllers/Api/ApiRelatoriosController.cs
+++ PlanoDeAcaoMVC/Controllers/Api/ApiRelatoriosController.cs
@@ -1,6 +1,10 @@
-﻿using DTO.Helpers;
+﻿using ADOFactory;
+using DTO.Helpers;
 using Newtonsoft.Json.Linq;
+using PlanoAcaoCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 
 namespace PlanoDeAcaoMVC.Controllers.Api
@@ -151,6 +155,83 @@ namespace PlanoDeAcaoMVC.Controllers.Api
             var items = QueryNinja(db, query);
 
             return items;
+        }
+        [HttpPost]
+        [Route("GetGraficoSgq")]
+        public List<JObject> GetGraficoSgq(JObject filtro)
+        {
+            string dtInit;
+            string dtFim;
+            GetParamsPeloFiltro(filtro, out dtInit, out dtFim);
+
+            var query = "SELECT Acao.Id,"+
+            "\n Acao.Level1Id, "+
+            "\n Acao.Level2Id, "+
+            "\n Acao.Level3Id, "+
+            "\nSta.Name AS[Status]," +
+            "\n  ISNULL(Quem.Name, 'Não possui Responsável') AS Responsavel," +
+            "\n  ISNULL(ContraMedGen.ContramedidaGenerica, 'Não possui Contramedida Genérica') AS 'Contramedida Genérica'," +
+            "\n  ISNULL(CausaGen.CausaGenerica, 'Não possui Causa Genérica') AS 'Causa Genérica'," +
+            "\n  ISNULL(GrpCausa.GrupoCausa, 'Não possui Grupo Causa') AS 'Grupo Causa'," +
+            "\n  ISNULL(Un.Description, 'Corporativo') AS 'Unidade'," +
+            "\n  ISNULL(IndicDir.Name, 'Não possui Indicadores Diretriz') AS 'Indicadores Diretriz'," +
+            "\n  ISNULL(DiretObj.Name, 'Não possui Diretriz / Objetivo') AS 'Diretriz / Objetivo'," +
+            "\n  ISNULL(Diretor.Name, 'Não possui Diretoria') AS 'Diretoria'," +
+            "\n  ISNULL(Dimens.Name, 'Não possui Dimensão') AS 'Dimensão'," +
+            "\n  ISNULL(Gere.Name, 'Não possui Gerência') AS 'Gerência'," +
+            "\n  ISNULL(Coord.Name, 'Não possui Coordenação') AS 'Coordenação'" +
+            "\n FROM Pa_Acao AS Acao" +
+            "\n LEFT JOIN Pa_Status Sta ON Sta.Id = Acao.Status" +
+            "\n LEFT JOIN Pa_Quem Quem ON Quem.Id = Acao.Quem_Id" +
+            "\n LEFT JOIN Pa_ContramedidaGenerica ContraMedGen ON ContraMedGen.Id = Acao.ContramedidaGenerica_Id" +
+            "\n LEFT JOIN Pa_CausaGenerica CausaGen ON CausaGen.Id = Acao.CausaGenerica_Id" +
+            "\n LEFT JOIN Pa_GrupoCausa GrpCausa ON GrpCausa.Id = Acao.GrupoCausa_Id" +
+            "\n LEFT JOIN Pa_Unidade Un ON Un.Id = Acao.Unidade_Id" +
+            "\n LEFT JOIN Pa_Planejamento PlanTatico ON PlanTatico.Id = Acao.Panejamento_Id" +
+            "\n LEFT JOIN Pa_Planejamento PlanEstrategy ON PlanEstrategy.Id = PlanTatico.Estrategico_Id" +
+            "\n LEFT JOIN Pa_IndicadoresDiretriz IndicDir ON IndicDir.Id = PlanEstrategy.IndicadoresDiretriz_Id" +
+            "\n LEFT JOIN Pa_Objetivo DiretObj ON DiretObj.Id = PlanEstrategy.Objetivo_Id" +
+            "\n LEFT JOIN Pa_Diretoria Diretor ON Diretor.Id = PlanEstrategy.Diretoria_Id" +
+            "\n LEFT JOIN Pa_Dimensao Dimens ON Dimens.Id = PlanEstrategy.Dimensao_Id" +
+            "\n LEFT JOIN Pa_Gerencia Gere ON Gere.Id = PlanTatico.Gerencia_Id" +
+            "\n LEFT JOIN Pa_Coordenacao Coord ON Coord.Id = PlanTatico.Coordenacao_Id" +
+            "\n WHERE Acao.AddDate BETWEEN ('" + dtInit + "') AND('" + dtFim + "') AND Acao.TipoIndicador = 2";
+
+
+            var results = QueryNinja(db, query);
+
+
+
+
+
+            var parLevel1Ids = results.Select(r => r.GetValue("Level1Id").ToString()).Distinct().ToList();
+            var parLevel2Ids = results.Select(r => r.GetValue("Level2Id").ToString()).Distinct().ToList();
+            var parLevel3Ids = results.Select(r => r.GetValue("Level3Id").ToString()).Distinct().ToList();
+            var level1ids =  String.Join(", ", parLevel1Ids.ToArray());
+            var level2ids =  String.Join(", ", parLevel2Ids.ToArray());
+            var level3ids =  String.Join(", ", parLevel3Ids.ToArray());
+         
+            //foreach (var i in )
+            //{
+
+            //    parLevel1Ids += i.GetValue("Level1Id") + ",";
+            //   // parLevel2Ids = i.Level2Id + ",";
+            //    //parLevel3Ids = i.Level3Id + ",";
+            //}
+            //var result = String.Join(", ", names.ToArray());
+
+           var level1 = QueryNinjaPeloDbSgq("SELECT Id, Name FROM ParLevel1 WHERE Id in("+ level1ids + ")");
+           var level2 = QueryNinjaPeloDbSgq("SELECT Id, Name FROM ParLevel2 WHERE Id in("+ level2ids + ")");
+           var level3 = QueryNinjaPeloDbSgq("SELECT Id, Name FROM ParLevel3 WHERE Id in("+ level3ids + ")");
+
+            foreach (var i in results)
+            {
+                i["Level1Name"] = level1.FirstOrDefault(r => i.GetValue("Level1Id").Equals(r.GetValue("Id"))).GetValue("Name");
+                i["Level2Name"] = level2.FirstOrDefault(r => i.GetValue("Level2Id").Equals(r.GetValue("Id"))).GetValue("Name");
+                i["Level3Name"] = level3.FirstOrDefault(r => i.GetValue("Level3Id").Equals(r.GetValue("Id"))).GetValue("Name");
+            }
+
+            return results;
         }
 
     }
