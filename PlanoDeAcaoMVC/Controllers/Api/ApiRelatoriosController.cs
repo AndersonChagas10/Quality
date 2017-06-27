c4a6fac98d7c17d4e78e4bcd58af7f7105bd78da
Note-pc <Note-pc@NOTE>
2017-06-27 09:38:20 -0300

    Alterações PA e comentarios FTA.

--- PlanoDeAcaoMVC/Controllers/Api/ApiRelatoriosController.cs
+++ PlanoDeAcaoMVC/Controllers/Api/ApiRelatoriosController.cs
@@ -12,7 +12,6 @@ namespace PlanoDeAcaoMVC.Controllers.Api
     [RoutePrefix("api/Relatorios")]
     public class ApiRelatoriosController : BaseApiController
     {
-
         private PlanoAcaoEF.PlanoDeAcaoEntities db;
 
         public ApiRelatoriosController()
@@ -21,9 +20,7 @@ namespace PlanoDeAcaoMVC.Controllers.Api
             db.Configuration.LazyLoadingEnabled = false;
             db.Configuration.AutoDetectChangesEnabled = false;
         }
-        
 
-        //Retorna Categorias Series e Dados
         [HttpPost]
         [Route("GetGrafico")]
         public List<JObject> GetGrafico(JObject filtro)
@@ -32,7 +29,9 @@ namespace PlanoDeAcaoMVC.Controllers.Api
             string dtFim;
             GetParamsPeloFiltro(filtro, out dtInit, out dtFim);
 
-            var query = "SELECT Acao.Id, Sta.Name AS[Status]," +
+            var query = "SELECT Acao.Id," +
+            "\n CAST(Acao.QuandoFim As Date) as QuandoFim," +
+            "\n Sta.Name AS[Status]," +
             "\n  ISNULL(Quem.Name, 'Não possui Responsável') AS Responsavel," +
             "\n  ISNULL(ContraMedGen.ContramedidaGenerica, 'Não possui Contramedida Genérica') AS 'Contramedida Genérica'," +
             "\n  ISNULL(CausaGen.CausaGenerica, 'Não possui Causa Genérica') AS 'Causa Genérica'," +
@@ -61,21 +60,11 @@ namespace PlanoDeAcaoMVC.Controllers.Api
             "\n LEFT JOIN Pa_Coordenacao Coord ON Coord.Id = PlanTatico.Coordenacao_Id" +
             "\n WHERE Acao.AddDate BETWEEN ('" + dtInit + "') AND('" + dtFim + "')";
 
-            dynamic results = QueryNinja(db, query);
+            var results = QueryNinja(db, query);
 
             return results;
         }
 
-        private void GetParamsPeloFiltro(JObject filtro, out string dtInit, out string dtFim)
-        {
-            dynamic filtroObj = filtro;
-            string startDate = filtroObj.startDate;
-            string endDate = filtroObj.endDate;
-            dtInit = Guard.ParseDateToSqlV2(startDate, Guard.CultureCurrent.BR).ToString("yyyy-MM-dd 00:00:00");
-            dtFim = Guard.ParseDateToSqlV2(endDate, Guard.CultureCurrent.BR).ToString("yyyy-MM-dd 23:59:59");
-        }
-
-        //Retorna apenas Series e Dados
         [HttpPost]
         [Route("GraficoPie")]
         public List<JObject> GraficoPie(JObject filtro)
@@ -100,7 +89,8 @@ namespace PlanoDeAcaoMVC.Controllers.Api
             dynamic teste = form;
 
             var query = "SELECT DATEPART(mm,QuandoInicio) as Mes, Count(id) as Quantidade FROM [Pa_Acao] " +
-                //"\n where [Status] not in (4,3) "+
+                 //"\n where [Status] not in (4,3) "+
+                 "\n WHERE QuandoInicio > '" + DateTime.Now.AddMonths(-6).ToString("yyyy-MM-dd 00:00:00") + "'" +
                 "\n group by  DATEPART(mm,QuandoInicio)";
 
             var items = QueryNinja(db, query);
@@ -118,6 +108,7 @@ namespace PlanoDeAcaoMVC.Controllers.Api
                 "\n Count(id) as Quantidade " +
                 "\n FROM  [Pa_Acao] " +
                 "\n where [Status] in (4,3) " +
+                 "\n AND QuandoInicio > '" + DateTime.Now.AddMonths(-6).ToString("yyyy-MM-dd 00:00:00") + "'" +
                 "\n group by  DATEPART(mm,QuandoInicio)";
 
             var items = QueryNinja(db, query);
@@ -134,7 +125,9 @@ namespace PlanoDeAcaoMVC.Controllers.Api
             var query = "SELECT A.*, B.MesConcluidas, IsNull(B.QuantidadeConcluidas, 0) as QuantidadeConcluidas, (A.QuantidadeIniciadas - IsNull(B.QuantidadeConcluidas, 0)) as Acc" +
                         "\n FROM" +
                         "\n (SELECT DATEPART(mm, QuandoInicio) as MesIniciadas, Count(id) as QuantidadeIniciadas FROM [Pa_Acao]  group by  DATEPART(mm, QuandoInicio)) A" +
-                        "\n LEFT JOIN(SELECT DATEPART(mm, QuandoInicio) as MesConcluidas, Count(id) as QuantidadeConcluidas FROM [Pa_Acao] where[Status] in (4, 3)  group by  DATEPART(mm, QuandoInicio)) B on A.MesIniciadas = B.MesConcluidas";
+                        "\n LEFT JOIN(SELECT DATEPART(mm, QuandoInicio) as MesConcluidas, Count(id) as QuantidadeConcluidas FROM [Pa_Acao] where[Status] in (4, 3) " +
+                         "\n AND QuandoInicio > '" + DateTime.Now.AddMonths(-6).ToString("yyyy-MM-dd 00:00:00") + "'" +
+                        "group by  DATEPART(mm, QuandoInicio)) B on A.MesIniciadas = B.MesConcluidas";
 
             var items = QueryNinja(db, query);
 
@@ -150,7 +143,9 @@ namespace PlanoDeAcaoMVC.Controllers.Api
             var query = "SELECT A.*, B.MesConcluidas, IsNull(B.QuantidadeConcluidas, 0) as QuantidadeConcluidas, (A.QuantidadeIniciadas - IsNull(B.QuantidadeConcluidas, 0)) as Acc, 20 as Meta" +
                         "\n FROM" +
                         "\n (SELECT DATEPART(mm, QuandoInicio) as MesIniciadas, Count(id) as QuantidadeIniciadas FROM [Pa_Acao]  group by  DATEPART(mm, QuandoInicio)) A" +
-                        "\n LEFT JOIN(SELECT DATEPART(mm, QuandoInicio) as MesConcluidas, Count(id) as QuantidadeConcluidas FROM [Pa_Acao] where[Status] in (4, 3)  group by  DATEPART(mm, QuandoInicio)) B on A.MesIniciadas = B.MesConcluidas";
+                        "\n LEFT JOIN(SELECT DATEPART(mm, QuandoInicio) as MesConcluidas, Count(id) as QuantidadeConcluidas FROM [Pa_Acao] where[Status] in (4, 3) " +
+                          "\n AND QuandoInicio > '" + DateTime.Now.AddMonths(-6).ToString("yyyy-MM-dd 00:00:00") + "'" +
+                         "\n group by  DATEPART(mm, QuandoInicio)) B on A.MesIniciadas = B.MesConcluidas";
 
             var items = QueryNinja(db, query);
 
@@ -164,10 +159,11 @@ namespace PlanoDeAcaoMVC.Controllers.Api
             string dtFim;
             GetParamsPeloFiltro(filtro, out dtInit, out dtFim);
 
-            var query = "SELECT Acao.Id,"+
-            "\n Acao.Level1Id, "+
-            "\n Acao.Level2Id, "+
-            "\n Acao.Level3Id, "+
+            var query = "SELECT Acao.Id," +
+                 "\n CAST(Acao.QuandoFim As Date) as QuandoFim," +
+            "\n Acao.Level1Id, " +
+            "\n Acao.Level2Id, " +
+            "\n Acao.Level3Id, " +
             "\nSta.Name AS[Status]," +
             "\n  ISNULL(Quem.Name, 'Não possui Responsável') AS Responsavel," +
             "\n  ISNULL(ContraMedGen.ContramedidaGenerica, 'Não possui Contramedida Genérica') AS 'Contramedida Genérica'," +
@@ -202,84 +198,46 @@ namespace PlanoDeAcaoMVC.Controllers.Api
 
 
 
-
-
-            var parLevel1Ids = results.Select(r => r.GetValue("Level1Id").ToString()).Distinct().ToList();
-            var parLevel2Ids = results.Select(r => r.GetValue("Level2Id").ToString()).Distinct().ToList();
-            var parLevel3Ids = results.Select(r => r.GetValue("Level3Id").ToString()).Distinct().ToList();
-            var level1ids =  String.Join(", ", parLevel1Ids.ToArray());
-            var level2ids =  String.Join(", ", parLevel2Ids.ToArray());
-            var level3ids =  String.Join(", ", parLevel3Ids.ToArray());
-         
-            //foreach (var i in )
-            //{
-
-            //    parLevel1Ids += i.GetValue("Level1Id") + ",";
-            //   // parLevel2Ids = i.Level2Id + ",";
-            //    //parLevel3Ids = i.Level3Id + ",";
-            //}
-            //var result = String.Join(", ", names.ToArray());
-
-           var level1 = QueryNinjaPeloDbSgq("SELECT Id, Name FROM ParLevel1 WHERE Id in("+ level1ids + ")");
-           var level2 = QueryNinjaPeloDbSgq("SELECT Id, Name FROM ParLevel2 WHERE Id in("+ level2ids + ")");
-           var level3 = QueryNinjaPeloDbSgq("SELECT Id, Name FROM ParLevel3 WHERE Id in("+ level3ids + ")");
-
-            foreach (var i in results)
+            if (results.Count > 0)
             {
-                i["Level1Name"] = level1.FirstOrDefault(r => i.GetValue("Level1Id").Equals(r.GetValue("Id"))).GetValue("Name");
-                i["Level2Name"] = level2.FirstOrDefault(r => i.GetValue("Level2Id").Equals(r.GetValue("Id"))).GetValue("Name");
-                i["Level3Name"] = level3.FirstOrDefault(r => i.GetValue("Level3Id").Equals(r.GetValue("Id"))).GetValue("Name");
+                var parLevel1Ids = results.Select(r => r.GetValue("Level1Id").ToString()).Distinct().ToList();
+                var parLevel2Ids = results.Select(r => r.GetValue("Level2Id").ToString()).Distinct().ToList();
+                var parLevel3Ids = results.Select(r => r.GetValue("Level3Id").ToString()).Distinct().ToList();
+                var level1ids = String.Join(", ", parLevel1Ids.ToArray());
+                var level2ids = String.Join(", ", parLevel2Ids.ToArray());
+                var level3ids = String.Join(", ", parLevel3Ids.ToArray());
+
+                //foreach (var i in )
+                //{
+
+                //    parLevel1Ids += i.GetValue("Level1Id") + ",";
+                //   // parLevel2Ids = i.Level2Id + ",";
+                //    //parLevel3Ids = i.Level3Id + ",";
+                //}
+                //var result = String.Join(", ", names.ToArray());
+
+                var level1 = QueryNinjaPeloDbSgq("SELECT Id, Name FROM ParLevel1 WHERE Id in(" + level1ids + ")");
+                var level2 = QueryNinjaPeloDbSgq("SELECT Id, Name FROM ParLevel2 WHERE Id in(" + level2ids + ")");
+                var level3 = QueryNinjaPeloDbSgq("SELECT Id, Name FROM ParLevel3 WHERE Id in(" + level3ids + ")");
+
+                foreach (var i in results)
+                {
+                    i["Indicador"] = level1.FirstOrDefault(r => i.GetValue("Level1Id").Equals(r.GetValue("Id"))).GetValue("Name");
+                    i["Monitoramento"] = level2.FirstOrDefault(r => i.GetValue("Level2Id").Equals(r.GetValue("Id"))).GetValue("Name");
+                    i["Tarefa"] = level3.FirstOrDefault(r => i.GetValue("Level3Id").Equals(r.GetValue("Id"))).GetValue("Name");
+                }
             }
 
             return results;
         }
 
+        private void GetParamsPeloFiltro(JObject filtro, out string dtInit, out string dtFim)
+        {
+            dynamic filtroObj = filtro;
+            string startDate = filtroObj.startDate;
+            string endDate = filtroObj.endDate;
+            dtInit = Guard.ParseDateToSqlV2(startDate, Guard.CultureCurrent.BR).ToString("yyyy-MM-dd 00:00:00");
+            dtFim = Guard.ParseDateToSqlV2(endDate, Guard.CultureCurrent.BR).ToString("yyyy-MM-dd 23:59:59");
+        }
     }
-
-    public class GraficoPieSet
-    {
-        public string name { get; set; }
-        public int y { get; set; }
-    }
-
-    public class RetornoInt
-    {
-        public int valor { get; set; }
-    }
-
-    public class RetornoGrafico1
-    {
-        public string Indicador { get; set; }
-        public int Indicador_Id { get; set; }
-        public List<Status> Status { get; set; }
-    }
-
-    public class Status
-    {
-        public string Name { get; set; }
-        public int Id { get; set; }
-        public List<int> QuantidadeAcoes { get; set; }
-    }
-
-    /// <summary>
-    /// Json : 
-    /// { 
-    ///     diretoria: 1,
-    ///     gerencia: 2,
-    ///     dataInicio: '20/01/2017'
-    ///     dataFim: '20/01/2017'
-    /// }
-    /// </summary>
-    public class filtros
-    {
-        public int diretoria { get; set; }
-        public int gerencia { get; set; }
-        public string dataInicio { get; set; }
-        public string dataFim { get; set; }
-        public string categoria { get; set; }
-        public string serie { get; set; }
-        public string[] filtroCategoria { get; set; }
-        public int relatorio { get; set; }
-    }
-
 }
