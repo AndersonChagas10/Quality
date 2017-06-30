45fbe3902613f57610994bd8e7f3e59636b29783
wesle <wesle@DESKTOP-T3K263B>
2017-06-30 11:25:08 -0300

    ppull

--- SgqSystem/Controllers/Api/NewSync/NewSyncController.cs
+++ SgqSystem/Controllers/Api/NewSync/NewSyncController.cs
@@ -27,73 +27,6 @@ namespace SgqSystem.Controllers.Api.NewSync
             _basedomain = basedomain;
         }
 
-        //[HttpGet]
-        //[Route("getParams")]
-        //public ParamsVinculos getParams()
-        //{
-        //    var retorno = new ParamsVinculos();
-        //    #region level1
-        //    string query = "select pl1.id as level1,pl2.id as level2, pl3.id as level3 from ParLevel3Level2Level1 Pl3L2L1 LEFT JOIN ParLevel1 Pl1 on Pl3L2L1.ParLevel1_Id = Pl1.Id "+
-        //                   "LEFT JOIN ParLevel3Level2 Pl3L2 on Pl3L2L1.ParLevel3Level2_Id = Pl3L2.Id LEFT JOIN ParLevel2 Pl2 on Pl3L2.ParLevel2_Id = Pl2.Id "+
-        //                   "LEFT JOIN ParLevel3 Pl3 on Pl3L2.ParLevel3_Id = Pl3.Id where pl1.IsActive = 1 and pl2.IsActive = 1 and pl3.IsActive = 1";
-        //    using (var db = new SgqDbDevEntities())
-        //    {
-        //        listaL1 = db.Database.SqlQuery<ParLevel1>(query).ToList();
-
-        //        query = "select distinct(pl2.id) as codigol2, pl2.* from ParLevel3Level2Level1 Pl3L2L1 LEFT JOIN ParLevel1 Pl1 on Pl3L2L1.ParLevel1_Id = Pl1.Id " +
-        //                "LEFT JOIN ParLevel3Level2 Pl3L2 on Pl3L2L1.ParLevel3Level2_Id = Pl3L2.Id LEFT JOIN ParLevel2 Pl2 on Pl3L2.ParLevel2_Id = Pl2.Id " +
-        //                "LEFT JOIN ParLevel3 Pl3 on Pl3L2.ParLevel3_Id = Pl3.Id where pl1.IsActive = 1 and pl2.IsActive = 1";
-        //        listaL2 = db.Database.SqlQuery<ParLevel2>(query).ToList();
-
-        //        query = "select distinct(pl3.id) as codigol3, pl3.* from ParLevel3Level2Level1 Pl3L2L1 LEFT JOIN ParLevel1 Pl1 on Pl3L2L1.ParLevel1_Id = Pl1.Id " +
-        //                    "LEFT JOIN ParLevel3Level2 Pl3L2 on Pl3L2L1.ParLevel3Level2_Id = Pl3L2.Id LEFT JOIN ParLevel2 Pl2 on Pl3L2.ParLevel2_Id = Pl2.Id " +
-        //                    "LEFT JOIN ParLevel3 Pl3 on Pl3L2.ParLevel3_Id = Pl3.Id where pl1.IsActive = 1 and pl2.IsActive = 1 and pl3.IsActive = 1 ";
-        //    }
-        //    #endregion
-        //    var count = 0;
-        //    #region level2
-
-        //    retorno.listaDeLevel1 = new List<Level1Params>();
-        //    foreach (var item in listaL1)
-        //    {
-        //        var it = new Level1Params();
-        //        it.level1 = item;
-
-        //        query = "select distinct(pl2.id) as codigol2, pl2.* from ParLevel3Level2Level1 Pl3L2L1 LEFT JOIN ParLevel1 Pl1 on Pl3L2L1.ParLevel1_Id = Pl1.Id " +
-        //                "LEFT JOIN ParLevel3Level2 Pl3L2 on Pl3L2L1.ParLevel3Level2_Id = Pl3L2.Id LEFT JOIN ParLevel2 Pl2 on Pl3L2.ParLevel2_Id = Pl2.Id " +
-        //                "LEFT JOIN ParLevel3 Pl3 on Pl3L2.ParLevel3_Id = Pl3.Id where pl1.IsActive = 1 and pl2.IsActive = 1 and pl1.Id = "+item.Id;
-
-        //        using (var db = new SgqDbDevEntities())
-        //        {
-        //            listaL2 = db.Database.SqlQuery<ParLevel2>(query).ToList();
-
-        //        }
-        //        it.listaDeLevel2 = new List<Level2Params>();
-        //        foreach (var i in listaL2)
-        //        {
-        //            var itL2 = new Level2Params();
-        //            itL2.level2 = i;
-        //            it.listaDeLevel2.Add(itL2);
-        //            query = "select distinct(pl3.id) as codigol3, pl3.* from ParLevel3Level2Level1 Pl3L2L1 LEFT JOIN ParLevel1 Pl1 on Pl3L2L1.ParLevel1_Id = Pl1.Id " +
-        //                    "LEFT JOIN ParLevel3Level2 Pl3L2 on Pl3L2L1.ParLevel3Level2_Id = Pl3L2.Id LEFT JOIN ParLevel2 Pl2 on Pl3L2.ParLevel2_Id = Pl2.Id " +
-        //                    "LEFT JOIN ParLevel3 Pl3 on Pl3L2.ParLevel3_Id = Pl3.Id where pl1.IsActive = 1 and pl2.IsActive = 1 and pl3.IsActive = 1 and pl1.id = " + item.Id
-        //                    + " and pl2.Id = " + i.Id;
-        //            using (var db = new SgqDbDevEntities())
-        //            {
-
-        //            }
-        //                foreach (var tarefa in )
-        //        }
-        //        retorno.listaDeLevel1.Add(it);
-
-        //        count++;
-        //    }
-        //    #endregion
-
-
-        //    return retorno;
-        //}
-
         [HttpGet]
         [Route("getParams")]
         public ParamsVinculos getParams()
@@ -142,6 +75,30 @@ namespace SgqSystem.Controllers.Api.NewSync
                 return obj;
             }
         }
+
+        [HttpGet]
+        [Route("GetConsolidationLevel1")]
+        public ConsolidacaoReturn getConsolidationLevel1(int unidade, string data)
+        {
+            using (var db = new SgqDbDevEntities())
+            {
+                db.Configuration.LazyLoadingEnabled = false;
+                db.Configuration.ProxyCreationEnabled = false;
+                ConsolidacaoReturn consolidacao = new ConsolidacaoReturn();
+                consolidacao.consolidationLevel1 = db.ConsolidationLevel1.Where(r => r.UnitId == unidade).ToList();
+                //consolidacao.consolidationLevel2 = db.ConsolidationLevel2.Where(r => r.UnitId == unidade).ToList();
+                string queryConsolidationL2 = " select cdl2.* from consolidationlevel2 cdl2 where cast(cdl2.ConsolidationDate as date) = '"+data+"' and cdl2.UnitId = "+unidade;
+                consolidacao.consolidationLevel2 = db.Database.SqlQuery<ConsolidationLevel2>(queryConsolidationL2).ToList();
+                string queryCollection = "select * from collectionlevel2 cl2 inner join consolidationlevel2 cdl2 on cdl2.id = cl2.ConsolidationLevel2_Id "+
+                                         "\n where cast(cl2.CollectionDate as date) = '"+data+"' and cdl2.UnitId = "+unidade;
+                consolidacao.collectionLevel2 = db.Database.SqlQuery<CollectionLevel2>(queryCollection).ToList();
+                string query = "select rl3.* from result_level3 rl3 inner join collectionlevel2 cl2 on cl2.id = rl3.collectionlevel2_id "+
+                                            "\n inner join consolidationlevel2 cdl2 on cdl2.id = cl2.ConsolidationLevel2_Id "+
+                                            "\n where cast(cl2.CollectionDate as date) = '"+ data + "' and cdl2.UnitId =" + unidade;
+                consolidacao.result_Level3 = db.Database.SqlQuery<Result_Level3>(query).ToList();
+                return consolidacao;  
+             }
+        }
     }
 
     public class ParamsVinculos
@@ -157,5 +114,12 @@ namespace SgqSystem.Controllers.Api.NewSync
     {
         public ParLevel2 level2 { get; set; }
         public List<ParLevel3> listaDelevel3 { get; set; }
+    }
+    public class ConsolidacaoReturn
+    {
+        public List<ConsolidationLevel1> consolidationLevel1 { get; set; }
+        public List<ConsolidationLevel2> consolidationLevel2 { get; set; }
+        public List<CollectionLevel2> collectionLevel2 { get; set; }
+        public List<Result_Level3> result_Level3 { get; set; }
     }
 }
