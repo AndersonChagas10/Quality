930f6c5ea49d12d60b2e16f8da162d3b3c35bb2e
Note-pc <Note-pc@NOTE>
2017-07-26 16:07:11 -0300

    Correção NC para rel Não conformidade

--- PlanoAcaoCore/Conn.cs
+++ PlanoAcaoCore/Conn.cs
@@ -5,7 +5,7 @@
         //CONFIG COMMUN PARA TODOS
         public static int sessionTimer = 60;
         public static bool isSgqIntegrado = true;
-        public static bool visaoOperacional = true;
+        public static bool visaoOperacional = false;
         public static string nameCausaEspecifica { get { return !visaoOperacional ? "Causa Especifica" : "Assunto"; } }
         public static string nameComoPontosImportantes { get { return !visaoOperacional ? "Como Pontos Importantes" : "Como"; } }
         public static string nameContramedidaEspecifica { get { return !visaoOperacional ? "Contramedida Específica" : "O que"; } }
@@ -13,27 +13,27 @@
         public static string TitileMailNovaAcao = "Plano de Ação - Nova Ação.";
         public static string TitileMailNovoFTA = "Plano de Ação - Novo Relatório de Análise de Desvio criado.";
 
-        //Config para dev GRT
-        //Local Db
-        public static string catalog { get { return "PlanoDeAcao"; } }
-        public static string dataSource { get { return @"SERVERGRT\MSSQLSERVER2014"; } }
-        public static string user { get { return "sa"; } }
-        public static string pass { get { return "1qazmko0"; } }
+        ////Config para dev GRT
+        ////Local Db
+        //public static string catalog { get { return "PlanoDeAcao"; } }
+        //public static string dataSource { get { return @"SERVERGRT\MSSQLSERVER2014"; } }
+        //public static string user { get { return "sa"; } }
+        //public static string pass { get { return "1qazmko0"; } }
 
-        //Remoto SGQ
-        public static string catalog2 { get { return "dbGQualidade_JBS"; } }
-        public static string dataSource2 { get { return @"SERVERGRT\MSSQLSERVER2014"; } }
-        public static string user2 { get { return "sa"; } }
-        public static string pass2 { get { return "1qazmko0"; } }
+        ////Remoto SGQ
+        //public static string catalog2 { get { return "dbGQualidade_JBS"; } }
+        //public static string dataSource2 { get { return @"SERVERGRT\MSSQLSERVER2014"; } }
+        //public static string user2 { get { return "sa"; } }
+        //public static string pass2 { get { return "1qazmko0"; } }
 
-        public static string selfRoot { get { return "http://192.168.25.200/PlanoAcao/"; } }
-        public static string SgqHost { get { return "http://192.168.25.200/sgqbr/api/User/AuthenticationLogin"; } }
+        //public static string selfRoot { get { return "http://192.168.25.200/PlanoAcao/"; } }
+        //public static string SgqHost { get { return "http://192.168.25.200/sgqbr/api/User/AuthenticationLogin"; } }
 
-        public static string emailFrom = "celsogea@hotmail.com";
-        public static string emailPass = "tR48MJsfaz1Rf+dT+Ag8dQ==";
-        public static string emailSmtp = "smtp.live.com";
-        public static int emailPort = 587;
-        public static bool emailSSL = true;
+        //public static string emailFrom = "celsogea@hotmail.com";
+        //public static string emailPass = "tR48MJsfaz1Rf+dT+Ag8dQ==";
+        //public static string emailSmtp = "smtp.live.com";
+        //public static int emailPort = 587;
+        //public static bool emailSSL = true;
 
         ////Azure
         ////Local DB
@@ -90,25 +90,25 @@
         //public static string user2 { get { return "grjqualidadedev"; } }
         //public static string pass2 { get { return "Mi3UpU0J35<_"; } }
 
-        ////Utilizar para JBS
-        ////DB Local
-        //public static string catalog { get { return "dbGQualidadeTeste"; } }
-        //public static string dataSource { get { return @"10.255.0.41"; } }
-        //public static string user { get { return "UserGQualidade"; } }
-        //public static string pass { get { return "grJsoluco3s"; } }
-        ////Remoto SGQ
-        //public static string catalog2 { get { return "dbGQualidade"; } }
-        //public static string dataSource2 { get { return @"10.255.0.16"; } }
-        //public static string user2 { get { return "UserGQualidade"; } }
-        //public static string pass2 { get { return "grJsoluco3s"; } }
+        //Utilizar para JBS
+        //DB Local
+        public static string catalog { get { return "dbGQualidadeTeste"; } }
+        public static string dataSource { get { return @"10.255.0.41"; } }
+        public static string user { get { return "UserGQualidade"; } }
+        public static string pass { get { return "grJsoluco3s"; } }
+        //Remoto SGQ
+        public static string catalog2 { get { return "dbGQualidade"; } }
+        public static string dataSource2 { get { return @"10.255.5.93"; } }
+        public static string user2 { get { return "UserGQualidade"; } }
+        public static string pass2 { get { return "grJsoluco3s"; } }
 
-        //public static string selfRoot { get { return "http://mtzsvmqsc/PlanoDeAcao/"; } }
-        //public static string SgqHost { get { return "http://mtzsvmqsc/sgq/api/User/AuthenticationLogin"; } }
+        public static string selfRoot { get { return "http://mtzsvmqsc/PlanoDeAcao/"; } }
+        public static string SgqHost { get { return "http://mtzsvmqsc/sgq/api/User/AuthenticationLogin"; } }
 
-        //public static string emailFrom = "sgq@jbs.com.br";
-        //public static string emailPass = "Bvkw+iUcPJGZQe0tPxQDjg==";
-        //public static string emailSmtp = "correio.jbs.com.br";
-        //public static int emailPort = 587;
-        //public static bool emailSSL = false;
+        public static string emailFrom = "sgq@jbs.com.br";
+        public static string emailPass = "Bvkw+iUcPJGZQe0tPxQDjg==";
+        public static string emailSmtp = "correio.jbs.com.br";
+        public static int emailPort = 587;
+        public static bool emailSSL = false;
     }
 }
