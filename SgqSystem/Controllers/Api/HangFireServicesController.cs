10dc10e5e53248f74f4a821e54c23a20caa167ca
Note-pc <Note-pc@NOTE>
2017-08-01 18:51:15 -0300

    Mudanças no serviço do hangfire, chamando por HTTP agora.

---
+++ SgqSystem/Controllers/Api/HangFireServicesController.cs
@@ -0,0 +1,36 @@
+﻿using SgqSystem.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SgqSystem.Controllers.Api
+{
+    [RoutePrefix("api/hf")]
+    public class HangFireServicesController : ApiController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Reconsolidacao")]
+        public void Reconsolidacao()
+        {
+            SimpleAsynchronous.ResendProcessJson();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("SendMail")]
+        public void SendMail()
+        {
+            SimpleAsynchronous.SendMailFromDeviationSgqApp();
+        }
+    }
+}
