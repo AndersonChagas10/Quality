3088c028c996c762470ad3f3c3e4ad142ac33b23
Note-pc <Note-pc@NOTE>
2017-09-15 09:36:20 -0300

    Rel Migração.

---
+++ SgqSystem/Controllers/Auxiliares/EUADbController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SgqSystem.Controllers.Auxiliares
+{
+    public class EUADbController : Controller
+    {
+        // GET: EUADb
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
