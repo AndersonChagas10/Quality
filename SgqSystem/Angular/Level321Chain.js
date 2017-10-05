03d94201e03de1e231bb0e892e82fb863b282d30
Note-pc <Note-pc@NOTE>
2017-10-05 19:32:49 -0300

    Melhoria DDLs Level321 chain

--- SgqSystem/Angular/Level321Chain.js
+++ SgqSystem/Angular/Level321Chain.js
@@ -33,61 +33,63 @@
             });
 
             //Defining the $http service for getting Level2 By Level1
-            $scope.GetListLevel2VinculadoLevel1 = function () {
-                if ($scope.level1Value) {
+            $scope.GetNextDDL = function (elementValue, url, elementChainedName, urlParaOptionSelecione) {
+                if (elementValue) {
                     $http({
                         method: 'POST',
-                        url: GetListLevel2VinculadoLevel1 + "/" + $scope.level1Value,
-                        //data: JSON.stringify({ Id: $scope.level1Value })
+                        url: url + "/" + elementValue,
                     }).
                     then(function (r) {
-                        $scope.level2 = r.data;
+                        $scope[elementChainedName] = r.data;
                     });
                 }
                 else {
-                    $http({
-                        method: 'POST',
-                        url: GetListLevel2
-                    }).
-                   then(function (r) {
-                       $scope.level2 = r.data;
-                   });
+                    if (urlParaOptionSelecione) {
+                         $http({
+                             method: 'POST',
+                             url: urlParaOptionSelecione
+                         }).
+                        then(function (r) {
+                            $scope[elementChainedName] = r.data;
+                        });
+                    }
+                    $scope[elementChainedName] = [];
                 }
             }
 
-            $scope.GetListLevel3VinculadoLevel2 = function () {
-                //Defining the $http service for getting Level3 By Level2
-                if ($scope.level2Value && !$scope.level1Value) {
-                    $http({
-                        method: 'POST',
-                        url: GetListLevel3VinculadoLevel2 + "/" + $scope.level2Value,
-                        //data: JSON.stringify({ Id: $scope.level2Value })
-                    }).
-                    then(function (r) {
-                        $scope.level3 = r.data;
-                    });
-                }
-                    //Defining the $http service for getting Level3 By Level2 and Level1
-                else if  ($scope.level2Value && $scope.level1Value) {
-                    $http({
-                        method: 'POST',
-                        url: GetListLevel3VinculadoLevel2Level1 + "/" + $scope.level1Value + "/" + $scope.level2Value,
-                        //data: JSON.stringify({ Id:  } + { Id:  })
-                    }).
-                    then(function (r) {
-                        $scope.level3 = r.data;
-                    });
-                }
-                else {
-                    $http({
-                        method: 'POST',
-                        url: GetListLevel3
-                    }).
-                   then(function (r) {
-                       $scope.level3 = r.data;
-                   });
-                }
-            }
+            //$scope.GetListLevel3VinculadoLevel2 = function () {
+            //    //Defining the $http service for getting Level3 By Level2
+            //    if ($scope.level2Value && !$scope.level1Value) {
+            //        $http({
+            //            method: 'POST',
+            //            url: GetListLevel3VinculadoLevel2 + "/" + $scope.level2Value,
+            //            //data: JSON.stringify({ Id: $scope.level2Value })
+            //        }).
+            //        then(function (r) {
+            //            $scope.level3 = r.data;
+            //        });
+            //    }
+            //        //Defining the $http service for getting Level3 By Level2 and Level1
+            //    else if  ($scope.level2Value && $scope.level1Value) {
+            //        $http({
+            //            method: 'POST',
+            //            url: GetListLevel3VinculadoLevel2Level1 + "/" + $scope.level1Value + "/" + $scope.level2Value,
+            //            //data: JSON.stringify({ Id:  } + { Id:  })
+            //        }).
+            //        then(function (r) {
+            //            $scope.level3 = r.data;
+            //        });
+            //    }
+            //    else {
+            //        $http({
+            //            method: 'POST',
+            //            url: GetListLevel3
+            //        }).
+            //       then(function (r) {
+            //           $scope.level3 = r.data;
+            //       });
+            //    }
+            //}
         }]);
 
     /*
