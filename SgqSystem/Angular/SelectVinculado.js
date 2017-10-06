e7bd4f13982e3d9237b22b49d76dd297649cf299
renan <renan@ASUS-K45VM>
2017-10-06 14:53:03 -0300

    NaoCumprimentoMeta

--- SgqSystem/Angular/SelectVinculado.js
+++ SgqSystem/Angular/SelectVinculado.js
@@ -21,24 +21,6 @@
                 });
             }
 
-            //$http({
-            //    method: 'POST',
-            //    url: GetListLevel2
-            //}).
-            //then(function (r) {
-            //    $scope.level2 = r.data;
-            //});
-
-            //$http({
-            //    method: 'POST',
-            //    url: GetListLevel3
-            //}).
-            //then(function (r) {
-            //    $scope.level3 = r.data;
-            //});
-
-
-            //Defining the $http service for getting Level2 By Level1
             $scope.GetNextDDL = function (elementValue, url, elementChainedName, urlParaOptionSelecione) {
                 if (elementValue) {
                     $http({
