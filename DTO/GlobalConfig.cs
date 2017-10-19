efabf9167f08ba264a086087a18ce76ae80acee5
renan <renan@ASUS-K45VM>
2017-10-19 09:50:46 -0200

    Evaluated FTA no Relatorio de NC

--- DTO/GlobalConfig.cs
+++ DTO/GlobalConfig.cs
@@ -200,9 +200,11 @@ namespace DTO
             emailSmtp = dto.MailSmtp;
             emailPort = dto.MailPort;
             mockEmail = dto.MockEmail;
-            pathFTA = "http://mtzsvmqsc/PlanoDeAcao/Pa_Acao/NewFTA?";
+            //pathFTA = "http://mtzsvmqsc/PlanoDeAcao/Pa_Acao/NewFTA?";
             //pathFTA = "http://localhost:57506/Pa_Acao/NewFTA?";
             //pathFTA = "http://192.168.25.200/PlanoAcao/Pa_Acao/NewFTA?";
+            pathFTA = "http://192.168.25.200/PlanoAcaoUSA/Pa_Acao/NewFTA?";
+            //pathFTA = "http://10.190.2.34/ActionPlanHML/Pa_Acao/NewFTA?";
 
             Verifica += "recoveryPassAvaliable:  " + recoveryPassAvaliable.ToString() + "\n";
             Verifica += "urlPreffixAppColleta:  " + urlPreffixAppColleta + "\n";
