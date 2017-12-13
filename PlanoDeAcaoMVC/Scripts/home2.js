a7a47627fabb8e5ebea39e2b5dee1745eb575e97
Renan Santini <GRT-001@DESKTOP-0KN2R6G>
2017-12-13 10:21:22 -0200

    Correcoes PA

--- PlanoDeAcaoMVC/Scripts/home2.js
+++ PlanoDeAcaoMVC/Scripts/home2.js
@@ -2,8 +2,7 @@
 
 //var urlGetPlanejamentoAcaoRange = 'http://192.168.25.200/PlanoAcao/api/Pa_Planejamento/GetPlanejamentoAcaoRange';
 //var urlGetPlanejamentoAcaoRange = 'http://mtzsvmqsc/PlanoDeAcao/api/Pa_Planejamento/GetPlanejamentoAcaoRange';
-var urlGetPlanejamentoAcaoRange = 'http://localhost:59907/api/Pa_Planejamento/GetPlanejamentoAcaoRange';
-
+//var urlGetPlanejamentoAcaoRange = 'http://localhost:59907/api/Pa_Planejamento/GetPlanejamentoAcaoRange';
 
 var ColvisarrayVisaoAtual_show = [];
 var ColvisarrayVisaoAtual_hide = [];
