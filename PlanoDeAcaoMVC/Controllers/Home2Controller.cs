e4588c9a9cbdf42c8486a9c669baeb0b17b42c1c
Renan Santini <GRT-001@DESKTOP-0KN2R6G>
2018-02-07 11:57:39 -0200

    Correção dos dias de atraso no Visao Geral dos Projetos e Update automático das acões para "Em Andamento"

--- PlanoDeAcaoMVC/Controllers/Home2Controller.cs
+++ PlanoDeAcaoMVC/Controllers/Home2Controller.cs
@@ -32,6 +32,7 @@ namespace PlanoDeAcaoMVC.Controllers
             using (var dbPa = new PlanoAcaoEF.PlanoDeAcaoEntities())
             {
                 dbPa.Database.ExecuteSqlCommand("UPDATE Pa_acao SET [STATUS] = 1 WHERE Id IN (SELECT Id FROM Pa_acao WHERE [Status] = (5) AND  CONVERT (date ,QuandoFim) < CONVERT (date ,GETDATE()))");
+                dbPa.Database.ExecuteSqlCommand("UPDATE Pa_acao SET [STATUS] = 5 WHERE Id IN (SELECT Id FROM Pa_acao WHERE [Status] = (1) AND  CONVERT (date ,QuandoFim) >= CONVERT (date ,GETDATE()))");
             }
         }
     }
