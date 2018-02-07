01d30343ef0e7ace249102c3a116e9611e971c67
Renan Santini <GRT-001@DESKTOP-0KN2R6G>
2018-02-07 10:12:36 -0200

    Correção do Update de Status automático

--- PlanoDeAcaoMVC/Controllers/Home2Controller.cs
+++ PlanoDeAcaoMVC/Controllers/Home2Controller.cs
@@ -8,6 +8,12 @@ namespace PlanoDeAcaoMVC.Controllers
 {
     public class Home2Controller : Controller
     {
+        public Home2Controller()
+        {
+            UpdateStatus();
+        }
+
+
         // GET: Home2
         public ActionResult Index()
         {
@@ -19,6 +25,14 @@ namespace PlanoDeAcaoMVC.Controllers
         public ActionResult Index2()
         {
             return View();
+        }
+
+        protected void UpdateStatus()
+        {
+            using (var dbPa = new PlanoAcaoEF.PlanoDeAcaoEntities())
+            {
+                dbPa.Database.ExecuteSqlCommand("UPDATE Pa_acao SET [STATUS] = 1 WHERE Id IN (SELECT Id FROM Pa_acao WHERE [Status] = (5) AND  CONVERT (date ,QuandoFim) < CONVERT (date ,GETDATE()))");
+            }
         }
     }
 }
