96096dbf7e56d1080f86b0dc65fdc9d6fd2f34e1
GRT Solucoes <devgrtsolucoes@gmail.com>
2018-03-28 21:25:17 -0300

    Substituido Hangfire para Quartz

---
+++ SgqSystem/Jobs/MailJob.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+using System.Threading;
+using System.Threading.Tasks;
+using DTO;
+using SgqSystem.Mail;
+using System.Configuration;
+
+namespace Jobs
+{
+    public class MailJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            SendMailJobFunction(null);
+        }
+
+        public static void SendMailJobFunction(object stateInfo)
+        {
+            Thread.Sleep(30000);
+            while (true)
+            {
+                try
+                {
+                    if (ConfigurationManager.AppSettings["SendMailJob"] == "exec")
+                    {
+                        if (GlobalConfig.Brasil)
+                        {
+                            SimpleAsynchronous.SendMailFromDeviationSgqApp();
+                        }
+                        else if (GlobalConfig.Eua)
+                        {
+                            SimpleAsynchronousUSA.SendMailUSA();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    new CreateLog(new Exception("Erro no metodo [SendMailJobFunction]", ex));
+                }
+                Thread.Sleep(30000);
+            }
+        }
+    }
+}
---
+++ SgqSystem/Jobs/ReProcessJsonJob.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+using System.Threading;
+using System.Threading.Tasks;
+using DTO;
+using SgqSystem.Mail;
+using System.Configuration;
+
+namespace Jobs
+{
+    public class ReProcessJsonJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            ReProcessJsonJobFunction(null);
+        }
+
+        public static void ReProcessJsonJobFunction(object stateInfo)
+        {
+            Thread.Sleep(40000);
+            while (true)
+            {
+                if (ConfigurationManager.AppSettings["ResendProcessJsonJob"] == "exec")
+                {
+                    try
+                    {
+                        SimpleAsynchronous.ResendProcessJson();
+                    }
+                    catch (Exception ex)
+                    {
+                        new CreateLog(new Exception("Erro no metodo [ReProcessJsonJobFunction]", ex));
+                    }
+                }
+                Thread.Sleep(40000);
+            }
+        }
+    }
+}
