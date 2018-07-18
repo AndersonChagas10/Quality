3bc90e3f8abcbefd8332a58126cf7432678b36b3
GRT Solucoes <devgrtsolucoes@gmail.com>
2018-07-18 11:05:45 -0300

    [DEV-14] Corrigido nomenclatura de classes para ingles e criado o código para criação das tabelas no banco

---
+++ Dominio/ImportFormat.cs
@@ -0,0 +1,24 @@
+﻿namespace Dominio
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    
+    public partial class ImportFormat
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime Title { get; set; }
+
+        public DateTime AddDate { get; set; }
+
+        public DateTime? AlterDate { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        public virtual ICollection<ImportFormatItem> ImportFormatItems { get; set; }
+    }
+}
---
+++ Dominio/ImportFormatItem.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Dominio
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    
+    public partial class ImportFormatItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime Key { get; set; }
+
+        [Required]
+        public DateTime Value { get; set; }
+
+        public DateTime AddDate { get; set; }
+
+        public DateTime? AlterDate { get; set; }
+
+        [Required]
+        public int ImportFormat_Id { get; set; }
+
+        public ImportFormat ImportFormat { get; set; }
+    }
+}
