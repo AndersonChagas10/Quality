4e854d4b22d9e0a2522233d05551ba74923d14c9
GRT Solucoes <devgrtsolucoes@gmail.com>
2018-07-18 17:49:02 -0300

    [DEV-24] Implementação inicial do controller e view do relatório SIF

---
+++ SgqSystem/Controllers/Relatorios/SIFReportsController.cs
@@ -0,0 +1,22 @@
+﻿using Helper;
+using SgqSystem.Secirity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SgqSystem.Controllers.Relatorios
+{
+    [CustomAuthorize]
+    [OutputCache(Duration = 20, VaryByParam = "none")]
+    public class SIFReportsController : Controller
+    {
+        // GET: SIFReports
+        [FormularioPesquisa(filtraUnidadeDoUsuario = true)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
