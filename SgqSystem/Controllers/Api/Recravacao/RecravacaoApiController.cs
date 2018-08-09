1ab4888a0e96990f32549382c335d54429d07586
GRT Solucoes <devgrtsolucoes@gmail.com>
2018-08-09 18:36:11 -0300

    [DEV-102] Implementação inicial da recravacao na collection json

--- SgqSystem/Controllers/Api/Recravacao/RecravacaoApiController.cs
+++ SgqSystem/Controllers/Api/Recravacao/RecravacaoApiController.cs
@@ -102,14 +102,26 @@ namespace SgqSystem.Controllers.Api
                     RecravacaoJsonId = Update(linhaStringFormatada, existente, userFinished_Id, userValidated_Id);
                 else
                 {
-                    if (existente.GetValueOrDefault() > 0  && salvoParaInserirNovaColeta == true)
+                    if (existente.GetValueOrDefault() > 0 && salvoParaInserirNovaColeta == true)
                         RecravacaoJsonId = UpdateRecravacaoJsonParaNovaColeta(linhaStringFormatada, existente.GetValueOrDefault());
                     else
                         RecravacaoJsonId = Save(linhaStringFormatada, idLinha, idCompany, parLevel1_Id, userFinished_Id, userValidated_Id);
                 }
 
                 if (isValidated)
-                    RecravacaoJsonId = UpdateRecravacaoJsonFinalizaColetaValidada(linhaStringFormatada, existente.GetValueOrDefault(), userFinished_Id, userValidated_Id);
+                {
+                    RecravacaoJson recravacaoJsonValidada;
+                    RecravacaoJsonId = UpdateRecravacaoJsonFinalizaColetaValidada(linhaStringFormatada, existente.GetValueOrDefault(), userFinished_Id, userValidated_Id, out recravacaoJsonValidada);
+
+                    var listaCollectionJson = MapeiaRecravacaoJsonParaCollectionJson(linha, latas);
+                    using (Factory factory = new Factory("DefaultConnection"))
+                    {
+                        foreach (string collectionJson in listaCollectionJson)
+                        {
+                            new SgqSystem.Services.SyncServices().InsertJson(collectionJson, "1", "1", false);
+                        }
+                    }
+                }
                 //Post Save
                 mensagemSucesso = "Registro atualizado";
 
@@ -128,16 +140,16 @@ namespace SgqSystem.Controllers.Api
             return Request.CreateResponse(HttpStatusCode.OK, new { resposta = mensagemSucesso });
 
         }
-        
-        private int UpdateRecravacaoJsonFinalizaColetaValidada(string linhaStringFormatada, int? existente, int? userFinished_Id, int? userValidated_Id)
+
+        private int UpdateRecravacaoJsonFinalizaColetaValidada(string linhaStringFormatada, int? existente, int? userFinished_Id, int? userValidated_Id, out RecravacaoJson recravacaoJsonValidada)
         {
-            var updateRecravacaoJsonFinalizaColetaValidada = db.RecravacaoJson.FirstOrDefault(r => r.Id == existente);
-            updateRecravacaoJsonFinalizaColetaValidada.ObjectRecravacaoJson = linhaStringFormatada;
-            updateRecravacaoJsonFinalizaColetaValidada.AlterDate = DateTime.Now;
-            updateRecravacaoJsonFinalizaColetaValidada.UserFinished_Id = userFinished_Id;
-            updateRecravacaoJsonFinalizaColetaValidada.UserValidated_Id = userValidated_Id;
-            updateRecravacaoJsonFinalizaColetaValidada.isValidated = true;
-            return repo.Save(updateRecravacaoJsonFinalizaColetaValidada).Id;
+            recravacaoJsonValidada = db.RecravacaoJson.FirstOrDefault(r => r.Id == existente);
+            recravacaoJsonValidada.ObjectRecravacaoJson = linhaStringFormatada;
+            recravacaoJsonValidada.AlterDate = DateTime.Now;
+            recravacaoJsonValidada.UserFinished_Id = userFinished_Id;
+            recravacaoJsonValidada.UserValidated_Id = userValidated_Id;
+            recravacaoJsonValidada.isValidated = true;
+            return repo.Save(recravacaoJsonValidada).Id;
         }
 
         private int UpdateRecravacaoJsonParaNovaColeta(string linhaStringFormatada, int? existente)
@@ -180,29 +192,33 @@ namespace SgqSystem.Controllers.Api
 
         private void SaveLatas(int? RecravacaoJson_Id, dynamic latas)
         {
-
-            Factory factory = new Factory("DefaultConnection");
-            factory.ExecuteSql(string.Format("DELETE FROM RecravacaoLataJson WHERE RecravacaoJson_Id = {0};", RecravacaoJson_Id));
-            foreach (dynamic lata in latas)
+            using (Factory factory = new Factory("DefaultConnection"))
             {
-                var lataStringFormatada = ToJson(lata);
-                factory.ExecuteSql(
-                    string.Format("INSERT INTO RecravacaoLataJson (RecravacaoJson_Id, AddDate, ObjectRecravacaoJson) VALUES ({0}, GETDATE(), '{1}')", RecravacaoJson_Id, lataStringFormatada));
+                factory.ExecuteSql(string.Format("DELETE FROM RecravacaoLataJson WHERE RecravacaoJson_Id = {0};", RecravacaoJson_Id));
+                foreach (dynamic lata in latas)
+                {
+                    var lataStringFormatada = ToJson(lata);
+                    factory.ExecuteSql(
+                        string.Format("INSERT INTO RecravacaoLataJson (RecravacaoJson_Id, AddDate, ObjectRecravacaoJson) VALUES ({0}, GETDATE(), '{1}')", RecravacaoJson_Id, lataStringFormatada));
+                }
             }
         }
 
         private void UpdateRecravacaoLataJson(int RecravacaoJson_Id, string LataJson)
         {
-            Factory factory = new Factory("DefaultConnection");
-            factory.ExecuteSql(
+            using (Factory factory = new Factory("DefaultConnection"))
+            {
+                factory.ExecuteSql(
                 string.Format("UPDATE RecravacaoLataJson SET AlterDate = GETDATE(), ObjectRecravacaoJson = '{1}' WHERE RecravacaoJson_Id = {0}", RecravacaoJson_Id, LataJson));
+            }
         }
 
         private bool IsPropertyExist(dynamic obj, string name)
         {
             try
             {
-                return ((IDictionary<string, object>)obj).ContainsKey(name);
+                //return ((IDictionary<string, object>)obj).ContainsKey(name);
+                return !(obj[name] == null);
             }
             catch (Exception)
             {
@@ -214,7 +230,187 @@ namespace SgqSystem.Controllers.Api
             //return obj.GetType().GetProperty(name) != null;
         }
 
+        //[Angular5]
+        private List<string> MapeiaRecravacaoJsonParaCollectionJson(dynamic linha, dynamic latas)
+        {
+            int amostraAtual = 0;
+            List<string> listaCollectionJson = new List<string>();
+            foreach (dynamic lata in latas)
+            {
+                foreach (dynamic ContadorDeAmostras in lata["ResultValue"])
+                {
+                    DateTime dataColetaFormatada = DateTime.MinValue;
+                    DateTime.TryParseExact(Convert.ToString(linha["AddDate"]), "dd/MM/yyyy HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out dataColetaFormatada);
+
+                    string collectionJson = "<level02>";
+                    collectionJson += "{Processo}" + "|" + linha["ParLevel1_Id"]; //[0]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += dataColetaFormatada.ToString("MM/dd/yyyy HH:mm:ss"); //[1]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "{Processo}" + "|" + linha["ParLevel2_Id"]; //[2]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += dataColetaFormatada.ToString("MM/dd/yyyy HH:mm:ss"); //[3]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += linha["ParCompany_Id"]; //[4]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "1"; //[5]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "1"; //[6]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "1"; //[7]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "0"; //[8]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "false"; //[9]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += dataColetaFormatada.ToString("MM/dd/yyyy HH:mm:ss"); //[10]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "1"; //[11]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "1"; //[12]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += ""; //[13]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "false"; //[14]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "false"; //[15]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += ""; //[16]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "undefined"; //[17]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "undefined"; //[18]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "false"; //[19]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "recravacao"; //[20]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "jbs"; //[21]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+
+                    // Início do Level03
+                    foreach (dynamic Tarefa in lata["ResultValue"])
+                    {
+                        string tarefa_id = Convert.ToString(Tarefa);
+                        int primeiraAspas = tarefa_id.IndexOf("\"");
+                        int segundaAspas = tarefa_id.Substring(primeiraAspas + 1).IndexOf("\"");
+                        tarefa_id = tarefa_id.Substring(primeiraAspas + 1, segundaAspas - primeiraAspas);
+                        int contador = 0;
+                        string valor = "";
+                        foreach (dynamic valorTarefa in Tarefa)
+                        {
+                            valor = valorTarefa[(amostraAtual + 1).ToString()];
+                            if (contador == amostraAtual)
+                            {
+                                continue;
+                            }
+                            ++contador;
+                        }
+                        collectionJson += "<level03>";
+                        collectionJson += tarefa_id; //level3 1
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += dataColetaFormatada.ToString("MM/dd/yyyy HH:mm:ss");//collectionDate; //level3 2
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "0"; //level3 2???
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "true"; //level3 3
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "1"; //level3 6
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "null"; //level3 7
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "undefined"; //level3 8
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "undefined"; //level3 9
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "0"; //level3 10
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += ""; //level3 11
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "0"; //level3 12
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "0"; //level3 13
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "0"; //level3 14
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "{Avaliacao}";//av; //level3 15
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "{Nao Conformidade}";//NC; //level3 16
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "0"; //level3 17
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "0"; //level3 17??
+                        collectionJson += ","; //SEPARADOR LEVEL3
+                        collectionJson += "0"; //level3 17??
+                        collectionJson += "</level03>";
+
+                    }
+
+                    // Final do Level03
+
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += ""; //[23]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "false"; //[24]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "false"; //[25]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "0"; //[26]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "null"; //[27]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "null"; //[28]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "false"; //[29]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "undefined"; //[30]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "false"; //[31]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "false"; //[32]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "undefined"; //[33]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "1"; //[35]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "0"; //[36]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "0"; //[37]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "1"; //[38]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "0"; //[39]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "0"; //[40]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "1"; //[41]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "0"; //[42]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "1"; //[43]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "1"; //[44]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "0"; //[45]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "0"; //[46]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "0"; //[47]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "0"; //[48]
+                    collectionJson += ";"; //SEPARADOR LEVEL2
+                    collectionJson += "{Processo}"; //[49]
+                    collectionJson += "</level02>";
+                    listaCollectionJson.Add(collectionJson);
+                }
+
+                amostraAtual++;
+            }
+
+            return listaCollectionJson;
+        }
+
     }
 
-    
+
 }
