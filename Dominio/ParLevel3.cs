9391d36556ee6c0995a985356d587161f237fc12
Marcos <markos_augusto@hotmail.com>
2018-09-28 12:29:36 -0300

    [DEV-302] - Implementa responsividade na tabela de vinculo entra Grupo de indicadores e tarefa.

--- Dominio/ParLevel3.cs
+++ Dominio/ParLevel3.cs
@@ -2,6 +2,7 @@ namespace Dominio
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -24,6 +25,7 @@ namespace Dominio
 
         [Required(AllowEmptyStrings = true)]
         [StringLength(1000)]
+        [DisplayName("Tarefa")]
         public string Name { get; set; }
 
         [Required(AllowEmptyStrings = true)]
