9c2d6d32065b5e2caa20cf6e2ae4ba53c2cc71dc
Marcos <markos_augusto@hotmail.com>
2018-10-02 17:17:56 -0300

    [DEV-302] - Implemente regra para preencher id do pai e o hash, para implementar a arvore. E apresentar somente as funções que estiverem ativas

--- SgqSystem/Controllers/ParGroupParLevel1Controller.cs
+++ SgqSystem/Controllers/ParGroupParLevel1Controller.cs
@@ -17,7 +17,12 @@ namespace SgqSystem.Controllers
         // GET: ParGroupParLevel1
         public ActionResult Index()
         {
-            var parGroupParLevel1 = db.ParGroupParLevel1.Include(p => p.ParGroupParLevel1Type);
+            var parGroupParLevel1 = db.ParGroupParLevel1.Include(p => p.ParGroupParLevel1Type).Where(x => x.IsActive);
+
+            var listaFilhos = db.ParGroupParLevel1.Where(x => x.IsActive).ToList();
+            listaFilhos.Add(new ParGroupParLevel1() { Id = -1, Name = "Selecione" });
+            ViewBag.Parent_Id = new SelectList(listaFilhos, "Id", "Name", -1);
+
             return View(parGroupParLevel1.ToList());
         }
 
@@ -58,6 +63,7 @@ namespace SgqSystem.Controllers
         public ActionResult Create([Bind(Include = "Id,Name,ParGroupParLevel1Type_Id,IsActive,Parent_Id")] ParGroupParLevel1 parGroupParLevel1)
         {
             ValidaGrupoIndicadores(parGroupParLevel1);
+            ValidaHash(parGroupParLevel1);
             if (ModelState.IsValid)
             {
                 db.ParGroupParLevel1.Add(parGroupParLevel1);
@@ -67,6 +73,27 @@ namespace SgqSystem.Controllers
 
             ViewBag.ParGroupParLevel1Type_Id = new SelectList(db.ParGroupParLevel1Type, "Id", "Name", parGroupParLevel1.ParGroupParLevel1Type_Id);
             return View(parGroupParLevel1);
+        }
+
+        private void ValidaHash(ParGroupParLevel1 parGroupParLevel1)
+        {
+            var pai = db.ParGroupParLevel1.AsNoTracking().Where(x => x.Parent_Id == parGroupParLevel1.Parent_Id).FirstOrDefault();
+
+            if (pai != null)
+            {
+                if (pai.Hash != null)
+                {
+                    parGroupParLevel1.Hash = pai.Hash + "|" + pai.Parent_Id;
+                }
+                else
+                {
+                    parGroupParLevel1.Hash = pai.Parent_Id.ToString();
+                }
+            }
+            else
+            {
+                parGroupParLevel1.Hash = null;
+            }
         }
 
         // GET: ParGroupParLevel1/Edit/5
@@ -85,6 +112,9 @@ namespace SgqSystem.Controllers
             listaGrupos.Add(new ParGroupParLevel1Type() { Id = -1, Name = "Selecione" });
             ViewBag.ParGroupParLevel1Type_Id = new SelectList(listaGrupos, "Id", "Name", parGroupParLevel1.ParGroupParLevel1Type_Id);
 
+            var listaGruposIndicador = db.ParGroupParLevel1.Where(x => x.IsActive).ToList();
+            listaGruposIndicador.Add(new ParGroupParLevel1() { Id = -1, Name = "Selecione" });
+            ViewBag.Parent_Id = new SelectList(listaGruposIndicador, "Id", "Name", parGroupParLevel1.Parent_Id);
             //ViewBag.ParGroupParLevel1Type_Id = new SelectList(db.ParGroupParLevel1Type, "Id", "Name", parGroupParLevel1.ParGroupParLevel1Type_Id);
             return View(parGroupParLevel1);
         }
@@ -97,6 +127,7 @@ namespace SgqSystem.Controllers
         public ActionResult Edit([Bind(Include = "Id,Name,ParGroupParLevel1Type_Id,IsActive,Parent_Id")] ParGroupParLevel1 parGroupParLevel1)
         {
             ValidaGrupoIndicadores(parGroupParLevel1);
+            ValidaHash(parGroupParLevel1);
             if (ModelState.IsValid)
             {
                 db.Entry(parGroupParLevel1).State = EntityState.Modified;
