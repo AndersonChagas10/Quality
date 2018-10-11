a0dfc0bef10b1a5b58167094360b802d1e4ed98e
Marcos <markos_augusto@hotmail.com>
2018-10-11 15:34:34 -0300

    [DEV-297] - Inicia a criação de validação de campos na edição de cabeçalhos.

--- DTO/DTO/Params/ParHeaderFieldDTO.cs
+++ DTO/DTO/Params/ParHeaderFieldDTO.cs
@@ -10,7 +10,7 @@ namespace DTO.DTO.Params
     {
         public int? ParFieldType_Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; } = " ";
+        public string Description { get; set; } = "";
         public int ParLevelDefinition_Id { get; set; }
         public bool LinkNumberEvaluetion { get; set; }
         public bool IsActive { get; set; }
