6ff6fe71b3ca38f468bc99a682759de6805d442b
GRT Solucoes <devgrtsolucoes@gmail.com>
2018-10-30 20:41:08 -0300

    [ADD] Implementação inicial de tela para listagem de integrações

---
+++ SgqSystem/Controllers/Integracao/IntegracaoController.cs
@@ -0,0 +1,23 @@
+﻿using ADOFactory;
+using Dominio;
+using Newtonsoft.Json.Linq;
+using SgqSystem.Controllers.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SgqSystem.Controllers.Integracao
+{
+    public class IntegracaoController : BaseController
+    {
+        // GET: Integracao
+        public ActionResult Index()
+        {
+            ViewBag.UnitId = getUserUnitId();
+            return View();
+        }
+    }
+
+}
