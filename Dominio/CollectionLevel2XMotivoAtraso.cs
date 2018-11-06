3a7536f490b10b28d2446abc21cf51a8e2c0f2da
Renan Santini <renanssantini@hotmail.com>
2018-11-06 11:10:17 -0200

    [DEV-398] - WebMethod InsertStatusColeta no syncServices para salvar o status da coleta

---
+++ Dominio/CollectionLevel2XMotivoAtraso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class CollectionLevel2XMotivoAtraso : BaseModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int CollectionLevel2_Id { get; set; }
+
+        public int MotivoAtraso_Id { get; set; }
+
+        [ForeignKey("CollectionLevel2_Id")]
+        public virtual CollectionLevel2 CollectionLevel2 { get; set; }
+
+        [ForeignKey("MotivoAtraso_Id")]
+        public virtual MotivoAtraso MotivoAtraso { get; set; }
+
+    }
+}
---
+++ Dominio/ViewModel/ListaCollectionsLevel2XMotivosAtraso.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class ListaCollectionsLevel2XMotivosAtraso
+    {
+        public List<CollectionLevel2XMotivoAtraso> CollectionsLevel2XMotivosAtraso { get; set; }
+
+        public bool DadosIsValid()
+        {
+            if (CollectionsLevel2XMotivosAtraso.Count <= 0)
+            {
+                return false;
+            }
+
+            foreach (var item in CollectionsLevel2XMotivosAtraso)
+            {
+                if(item.CollectionLevel2_Id <= 0)
+                {
+                    return false;
+                }
+
+                if (item.MotivoAtraso_Id <= 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
