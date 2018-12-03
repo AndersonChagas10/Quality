8d862d3b71a215d0e5fae18f38c8e749564b5a1a
Renan Santini <renanssantini@hotmail.com>
2018-12-03 15:49:55 -0200

    [DEV-449] - Controller Relatorio Sensorial

---
+++ SgqSystem/Controllers/RelatoriosMD/RelatoriosMDController.cs
@@ -0,0 +1,22 @@
+﻿using Helper;
+using SgqSystem.Secirity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SgqSystem.Controllers.RelatoriosMD
+{
+    [CustomAuthorize]
+    [OutputCache(Duration = 20, VaryByParam = "none")]
+    public class RelatoriosMDController : BaseController
+    {
+        // GET: RelatoriosMD
+        [FormularioPesquisa(filtraUnidadePorUsuario = true)]
+        public ActionResult AvaliacaoSensorial()
+        {
+            return View();
+        }
+    }
+}
