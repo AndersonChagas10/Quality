cc9111250fbd4cdfff97a6678a1722fda2b908ce
Renan Santini <renanssantini@hotmail.com>
2018-12-04 08:27:47 -0200

    [DEV-449] - Api Relatorio Sensorial

---
+++ SgqSystem/Controllers/Api/RelatoriosMD/AvaliacaoSensorialApiController.cs
@@ -0,0 +1,29 @@
+﻿using SgqSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SgqSystem.Controllers.Api.RelatoriosMD
+{
+    [RoutePrefix("api/AvaliacaoSensorial")]
+    public class AvaliacaoSensorialApiController : ApiController
+    {
+
+        [HttpPost]
+        [Route("Get")]
+        public Retorno Get([FromBody] FormularioParaRelatorioViewModel form)
+        {
+
+            return null;
+        }
+
+        public class Retorno
+        {
+
+        }
+
+    }
+}
