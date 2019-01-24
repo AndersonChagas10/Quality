cfcebc137a1282def40563328b98edb3eeaae609
GRT Solucoes <devgrtsolucoes@gmail.com>
2019-01-24 15:40:34 -0200

    [FIX] Adicionado a correção para preenchimento da DatabaseLog

---
+++ Helper/UtilType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public static class UtilType
+    {
+        public static bool IsPrimitiveAndNullableType(this Type type)
+        {
+            return
+                type == typeof(int) || type == typeof(int?) ||
+                type == typeof(long) || type == typeof(long?) ||
+                type == typeof(bool) || type == typeof(bool?) ||
+                type == typeof(string) ||
+                type == typeof(System.Int16) || type == typeof(System.Int16?) ||
+                type == typeof(System.Int32) || type == typeof(System.Int32?) ||
+                type == typeof(System.Int64) || type == typeof(System.Int64?) ||
+                type == typeof(String) ||
+                type == typeof(DateTime) || type == typeof(DateTime?) ||
+                type == typeof(char) || type == typeof(char?);
+        }
+    }
+}
