a0c6fdb6dd222a53a41c522089bc13d9d7edd15b
Marcos Augusto <markos_augusto@hotmail.com>
2019-02-13 16:09:25 -0200

    [DEV-537] - Inicia mensagens do FAQ, e uma tela informando o conteudo das novas versões.

---
+++ SgqSystem/Controllers/NovidadesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SgqSystem.Controllers
+{
+    public class NovidadesController : BaseController
+    {
+        // GET: Novidades
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Versao1_4()
+        {
+            return View();
+        }
+
+        public ActionResult Versao1_5()
+        {
+            return View();
+        }
+    }
+}
