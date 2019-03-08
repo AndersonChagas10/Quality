cee377e9bda66e701eb61ced06fff260589eace6
Bruno Sousa <dosidantas@gmail.com>
2019-03-08 12:20:00 -0300

    Correção Report VGA = Correção Nivel Critico

--- SgqSystem/Controllers/Api/RelatoriosBrasil/VisaoGeralDaAreaApiController.cs
+++ SgqSystem/Controllers/Api/RelatoriosBrasil/VisaoGeralDaAreaApiController.cs
@@ -1896,7 +1896,7 @@ namespace SgqSystem.Controllers.Api.RelatoriosBrasil
 
             if (form.criticalLevelId > 0)
             {
-                whereCriticalLevel = $@"AND P1.Id IN (SELECT P1XC.ParLevel1_Id FROM ParLevel1XCluster P1XC WHERE P1XC.ParCriticalLevel_Id = { form.criticalLevelId })";
+                whereCriticalLevel = $@"AND S.Level1Id IN (SELECT P1XC.ParLevel1_Id FROM ParLevel1XCluster P1XC WHERE P1XC.ParCriticalLevel_Id = { form.criticalLevelId })";
             }
 
             _list = new List<VisaoGeralDaAreaResultSet>();
