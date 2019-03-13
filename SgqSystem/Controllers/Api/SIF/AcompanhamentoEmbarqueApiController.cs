35214d9a07e1a18373d1fd0d7e4a179182135f4e
Renan Santini <renanssantini@hotmail.com>
2019-03-13 17:26:33 -0300

    [HOTFIX] - Correção de merge Relatório de Acompanhamento de Embarque - Amostras Infinitas não sequenciais

--- SgqSystem/Controllers/Api/SIF/AcompanhamentoEmbarqueApiController.cs
+++ SgqSystem/Controllers/Api/SIF/AcompanhamentoEmbarqueApiController.cs
@@ -20,6 +20,8 @@ namespace SgqSystem.Controllers.Api.SIF
 
             if (retorno != null)
             {
+                retorno.Fotos = GetFotos(form);
+
                 using (var db = new SgqDbDevEntities())
                 {
                     retorno.Aprovador = getAprovadorName(form, db);
@@ -81,112 +83,195 @@ namespace SgqSystem.Controllers.Api.SIF
                     var TemperaturaMax = new List<string>();
 
 
+                    int TipoVeiculoId = int.Parse(GetDicionarioEstatico("TipoVeiculo"));
+                    int TransportadoraId = int.Parse(GetDicionarioEstatico("Transportador"));
+                    int PlacaId = int.Parse(GetDicionarioEstatico("Placa"));
+                    int NomeMotoristaId = int.Parse(GetDicionarioEstatico("NomeMotorista"));
+                    int LacreNumeroId = int.Parse(GetDicionarioEstatico("LacreNumero"));
+                    int TermografoId = int.Parse(GetDicionarioEstatico("Termografo_Id"));
+                    int SifNumberId = int.Parse(GetDicionarioEstatico("SifNumber"));
+                    int PedidoId = int.Parse(GetDicionarioEstatico("Pedido"));
+                    int InstrucaoId = int.Parse(GetDicionarioEstatico("Instrucao"));
+                    int TipoCargaId = int.Parse(GetDicionarioEstatico("TipoCarga"));
+                    int TipoProdutoId = int.Parse(GetDicionarioEstatico("TipoProduto"));
+                    int TipoEmbalagemId = int.Parse(GetDicionarioEstatico("TipoEmbalagem"));
+                    int TemperaturaMinId = int.Parse(GetDicionarioEstatico("TemperaturaMin"));
+                    int TemperaturaMaxId = int.Parse(GetDicionarioEstatico("TemperaturaMax"));
+                    int NumeroNotaFiscalId = int.Parse(GetDicionarioEstatico("NumeroNotaFiscal"));
+                    int DataCarregamentoId = int.Parse(GetDicionarioEstatico("DataCarregamento"));
+
                     foreach (var item in headerFieldsValues)
                     {
 
-                        switch (item.Id)
+                        if (item.Id == TipoVeiculoId)
                         {
-                            ////JBS
-                            //case 198: //Tipo de veículo
-                            //    TipoVeiculo.Add(item.Value);
-                            //    break;
-                            //case 199://Transportadora
-                            //    Transportadora.Add(item.Value);
-                            //    break;
-                            //case 200: //Placa do veículo
-                            //    PlacaDoVeiculo.Add(item.Value);
-                            //    break;
-                            //case 201: //Nome do motorista
-                            //    NomeMotorista.Add(item.Value);
-                            //    break;
-                            //case 202: //Lacre número
-                            //    LacreNumero.Add(item.Value);
-                            //    break;
-                            //case 203: //Termógrafo número
-                            //    Termografo_Id.Add(item.Value);
-                            //    break;
-                            //case 204: //SIF ou Nome
-                            //    SifNumber.Add(item.Value);
-                            //    break;
-                            //case 205: //Pedido
-                            //    Pedido.Add(item.Value);
-                            //    break;
-                            //case 206: //Data do carregamento
-                            //    DataCarregamento.Add(item.Value);
-                            //    break;
-                            //case 207: //Instrução
-                            //    Instrucao.Add(item.Value);
-                            //    break;
-                            //case 208: //Notas Fiscais
-                            //    NumeroNotaFiscal.Add(item.Value);
-                            //    break;
-                            //case 209: //Tipo de produto
-                            //    TipoCarga.Add(item.Value);
-                            //    break;
-                            //case 210: //Tipo de embalagem
-                            //    TipoEmbalagem.Add(item.Value);
-                            //    break;
-                            //case 211: //Tipo de produto
-                            //    TipoProduto.Add(item.Value);
-                            //    break;
-                            //case 212: //Termógrafo - T° mín
-                            //    TemperaturaMin.Add(item.Value);
-                            //    break;
-                            //case 213: //Termógrafo - T° máx   
-                            //    TemperaturaMax.Add(item.Value);
-                            //    break;
-
-
-                            //GRT
-                            case 1166: //Tipo de veículo
-                                TipoVeiculo.Add(item.Value);
-                                break;
-                            case 1167://Transportadora
-                                Transportadora.Add(item.Value);
-                                break;
-                            case 1168: //Placa do veículo
-                                PlacaDoVeiculo.Add(item.Value);
-                                break;
-                            case 1169: //Nome do motorista
-                                NomeMotorista.Add(item.Value);
-                                break;
-                            case 1172: //Lacre número
-                                LacreNumero.Add(item.Value);
-                                break;
-                            case 1173: //Termógrafo número
-                                Termografo_Id.Add(item.Value);
-                                break;
-                            case 1174: //SIF ou Nome
-                                SifNumber.Add(item.Value);
-                                break;
-                            case 1175: //Pedido
-                                Pedido.Add(item.Value);
-                                break;
-                            case 1176: //Data do Carregamento
-                                DataCarregamento.Add(item.Value);
-                                break;
-                            case 1177: //Instrução
-                                Instrucao.Add(item.Value);
-                                break;
-                            case 1178: //Notas Fiscais
-                                NumeroNotaFiscal.Add(item.Value);
-                                break;
-                            case 1179: //Tipo de Carga
-                                TipoCarga.Add(item.Value);
-                                break;
-                            case 1181: //Tipo de produto
-                                TipoProduto.Add(item.Value);
-                                break;
-                            case 1180: //Tipo de embalagem
-                                TipoEmbalagem.Add(item.Value);
-                                break;
-                            case 1182: //Termógrafo - T° mín
-                                TemperaturaMin.Add(item.Value);
-                                break;
-                            case 1183: //Termógrafo - T° máx   
-                                TemperaturaMax.Add(item.Value);
-                                break;
+                            TipoVeiculo.Add(item.Value);
                         }
+                        else if (item.Id == TransportadoraId)
+                        {
+                            Transportadora.Add(item.Value);
+                        }
+                        else if (item.Id == PlacaId)
+                        {
+                            PlacaDoVeiculo.Add(item.Value);
+                        }
+                        else if (item.Id == NomeMotoristaId)
+                        {
+                            NomeMotorista.Add(item.Value);
+                        }
+                        else if (item.Id == LacreNumeroId)
+                        {
+                            LacreNumero.Add(item.Value);
+                        }
+                        else if (item.Id == TermografoId)
+                        {
+                            Termografo_Id.Add(item.Value);
+                        }
+                        else if (item.Id == SifNumberId)
+                        {
+                            SifNumber.Add(item.Value);
+                        }
+                        else if (item.Id == PedidoId)
+                        {
+                            Pedido.Add(item.Value);
+                        }
+                        else if (item.Id == InstrucaoId)
+                        {
+                            Instrucao.Add(item.Value);
+                        }
+                        else if (item.Id == TipoCargaId)
+                        {
+                            TipoCarga.Add(item.Value);
+                        }
+                        else if (item.Id == TipoProdutoId)
+                        {
+                            TipoProduto.Add(item.Value);
+                        }
+                        else if (item.Id == TipoEmbalagemId)
+                        {
+                            TipoEmbalagem.Add(item.Value);
+                        }
+                        else if (item.Id == TemperaturaMinId)
+                        {
+                            TemperaturaMin.Add(item.Value);
+                        }
+                        else if (item.Id == TemperaturaMaxId)
+                        {
+                            TemperaturaMax.Add(item.Value);
+                        }
+                        else if (item.Id == NumeroNotaFiscalId)
+                        {
+                            NumeroNotaFiscal.Add(item.Value);
+                        }
+                        else if (item.Id == DataCarregamentoId)
+                        {
+                            DataCarregamento.Add(item.Value);
+                        }
+
+
+                        //switch (item.Id)
+                        //{
+                        ////JBS
+                        //case 198: //Tipo de veículo
+                        //    TipoVeiculo.Add(item.Value);
+                        //    break;
+                        //case 199://Transportadora
+                        //    Transportadora.Add(item.Value);
+                        //    break;
+                        //case 200: //Placa do veículo
+                        //    PlacaDoVeiculo.Add(item.Value);
+                        //    break;
+                        //case 201: //Nome do motorista
+                        //    NomeMotorista.Add(item.Value);
+                        //    break;
+                        //case 202: //Lacre número
+                        //    LacreNumero.Add(item.Value);
+                        //    break;
+                        //case 203: //Termógrafo número
+                        //    Termografo_Id.Add(item.Value);
+                        //    break;
+                        //case 204: //SIF ou Nome
+                        //    SifNumber.Add(item.Value);
+                        //    break;
+                        //case 205: //Pedido
+                        //    Pedido.Add(item.Value);
+                        //    break;
+                        //case 206: //Data do carregamento
+                        //    DataCarregamento.Add(item.Value);
+                        //    break;
+                        //case 207: //Instrução
+                        //    Instrucao.Add(item.Value);
+                        //    break;
+                        //case 208: //Notas Fiscais
+                        //    NumeroNotaFiscal.Add(item.Value);
+                        //    break;
+                        //case 209: //Tipo de Carga
+                        //    TipoCarga.Add(item.Value);
+                        //    break;
+                        //case 210: //Tipo de embalagem
+                        //    TipoEmbalagem.Add(item.Value);
+                        //    break;
+                        //case 211: //Tipo de produto
+                        //    TipoProduto.Add(item.Value);
+                        //    break;
+                        //case 212: //Termógrafo - T° mín
+                        //    TemperaturaMin.Add(item.Value);
+                        //    break;
+                        //case 213: //Termógrafo - T° máx   
+                        //    TemperaturaMax.Add(item.Value);
+                        //    break;
+
+
+                        ////GRT
+                        //case 1166: //Tipo de veículo 
+                        //    TipoVeiculo.Add(item.Value);
+                        //    break;
+                        //case 1167://Transportadora
+                        //    Transportadora.Add(item.Value);
+                        //    break;
+                        //case 1168: //Placa do veículo
+                        //    PlacaDoVeiculo.Add(item.Value);
+                        //    break;
+                        //case 1169: //Nome do motorista
+                        //    NomeMotorista.Add(item.Value);
+                        //    break;
+                        //case 1172: //Lacre número
+                        //    LacreNumero.Add(item.Value);
+                        //    break;
+                        //case 1173: //Termógrafo número
+                        //    Termografo_Id.Add(item.Value);
+                        //    break;
+                        //case 1174: //SIF ou Nome
+                        //    SifNumber.Add(item.Value);
+                        //    break;
+                        //case 1175: //Pedido
+                        //    Pedido.Add(item.Value);
+                        //    break;
+                        //case 1176: //Data do Carregamento
+                        //    DataCarregamento.Add(item.Value);
+                        //    break;
+                        //case 1177: //Instrução
+                        //    Instrucao.Add(item.Value);
+                        //    break;
+                        //case 1178: //Notas Fiscais
+                        //    NumeroNotaFiscal.Add(item.Value);
+                        //    break;
+                        //case 1179: //Tipo de Carga
+                        //    TipoCarga.Add(item.Value);
+                        //    break;
+                        //case 1181: //Tipo de produto
+                        //    TipoProduto.Add(item.Value);
+                        //    break;
+                        //case 1180: //Tipo de embalagem
+                        //    TipoEmbalagem.Add(item.Value);
+                        //    break;
+                        //case 1182: //Termógrafo - T° mín
+                        //    TemperaturaMin.Add(item.Value);
+                        //    break;
+                        //case 1183: //Termógrafo - T° máx   
+                        //    TemperaturaMax.Add(item.Value);
+                        //    break;
+                        //}
 
                     }
 
@@ -279,25 +364,25 @@ namespace SgqSystem.Controllers.Api.SIF
 			clxhf.value
 		FROM CollectionLevel2XParHeaderField clxhf
 		WHERE clxhf.CollectionLevel2_Id = C2.Id
-		AND clxhf.ParHeaderField_Id = 216) --1186)
+		AND clxhf.ParHeaderField_Id = {GetDicionarioEstatico("SIF")})--216) --1186)
 	AS SIF
    ,(SELECT TOP 1
 			iif(clxhf.value is null, null, cast(clxhf.value as Date))
 		FROM CollectionLevel2XParHeaderField clxhf
 		WHERE clxhf.CollectionLevel2_Id = C2.Id
-		AND clxhf.ParHeaderField_Id = 218) --1188)
+		AND clxhf.ParHeaderField_Id = {GetDicionarioEstatico("DataValidade")}) --218) --1188)
 	AS DataValidade
    ,(SELECT TOP 1
 			iif(clxhf.value is null, null, cast(clxhf.value as date))
 		FROM CollectionLevel2XParHeaderField clxhf
 		WHERE clxhf.CollectionLevel2_Id = C2.Id
-		AND clxhf.ParHeaderField_Id = 217) --1187)
+		AND clxhf.ParHeaderField_Id = {GetDicionarioEstatico("DataProducaoEmbarque")}) -- 217) --1187)
 	AS DataProducao
    ,(SELECT TOP 1
 			clxhf.value
 		FROM CollectionLevel2XParHeaderField clxhf
 		WHERE clxhf.CollectionLevel2_Id = C2.Id
-		AND clxhf.ParHeaderField_Id = 215) --1185)
+		AND clxhf.ParHeaderField_Id = {GetDicionarioEstatico("CB")}) -- 215) --1185)
 	AS CB
 FROM CollectionLevel2 C2 WITH (NOLOCK)
 INNER JOIN CollectionLevel2XParHeaderField C2XHF WITH (NOLOCK)
@@ -346,6 +431,42 @@ DESC, c2.EvaluationNumber, c2.Sample, C2.CollectionDate DESC";
             }
         }
 
+        private List<Fotos> GetFotos(FormularioParaRelatorioViewModel form)
+        {
+
+            var sql = $@"
+                    select 
+                    	top 100 
+                    	CL2XPHF.Value as Produto_Id ,
+                    	P.cNmProduto Produto ,
+                    	RL3.ParLevel3_Id as ParLevel3_Id ,
+                    	RL3.ParLevel3_Name as ParLevel3 ,
+                    	CL2.Sample as Amostra ,
+                    	RL3P.Photo as Photo
+                    from CollectionLevel2 CL2
+                    Inner Join Result_Level3 RL3 with (nolock) on CL2.Id = RL3.CollectionLevel2_Id
+                    Inner Join Result_Level3_Photos RL3P with (nolock) on RL3P.Result_Level3_Id = RL3.Id
+                    inner join CollectionLevel2XParHeaderField CL2XPHF with (nolock) on CL2.Id = CL2XPHF.CollectionLevel2_Id
+                    Inner join Produto P with (nolock) on P.nCdProduto = CL2XPHF.Value
+                    where 1 = 1
+                    AND CL2XPHF.ParHeaderField_Id = 1184 --214
+                    AND CL2.ParLevel1_Id = 68 --89
+                    AND CAST(CL2.CollectionDate AS DATE) = '{form._dataInicioSQL}'
+                    AND CL2.parlevel1_id = { form.level1Id }
+                    AND CL2.ParLevel2_Id = { form.level2Id }
+                    AND CL2.UnitId = { form.unitId }
+                    AND CL2.EvaluationNumber = { form.avaliacao }
+                ";
+
+            using (var db = new SgqDbDevEntities())
+            {
+                db.Database.CommandTimeout = 300;
+                var fotos = db.Database.SqlQuery<Fotos>(sql).ToList();
+
+                return fotos;
+            }
+        }
+
     }
 
     public class Retorno
@@ -389,6 +510,8 @@ DESC, c2.EvaluationNumber, c2.Sample, C2.CollectionDate DESC";
 
         public List<Coleta> Coletas { get; set; }
 
+        public List<Fotos> Fotos { get; set; }
+
     }
 
     public class HeaderFieldsValues
@@ -418,5 +541,15 @@ DESC, c2.EvaluationNumber, c2.Sample, C2.CollectionDate DESC";
         public DateTime? DataValidade { get; set; }
         public DateTime? DataProducao { get; set; }
         public string CB { get; set; }
+    }
+
+    public class Fotos
+    {
+        public string Produto_Id { get; set; }
+        public string Produto { get; set; }
+        public int ParLevel3_Id { get; set; }
+        public string ParLevel3 { get; set; }
+        public int Amostra { get; set; }
+        public string Photo { get; set; }
     }
 }
