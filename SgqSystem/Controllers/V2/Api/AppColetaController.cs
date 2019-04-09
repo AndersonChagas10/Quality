5022ca5304d79ad768641cd60aa56b1417eb6dc2
GRT Solucoes <devgrtsolucoes@gmail.com>
2019-04-08 22:40:09 -0300

    [ADD] Criado api para retorno do json relacionado com a coleta api/AppColeta/GetAppParametrization/{ParCompany_Id}

--- SgqSystem/Controllers/V2/Api/AppColetaController.cs
+++ SgqSystem/Controllers/V2/Api/AppColetaController.cs
@@ -18,17 +18,24 @@ namespace SgqSystem.Controllers.V2.Api
     [RoutePrefix("api/AppColeta")]
     public class AppColetaController : BaseApiController
     {
-        [Route("GetAppParametrization")]
-        public IHttpActionResult GetAppParametrization()
+        [Route("GetAppParametrization/{id}")]
+        public IHttpActionResult GetAppParametrization(int id)
         {
 
             List<ParVinculoPesoAppViewModel> listaParVinculoPeso;
             List<ParLevel1AppViewModel> listaParLevel1;
             List<ParLevel2AppViewModel> listaParLevel2;
             List<ParLevel3AppViewModel> listaParLevel3;
+
             List<ParEvaluationAppViewModel> listaParEvaluation;
             List<ParLevel3EvaluationSampleAppViewModel> listaParLevel3EvaluationSample;
-            var companyId = 50;
+
+            List<ParLevel3ValueAppViewModel> listaParLevel3Value;
+            List<ParLevel3InputTypeAppViewModel> listaParLevel3InputType;
+            List<ParMeasurementUnitAppViewModel> listaParMeasurementUnit;
+            List<ParLevel3BoolTrueAppViewModel> listaParLevel3BoolTrue;
+            List<ParLevel3BoolFalseAppViewModel> listaParLevel3BoolFalse;
+            var companyId = id;
 
             using (Dominio.SgqDbDevEntities db = new Dominio.SgqDbDevEntities())
             {
@@ -122,14 +129,82 @@ namespace SgqSystem.Controllers.V2.Api
                     .Where(x => listaParVinculoPeso.Any(y => y.ParLevel2_Id == x.ParLevel2_Id))
                     .Where(x => listaParVinculoPeso.Any(y => y.ParLevel3_Id == x.ParLevel3_Id))
                     .ToList();
+
+                listaParLevel3Value = db.ParLevel3Value
+                    .Where(x => x.ParCompany_Id == companyId || x.ParCompany_Id == null)
+                    .Where(x => x.IsActive == true)
+                    .Select(x => new ParLevel3ValueAppViewModel()
+                    {
+                        Id = x.Id,
+                        DynamicValue = x.DynamicValue,
+                        IntervalMax = x.IntervalMax,
+                        IntervalMin = x.IntervalMin,
+                        AcceptableValueBetween = x.AcceptableValueBetween,
+                        ParLevel3BoolFalse_Id = x.ParLevel3BoolFalse_Id,
+                        ParLevel3BoolTrue_Id = x.ParLevel3BoolTrue_Id,
+                        ParLevel3InputType_Id = x.ParLevel3InputType_Id,
+                        ParMeasurementUnit_Id = x.ParMeasurementUnit_Id,
+                        ParCompany_Id = x.ParCompany_Id,
+                        ParLevel1_Id = x.ParLevel1_Id,
+                        ParLevel2_Id = x.ParLevel2_Id,
+                        ParLevel3_Id = x.ParLevel3_Id
+                    })
+                    .ToList()
+                    .Where(x => listaParVinculoPeso.Any(y => y.ParLevel1_Id == x.ParLevel1_Id))
+                    .Where(x => listaParVinculoPeso.Any(y => y.ParLevel2_Id == x.ParLevel2_Id))
+                    .Where(x => listaParVinculoPeso.Any(y => y.ParLevel3_Id == x.ParLevel3_Id))
+                    .ToList();
+
+                listaParLevel3InputType = db.ParLevel3InputType
+                    .Select(x => new ParLevel3InputTypeAppViewModel()
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Description = x.Description
+                    })
+                    .ToList();
+
+                listaParMeasurementUnit = db.ParMeasurementUnit
+                    .Select(x => new ParMeasurementUnitAppViewModel()
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Description = x.Description
+                    })
+                    .ToList();
+
+                listaParLevel3BoolTrue = db.ParLevel3BoolTrue
+                    .Select(x => new ParLevel3BoolTrueAppViewModel()
+                    {
+                        Id = x.Id,
+                        Name = x.Name
+                    })
+                    .ToList();
+
+                listaParLevel3BoolFalse = db.ParLevel3BoolFalse
+                    .Select(x => new ParLevel3BoolFalseAppViewModel()
+                    {
+                        Id = x.Id,
+                        Name = x.Name
+                    })
+                    .ToList();
+
             }
 
-            return Ok(new { listaParVinculoPeso,
+            return Ok(new
+            {
+                listaParVinculoPeso,
                 listaParLevel1,
                 listaParLevel2,
                 listaParLevel3,
                 listaParEvaluation,
-                listaParLevel3EvaluationSample });
+                listaParLevel3EvaluationSample,
+                listaParLevel3Value,
+                listaParLevel3InputType,
+                listaParMeasurementUnit,
+                listaParLevel3BoolTrue,
+                listaParLevel3BoolFalse
+            });
         }
     }
 }
