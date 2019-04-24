d236b8dfac9fee1a70714c19805a7b958b89a4c1
Renan Santini <renanssantini@hotmail.com>
2019-04-24 16:49:13 -0300

    [DEV-584] - APPRH - Exibir botão de informação da tarefa - quando clicar, abrir um modal com o conteudo do ParLevel3XHelp

--- SgqSystem/Scripts/appColeta/RH/app.js
+++ SgqSystem/Scripts/appColeta/RH/app.js
@@ -12,52 +12,51 @@ var currentCollectDate = new Date();
 var currentTotalEvaluationValue = 0;
 var currentTotalSampleValue = 0;
 
-function onOpenAppColeta(){
+function onOpenAppColeta() {
 	_readFile("login.txt", function (data) {
-		if(typeof(data) != 'undefined' && data.length > 0)
+		if (typeof (data) != 'undefined' && data.length > 0)
 			currentLogin = JSON.parse(data);
-		
+
 		_readFile("appParametrization.txt", function (param) {
-			if(typeof(param) != 'undefined' && param.length > 0)
+			if (typeof (param) != 'undefined' && param.length > 0)
 				parametrization = JSON.parse(param);
-				currentParFrequency_Id = parametrization.currentParFrequency_Id;
+			currentParFrequency_Id = parametrization.currentParFrequency_Id;
 		});
 	});
 }
 
 function getAppParametrization(frequencyId) {
-	
-	if(currentParFrequency_Id > 0 && frequencyId == currentParFrequency_Id)
-	{
-		if(confirm('Deseja refazer o download da parametrização desta frequencia?'))
+
+	if (currentParFrequency_Id > 0 && frequencyId == currentParFrequency_Id) {
+		if (confirm('Deseja refazer o download da parametrização desta frequencia?'))
 			currentParFrequency_Id = 0;
 	}
-	
+
 	sincronizarResultado(frequencyId);
-	
-	if(frequencyId != currentParFrequency_Id){
+
+	if (frequencyId != currentParFrequency_Id) {
 		currentParFrequency_Id = frequencyId;
-		openMensagem('Por favor, aguarde até que seja feito o download do planejamento selecionado','blue','white');
+		openMensagem('Por favor, aguarde até que seja feito o download do planejamento selecionado', 'blue', 'white');
 		$.ajax({
-		  data: {},
-		  url: urlPreffix + '/api/AppColeta/GetAppParametrization/' + curretParCompany_Id + '/' + currentParFrequency_Id,
-		  type: 'GET',
-		  success: function (data) {
-			  data.currentParFrequency_Id = currentParFrequency_Id;
-			 _writeFile("appParametrization.txt", JSON.stringify(data), function () {
-				parametrization = data;
-				listarParDepartment(0);
-			 });
-			closeMensagem();
-		  },
-		  timeout: 600000,
-		  error: function () {
-			$(this).html($(this).attr('data-initial-text'));
-			closeMensagem();
-		  }
+			data: {},
+			url: urlPreffix + '/api/AppColeta/GetAppParametrization/' + curretParCompany_Id + '/' + currentParFrequency_Id,
+			type: 'GET',
+			success: function (data) {
+				data.currentParFrequency_Id = currentParFrequency_Id;
+				_writeFile("appParametrization.txt", JSON.stringify(data), function () {
+					parametrization = data;
+					listarParDepartment(0);
+				});
+				closeMensagem();
+			},
+			timeout: 600000,
+			error: function () {
+				$(this).html($(this).attr('data-initial-text'));
+				closeMensagem();
+			}
 		});
-	}else{
-		openMensagem('Carregando parametrização','blue','white');
+	} else {
+		openMensagem('Carregando parametrização', 'blue', 'white');
 		_readFile("appParametrization.txt", function (data) {
 			parametrization = JSON.parse(data);
 			listarParDepartment(0);
@@ -66,37 +65,37 @@ function getAppParametrization(frequencyId) {
 	}
 }
 
-function sincronizarResultado(frequencyId){
-	openMensagem('Sincronizando resultado','blue','white');
+function sincronizarResultado(frequencyId) {
+	openMensagem('Sincronizando resultado', 'blue', 'white');
 	$.ajax({
 		data: JSON.stringify({
 			ParCompany_Id: currentLogin.ParCompanyXUserSgq[0].ParCompany.Id,
 			CollectionDate: new Date().toISOString()
 		}),
-	  url: urlPreffix + '/api/AppColeta/GetResults/',
-	  type: 'POST',
-	  contentType: "application/json",
-	  success: function (data) {
-		coletasAgrupadas = data;
-		AtualizarArquivoDeColetas();
-		closeMensagem();
-	  },
-	  timeout: 600000,
-	  error: function () {
-		closeMensagem();
-	  }
+		url: urlPreffix + '/api/AppColeta/GetResults/',
+		type: 'POST',
+		contentType: "application/json",
+		success: function (data) {
+			coletasAgrupadas = data;
+			AtualizarArquivoDeColetas();
+			closeMensagem();
+		},
+		timeout: 600000,
+		error: function () {
+			closeMensagem();
+		}
 	});
 }
 
-function sincronizarColeta(){
-	openMensagem('Iniciada sequencia forçada de sincronização','orange','white');
+function sincronizarColeta() {
+	openMensagem('Iniciada sequencia forçada de sincronização', 'orange', 'white');
 	enviarColeta();
 	closeMensagem(2000);
 }
 
 function showAllGlobalVar() {
-   console.log("ParCompany:" + curretParCompany_Id);
-   console.log("Frequencia: " + currentParFrequency_Id);
-   console.log("Departamento: " + currentParDepartment_Id);
-   console.log("Cargo: " + currentParCargo_Id);
+	console.log("ParCompany:" + curretParCompany_Id);
+	console.log("Frequencia: " + currentParFrequency_Id);
+	console.log("Departamento: " + currentParDepartment_Id);
+	console.log("Cargo: " + currentParCargo_Id);
 }
---
+++ SgqSystem/Scripts/appColeta/RH/utils.js
@@ -0,0 +1,34 @@
+function openModal(title, body, callback) {
+
+    var modal =
+        '<div class="modal modal-lg fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="...">' +
+        '<div class="modal-dialog" role="document">' +
+        '<div class="modal-content">' +
+        '<div class="modal-header">' +
+        '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+        '<h4 class="modal-title">' + title + '</h4>' +
+        '</div>' +
+        '<div class="modal-body">' +
+        '</div>' +
+        '<div class="modal-footer">' +
+        '<button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
+
+    $('body').append(modal);
+
+    $('#myModal .modal-body').html(body);
+
+    $('#myModal').on('hidden.bs.modal', function (e) {
+        $('#myModal').remove();
+    });
+
+    // $('#myModal').on('hidden.bs.modal', function (e) {
+    //     callback();
+    // });
+
+    $('#myModal').modal('show');
+
+}
