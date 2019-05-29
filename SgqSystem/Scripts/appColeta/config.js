3058bf02f81a46703d970ef21235ddc896a22751
Renan Santini <renanssantini@hotmail.com>
2019-05-29 10:20:46 -0300

    [APPCOLETA] - Exibe a plataforma que está sendo executado na tela de login do APP

--- SgqSystem/Scripts/appColeta/config.js
+++ SgqSystem/Scripts/appColeta/config.js
@@ -1,23 +1,5 @@
-//var urlPreffix = "http://192.168.25.32/SgqSystem";
-//var urlPreffix = "http://localhost/SgqSystem";
-//var urlPreffix = "https://sgq.jbssa.com/sgq"; //usa prod
-//var urlPreffix = "http://192.168.25.5/SgqSystem";
-//var urlPreffix = "https://sgqtest.jbssa.com/HMLUSA"; //usa hom
-//var urlPreffix = "http://10.160.99.89/sgqprodtolleson";
-//var urlPreffix = "http://192.168.25.200/Sgq_1_0_8";
-//var urlPreffix = "http://mtzsvmqschml01/SGQ";
-//var urlPreffix = "http://192.168.25.200/SgqUsa";
-//var urlPreffix = "http://192.168.25.200/SGQYtoara";
-//var urlPreffix = "http://mtzsvmqsc/Teste"; //Ambiente de Homologação Brasil
-//var urlPreffix = "http://mtzsvmqsc/Sgq_Service2"; //Ambiente de Homologação Brasil Android
-//var urlPreffix = "http://sgq.somee.com/SGQOnline";
-//var urlPreffix = "http://server20129141.cloudapp.net/SGQSantander"; //Santander
-//var urlPreffix = "http://ytoara.no-ip.info:8081/SGQYtoara"; //Ytoara
-
-var baseAmbiente = "AppColeta ";
-var versao = " 2.0.48"; //platform Android
-//var versao = " 2.0.46 Windows"; //platform Windows
-//var versao = " 2.0.47"; //platform Browser
+var baseAmbiente = "AppColeta";
+var versao = " 2.0.48 "+device.platform; //platform Android
 
 var isEUA = false; //true; 
 
