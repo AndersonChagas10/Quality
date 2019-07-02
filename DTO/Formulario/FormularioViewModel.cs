298d4ec3ae508a535d56c0d5f8bf200176f2275c
Renan Santini <renanssantini@hotmail.com>
2019-07-02 14:02:51 -0300

    [SGQ] - Novo Formulário query data (em Dev)

---
+++ DTO/Formulario/FormularioViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Formulario
+{
+    public class FormularioViewModel
+    {
+        public List<ParStructure> ParStructures { get; set; }
+        public List<ParCompany> ParCompanies { get; set; }
+        public List<ParLevel1> ParLevel1s { get; set; }
+        public List<ParLevel2> ParLevel2s { get; set; }
+        public List<ParLevel3> ParLevel3s { get; set; }
+    }
+}
