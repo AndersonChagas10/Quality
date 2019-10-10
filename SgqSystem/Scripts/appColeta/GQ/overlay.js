ccd7dad8e88256caf6e10c2ff3c1683811496bfd
GRT Solucoes <devgrtsolucoes@gmail.com>
2019-10-10 09:43:00 -0300

    [DELETE] pasta Script/GQ no SgqSystem

--- SgqSystem/Scripts/appColeta/GQ/overlay.js
+++
@@ -1,6 +0,0 @@
-$(document).on('click', '.overlay', function (e) {
-    if ($('.btnAreaSaveConfirm:visible').length) {
-        $(this).hide();
-        $('.btnAreaSaveConfirm:visible').addClass('hide').siblings('.btnAreaSave').removeAttr('disabled');
-    }
-});
