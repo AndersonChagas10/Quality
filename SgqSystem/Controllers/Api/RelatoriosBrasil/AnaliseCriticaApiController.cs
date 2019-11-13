a93aafcd65cffb6e612d9205f0ce1852023b2893
Renan Santini <renanssantini@hotmail.com>
2019-11-13 10:22:56 -0300

    [SGQ-157] - Analise Critica - Grafico de Historico por Unidade

---
+++ SgqSystem/Controllers/Api/RelatoriosBrasil/AnaliseCriticaApiController.cs
@@ -0,0 +1,72 @@
+﻿using SgqService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SgqSystem.Controllers.Api.RelatoriosBrasil
+{
+    [RoutePrefix("api/AnaliseCritica")]
+    public class AnaliseCriticaApiController : BaseApiController
+    {
+
+        [HttpPost]
+        [Route("GetGraficoHistoricoUnidade")]
+        public List<HistoricoUnidade> GetGraficoHistoricoUnidade([FromBody] FormularioParaRelatorioViewModel form)
+        {
+
+            var retornoHistoricoUnidade = new List<HistoricoUnidade>();
+
+            retornoHistoricoUnidade.Add(new HistoricoUnidade() { Av = 1, AvComPeso = 1, Date = DateTime.Now, Mes = "2019-06", Nc = 1, NcComPeso = 1, PorcentagemNc = 100, Semana = "2019/1" });
+            retornoHistoricoUnidade.Add(new HistoricoUnidade() { Av = 2, AvComPeso = 1, Date = DateTime.Now, Mes = "2019-06", Nc = 2, NcComPeso = 1, PorcentagemNc = 100, Semana = "2019/2" });
+            retornoHistoricoUnidade.Add(new HistoricoUnidade() { Av = 3, AvComPeso = 1, Date = DateTime.Now, Mes = "2019-07", Nc = 3, NcComPeso = 1, PorcentagemNc = 100, Semana = "2019/4" });
+            retornoHistoricoUnidade.Add(new HistoricoUnidade() { Av = 4, AvComPeso = 1, Date = DateTime.Now, Mes = "2019-07", Nc = 4, NcComPeso = 1, PorcentagemNc = 100, Semana = "2019/4" });
+            retornoHistoricoUnidade.Add(new HistoricoUnidade() { Av = 5, AvComPeso = 1, Date = DateTime.Now, Mes = "2019-07", Nc = 5, NcComPeso = 1, PorcentagemNc = 100, Semana = "2019/4" });
+
+            return retornoHistoricoUnidade;
+        }
+    }
+
+    public class HistoricoUnidade
+    {
+        public decimal? Nc { get; set; }
+        public decimal? PorcentagemNc { get; set; }
+        public decimal? Av { get; set; }
+        public decimal? AvComPeso { get; set; }
+        public decimal? NcComPeso { get; set; }
+        public string Semana { get; set; }
+        public string Mes { get; set; }
+        public DateTime? Date { get; set; }
+        public string _Date
+        {
+            get
+            {
+                if (Date.HasValue)
+                {
+                    return Date.Value.ToString("dd/MM/yyyy");
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
+        public string _DateEUA
+        {
+            get
+            {
+
+                if (Date.HasValue)
+                {
+                    return Date.Value.ToString("MM-dd-yyyy");
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
+    }
+}
