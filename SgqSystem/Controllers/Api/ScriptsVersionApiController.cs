fade5b28075ffbdab75a281bb4ff37fa14c479d5
Marcos Augusto <desenvolvimento@grtsolucoes.com.br>
2020-01-10 14:35:21 -0300

    [feature-xml] - cria tela e controller para ler e apresentar os scripts de banco de dados

---
+++ SgqSystem/Controllers/Api/ScriptsVersionApiController.cs
@@ -0,0 +1,59 @@
+﻿using Dominio;
+using SgqSystem.Handlres;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SgqSystem.Controllers.Api
+{
+    [RoutePrefix("api/ScriptsVersion")]
+    [HandleApi()]
+    public class ScriptsVersionApiController : ApiController
+    {
+        public SgqDbDevEntities db = new SgqDbDevEntities();
+
+        public class XmlObj {
+            public string CardNumber { get; set; }
+            public string Version { get; set; }
+            public string Description { get; set; }
+            public string Script { get; set; }
+        }
+
+        [HttpGet]
+        [Route("GetScriptsVersion")]
+        public IHttpActionResult GetScriptsVersion([FromUri] string cardNumber, string version)
+        {
+            //XmlDocument doc = new XmlDocument();
+            //doc.Load("C:\\Users\\GRT\\source\\repos\\sgq_pa\\DbScriptsVersion.xml");
+
+            List<XmlObj> objList = new List<XmlObj>();
+
+            var xmlString = File.ReadAllText("C:\\Users\\GRT\\source\\repos\\sgq_pa\\DbScriptsVersion.xml");
+            var stringReader = new StringReader(xmlString);
+            var dsSet = new DataSet();
+            dsSet.ReadXml(stringReader);
+
+            // Loading from a file, you can also load from a stream
+            var xml = XDocument.Load("C:\\Users\\GRT\\source\\repos\\sgq_pa\\DbScriptsVersion.xml");
+
+
+            // Query the data and write out a subset of contacts
+            var query = from c in xml.Root.Descendants("card")
+                        //where c.Element("number").Value == cardNumber
+                        //&& c.Element("version").Value == version
+                        select c.Element("number").Value + " " +
+                               c.Element("version").Value + " " +
+                               c.Element("description").Value + " " +
+                               c.Element("script").Value;
+
+            return Ok(dsSet);
+        }
+    }
+}
---
+++ SgqSystem/Controllers/ScriptsVersionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SgqSystem.Controllers
+{
+    public class ScriptsVersionController : Controller
+    {
+        // GET: ScriptsVersion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
