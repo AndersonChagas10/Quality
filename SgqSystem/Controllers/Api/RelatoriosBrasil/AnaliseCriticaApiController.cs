d1fd3f5946c551b7e929800fe924161ecca8b1b0
Ricardo <ricardo.ribeiro@grtsolucoes.com.br>
2020-02-06 15:55:24 -0300

    [SGQ - 366] Disponibilizar o link do plano de ação ao clicar no ver mais da analise critica, assim redirecionando para la e abrindo o acompanhamento automatico.

--- SgqSystem/Controllers/Api/RelatoriosBrasil/AnaliseCriticaApiController.cs
+++ SgqSystem/Controllers/Api/RelatoriosBrasil/AnaliseCriticaApiController.cs
@@ -2238,7 +2238,8 @@ ORDER BY S3.PorcentagemNc DESC";
             var sql = $@"
 
 SELECT
-   PL1.Id AS ParLevel1_Id
+    Acao.Id
+   ,PL1.Id AS ParLevel1_Id
    ,PL1.Name AS ParLevel1
    ,PL2.Id as ParLevel2_Id
    ,PL2.Name AS ParLevel2
