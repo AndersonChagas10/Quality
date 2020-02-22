72df7a7a7dec5cff331374178e956e683e8096e2
GRT Solucoes <devgrtsolucoes@gmail.com>
2020-02-22 19:11:13 -0300

    [SGQ-419] Adicionado regra para ao selecionar indicador que pertença a uma familia, seja aberto o indicador com a visualização IC, sem a necessidade de dicionario estático e listagem de Familia de Produto (pois está vinculada ao indicador)

--- AppColeta/SEARA/businessRoute.js
+++ AppColeta/SEARA/businessRoute.js
@@ -1,8 +1,23 @@
 function level1BusinessRoute(parlevel1_id){
-    if(parlevel1_id == globalDicionarioEstatico.ParLevel1DCA){
-        listarFamiliaProdutoDCA();
+    var parLevel1XParFamiliaProduto = existParLevel1XParFamiliaProduto(parlevel1_id);
+    if (parLevel1XParFamiliaProduto != null) {
+        //listarFamiliaProdutoDCA();
+        currentFamiliaProdutoDCA_Id = parLevel1XParFamiliaProduto.ParFamiliaProduto_Id;
+        listarParLevel2DCA();
     }else{
         listarParLevel2();
     }
     return;
+}
+
+function getParLevel1XParFamiliaProduto(parlevel1_id) {
+    var _parlevel1_id = parlevel1_id;
+    var exists = $.grep(parametrization.listaParLevel1XParFamiliaProduto, function (item) {
+        return item.ParLevel1_Id == _parlevel1_id;
+    })
+    if (exists.length > 0) {
+        return exists[0];
+    } else {
+        return null;
+    }
 }
