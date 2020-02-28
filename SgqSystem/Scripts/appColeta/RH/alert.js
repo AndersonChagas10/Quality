f9f7f30a8a51e9fef18d2707cc9de0eed7deddf5
Ricardo <ricardo.ribeiro@grtsolucoes.com.br>
2020-02-28 18:03:03 -0300

    [SGQ - 445] Alerta KO na coleta do RH, ao inves de mostrar o alerta e depois redirecionar , agora ele mostra tudo na mesma tela(modal).

--- SgqSystem/Scripts/appColeta/RH/alert.js
+++ SgqSystem/Scripts/appColeta/RH/alert.js
@@ -59,14 +59,85 @@ function setTimeoutOpenCorrectiveAction(e,c,n){
     var numeroDeAlertas = n;
 
     setTimeout(function () {
-        openMensagem('Alerta ' + numeroDeAlertas + ' (' + exists[0].Name + ') foi disparado.', 'red', 'white');
-        closeMensagem(3000);
+        
+        //colocar um botao no alerta que quando a acao corretiva nao for obrigatoria ele aparece,
+        //e quando clicar nele tira a propriedade none e mostra ela na tela, talvez com um botao de pular ou proximo
+        //caso ele nao queira responder o opcional. 
 
-        if (exists[0].HasCorrectiveAction) {
-            //Verificar se disparou alerta e se existe ação corretiva - Caso existir, abre o modal - após salvar a ação corretiva abre a função abaixo;
-            setTimeout(function () {
-                OpenCorrectiveAction(coleta);
-            }, 3100);
+        var alerta = 
+            '<div style="background-color:red; padding:10px;">' +
+            '<div>' +
+            '<p>Alerta ' + numeroDeAlertas + ' (' + exists[0].Name + ') foi disparado.</p>' +
+            '</div>' +
+            '<div style="text-align:center">' +
+            '<button class="btn btn-primary">Ok</button>' +
+            '</div>' +
+            '</div>';
+
+        if(exists[0].HasCorrectiveAction){
+            var modal = '<h3 style="font-weight:bold;">Ação Corretiva</h3>';
+
+            var body = '<div class="form-group">' +
+                '<div class="form-group col-xs-12">' +
+                '<strong>Informações</strong>' +
+                '<small><br/>Data/Hora: ' + currentCollectDate.toLocaleDateString() + ' ' + currentCollectDate.toLocaleTimeString() +
+                '<br/>Monitor: ' + currentLogin.Name +
+                '<br/>Tarefa: ' + $.grep(parametrization.listaParLevel3, function (o, i) { return o.Id == coleta.ParLevel3_Id; })[0].Name +
+                '<br/>Frequência: ' + $.grep(parametrization.listaParFrequency, function (item) { return item.Id == parametrization.currentParFrequency_Id; })[0].Name +
+                '</small></div>' +
+        
+                '<div class="form-group col-xs-12">' +
+                '<label>Descrição da Falha:</label>' +
+                '<input name="DescriptionFailure" id="descriptionFailure" class="col-sx-12 form-control" style="height: 80px;">' +
+                '</div>' +
+                '<div class="form-group col-xs-12">' +
+                '<label for="email">Ação Corretiva Imediata:</label>' +
+                '<input name="ImmediateCorrectiveAction" id="immediateCorrectiveAction" class="form-control" style="height: 80px;">' +
+                '</div>' +
+                '<div class="form-group col-xs-12">' +
+                '<label for="email">Ação Preventiva:</label>' +
+                '<input name="PreventativeMeasure" id="preventativeMeasure" class="form-control" style="height: 80px;">' +
+                '</div>';
+
+            var salvaAcaoCorretiva = '<button class="btn btn-primary" id="btnSendCA">Salvar Ação Corretiva</button>';
+
+        }else{
+            modal = "";
+            body = "";
+            salvaAcaoCorretiva = "";
         }
+
+        var corpo = 
+            '<div class="container">' +
+            '<div class="row" style="overflow:auto">' +
+            '<div>' +
+            alerta +
+            '</div>' +
+            '<hr>' +
+            modal +
+            '<hr>' +
+            '<div>' +
+            body +
+            '</div>' +
+            '<hr>' +
+            '<div class="form-group col-xs-6">' +
+            salvaAcaoCorretiva +
+            '</div>' +
+            '</div>' +
+            '</div>';
+
+        openModal(corpo, 'white', 'black');
+
+
+
+        // openMensagem('Alerta ' + numeroDeAlertas + ' (' + exists[0].Name + ') foi disparado.', 'red', 'white');
+        // //closeMensagem(3000);
+
+        // if (exists[0].HasCorrectiveAction) {
+        //     //Verificar se disparou alerta e se existe ação corretiva - Caso existir, abre o modal - após salvar a ação corretiva abre a função abaixo;
+        //     setTimeout(function () {
+        //         OpenCorrectiveAction(coleta);
+        //     }, 3100);
+        // }
     }, 3500);
 }
