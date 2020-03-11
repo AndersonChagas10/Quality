30a183ef378fc8278b41ab234fd775c057aa12f0
Renan Santini <renanssantini@hotmail.com>
2020-03-11 17:34:29 -0300

    [SGQ-480] - Validação de Gmud na tela de Changelog

--- SgqSystem/Controllers/ScriptsVersionController.cs
+++ SgqSystem/Controllers/ScriptsVersionController.cs
@@ -19,11 +19,13 @@ namespace SgqSystem.Controllers
         {
             List<string> versions;
             var listChangeLog = GetChangeLogByVersion(version, out versions);
-            ViewBag.Versions = versions.OrderBy(x=>x).ToList();
+            var listVersionName = versions.OrderBy(x => x).ToList();
+
+            ViewBag.Versions = geVersions(listVersionName);
             return View(listChangeLog);
         }
 
-        public List<ChangeLog> GetChangeLogByVersion(string version, out List<string> versions)
+        private List<ChangeLog> GetChangeLogByVersion(string version, out List<string> versions)
         {
             versions = new List<string>();
             List<ChangeLog> changeLogs = new List<ChangeLog>();
@@ -41,7 +43,7 @@ namespace SgqSystem.Controllers
                     });
                 }
 
-                if(!versions.Any(x=>x == card.Element("Version").Value))
+                if (!versions.Any(x => x == card.Element("Version").Value))
                 {
                     versions.Add(card.Element("Version").Value);
                 }
@@ -50,12 +52,81 @@ namespace SgqSystem.Controllers
 
             return changeLogs;
         }
+
+        private List<Version> geVersions(List<string> versions)
+        {
+
+            //List<ChangeLog> changeLogs = new List<ChangeLog>();
+            List<Version> Versoes = new List<Version>();
+            var path = Path.Combine(@AppDomain.CurrentDomain.BaseDirectory, "ChangeLog", "ChangeLog.xml");
+
+            var listMigrationHistory = db.MigrationHistory.ToList();
+
+            foreach (var versao in versions)
+            {
+                var hasInMigrationHistory = true;
+
+                foreach (XElement card in XElement.Load(path).Elements("Card"))
+                {                   
+                    if (card.Element("Version").Value.Contains(versao) && 
+                        card.Element("Script").Value != null && 
+                        card.Element("Script").Value != "")
+                    {
+                        hasInMigrationHistory = hasAnyNoInsert(card.Element("CardNumber").Value, listMigrationHistory);
+
+                        if (!hasInMigrationHistory)
+                            break;
+                    }
+                }
+
+                Versoes.Add(new Version
+                {
+                    Name = versao,
+                    HasInMigrationHistory = hasInMigrationHistory
+                });
+            }
+
+            return Versoes;
+        }
+
+        //Se algum card não estiver no banco, retornar na lista de versões HasInMigrationHistory = false
+        private bool hasAnyNoInsert(string CardNumber, List<MigrationHistory> listMigrationHistory)
+        {
+            var hasAnyNoInsert = false;
+
+            if (listMigrationHistory.Any(x => x.Name == CardNumber))
+            {
+                hasAnyNoInsert = true;
+            }
+
+            return hasAnyNoInsert;
+        }
+
     }
+
     public class ChangeLog
     {
         public string CardNumber { get; set; }
         public string Version { get; set; }
         public string Description { get; set; }
         public string Script { get; set; }
+    }
+
+    public class Version
+    {
+        public string Name { get; set; }
+
+        public bool HasInMigrationHistory { get; set; }
+
+        public string Fa_Icon
+        {
+            get
+            {
+                if (this.HasInMigrationHistory)
+                    return "fa fa-check-square-o";
+                else
+                    return "fa fa-square-o";
+            }
+        }
     }
 }
