257073c2d727991b85e54f85b8dd1e7dba5cff01
Carla <estagiarios@grtsolucoes.com.br>
2020-05-20 12:19:56 -0300

    [SGQ-713] AppColeta - Criação de um campo de busca para os Indicadores.

--- AppColeta/SEARA/parLevel1.js
+++ AppColeta/SEARA/parLevel1.js
@@ -27,11 +27,21 @@ function listarParLevel1(isVoltar) {
 		'		<div class="col-xs-12">                                    ' +
 		'                                                                  ' +
 		'			<div class="panel panel-primary">                      ' +
-		'			  <div class="panel-heading">                          ' +
-		'				<h3 class="panel-title">' + voltar + ' Selecione o Indicador</h3>            ' +
+        '			  <div class="panel-heading">                          ' +
+        '			    <div class="row">                          ' +
+        '			      <div class="col-xs-9">                         ' +
+        '				    <h3 class="panel-title">' + voltar + ' Selecione o Indicador</h3>' +
+        '                 </div >                                          ' +
+        '                 <div class="col-xs-3">                           ' +
+        '                     <div class="pull-right">                     ' +
+        '                        <label style="padding-right:5px">Buscar</label> ' +
+        '                        <input id="buscarIndicador" type="text" />' +
+        '                     </div>                                       ' +
+        '                 </div>                                           ' +
+        '               </div>                                             ' +
 		'			  </div>                                               ' +
 		'			  <div class="panel-body">                             ' +
-		'				<div class="list-group">                           ' +
+		'				<div class="list-group" id="listaIndicadores">                           ' +
 		htmlLista +
 		'				</div>                                             ' +
 		'			  </div>                                               ' +
@@ -49,6 +59,26 @@ function listarParLevel1(isVoltar) {
         $("[data-par-department-id]").trigger('click');
     }
 }
+
+$('body').off('keyup', "#buscarIndicador").on('keyup', "#buscarIndicador", function () {
+
+    var listaParLevel1 = RetornarParLevel1(currentParCluster_Id);
+    var listaIndicadores = "";
+
+    $(listaParLevel1).each(function (i, o) {
+        var mostrarIndicador = o.Name.toLowerCase().includes($("#buscarIndicador").val().toLowerCase());
+        if (mostrarIndicador) {
+            listaIndicadores += '<button type="button" class="list-group-item col-xs-12" ' +
+                '" data-par-level1-id="' + o.Id + '">' + o.Name +
+                '<span class="badge">></span>' +
+                '</button>';
+        }
+    });
+    if (listaIndicadores == "") {
+        listaIndicadores = "<span class='list-group-item col-xs-12 text-center'>Nenhum resultado encontrado com o termo digitado.</span>";
+    }
+    $("#listaIndicadores").html(listaIndicadores);
+});
 
 $('body').off('click', '[data-par-level1-id]').on('click', '[data-par-level1-id]', function (e) {
 
