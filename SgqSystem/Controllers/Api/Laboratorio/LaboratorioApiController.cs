1ca1f5fbc62ead6e28383066242e7c584ce99abf
Renan Santini <renanssantini@hotmail.com>
2020-05-25 21:37:29 -0300

    [SGQ-739] - Relatório Laboratório - Query no Dicionário estático

--- SgqSystem/Controllers/Api/Laboratorio/LaboratorioApiController.cs
+++ SgqSystem/Controllers/Api/Laboratorio/LaboratorioApiController.cs
@@ -1,5 +1,6 @@
 ﻿using ADOFactory;
 using DTO.Formulario;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,144 +16,19 @@ namespace SgqSystem.Controllers.Api
 
         [HttpPost]
         [Route("Get")]
-        public List<RelatorioLaboratorioReultSet> Get([FromBody] DataCarrierFormularioLaboratorio form)
+        public List<JObject> Get([FromBody] DataCarrierFormularioLaboratorio form)
         {
 
             string whereDate = $"AND cast(dColetaAmostra as date) BETWEEN '{ form.startDate }' AND '{ form.endDate }' ";
 
-            string sqlQuery = $@"
+            string sqlQuery = GetDicionarioEstatico("queryRelatorioLaboratorio");
 
-SET LANGUAGE 'Portuguese';
-
-SELECT --TOP 100
-	UPPER(S3.Name) AS cNmHolding,
-	UPPER(S2.Name) AS cNmRegional,
-	UPPER(S1.Name) AS cNmSubRegional,
-	E.nCdEmpresa,
-	E.cNmEmpresa,
-	E.cSgEmpresa,
-	CASE
-		WHEN cNmTpColeta = 'Água' 
-			THEN 'ÁGUA'
-		WHEN cNmTpColeta IN ('Swab Pré-operacional','Swab Operacional','Exposição de placa'/*ambiente*/) 
-			THEN 'AMBIENTE'
-		WHEN cNmTpColeta IN ('In natura','Subprodutos','Alimentos Preparados','Não Destrutivo'/*cc*/,'Destrutiva'/*cc*/) --cc,Cortes, MP,charque
-			THEN 'PRODUTO'
-			--Carcaca
-			----dColetaAmostra vs dMovimento para Fria ou Quente em CC
-
-	END AS cNmVeiculos,
-	CUBO.dColetaAmostra,
-	YEAR(dColetaAmostra) AS 'dColetaAmostra_ANO',
-	MONTH(dColetaAmostra) AS 'dColetaAmostra_MES',
-	CONCAT(RIGHT(CONCAT('0', MONTH(dColetaAmostra)), 2), '-', DATENAME(MONTH, dColetaAmostra)) AS 'dColetaAmostra_MES_NOME',
-	CONCAT(RIGHT(CONCAT('0', MONTH(dColetaAmostra)), 2), '-', LEFT(DATENAME(MONTH, dColetaAmostra), 3)) AS 'dColetaAmostra_MES_SIGLA',
-	DAY(dColetaAmostra) AS 'dColetaAmostra_DIA',
-	DATEPART(WEEKDAY, dColetaAmostra) AS 'dColetaAmostra_DIA_SEMANA_NUM',
-	CONCAT(RIGHT(CONCAT('0', DATEPART(WEEKDAY, dColetaAmostra)), 2), '-', DATENAME(WEEKDAY, dColetaAmostra)) AS 'dColetaAmostra_DIA_SEMANA',
-	CUBO.dProducao,
-	CUBO.nCdProduto,
-	CASE
-		WHEN CUBO.cNmTpColeta IN('Não Destrutivo'/*cc*/,'Destrutiva'/*cc*/)
-		THEN 'CARCARÇA'
-	ELSE P.cNmProduto
-	END cNmProduto,
-	CASE
-		WHEN CUBO.cNmTpColeta IN('Não Destrutivo'/*cc*/,'Destrutiva'/*cc*/)
-		THEN 'CARCARÇA'
-	ELSE P.cNmTipoProdutoAL
-	END cNmTipoProdutoAL,
-	CASE
-		WHEN CUBO.cNmTpColeta IN('Não Destrutivo'/*cc*/,'Destrutiva'/*cc*/)
-		THEN CASE
-				WHEN CAST(dColetaAmostra as date) = CAST(dMovimento AS DATE)
-				THEN 'FRIA'
-			 ELSE 'QUENTE'
-		END
-	ELSE P.cNmFamiliaSubGrupo
-	END cNmFamiliaSubGrupo,
-	CUBO.nCdAnalise,
-	CUBO.cNmAnalise,
-	CUBO.cGrupoColeta,
-	CASE
-		WHEN cGrupoColeta = 2 THEN 'Requisição de carcaça'
-		WHEN cGrupoColeta = 3 THEN 'Requisição de produto'
-		WHEN cGrupoColeta = 4 THEN 'Requisição ambiente'
-	END AS cNmGrupoColeta,
-	CUBO.nCdSetor,
-	CUBO.cNmSetor,
-	CUBO.nCdTpColeta,
-	CASE
-		WHEN CUBO.cNmTpColeta IN('Não Destrutivo'/*cc*/,'Destrutiva'/*cc*/)
-		THEN 'In natura'
-	ELSE CUBO.cNmTpColeta
-	END cNmTpColeta,
-	CUBO.nResultadoAnalise,
-	CUBO.cSgUnidadeMedidaLaboratorio,
-	CUBO.dMovimento,
-	CUBO.iLote,
-	CUBO.nCdPontoColeta,
-	CUBO.cNmPontoColeta,
-	CUBO.cNmDetalhePontoColeta,
-	CUBO.nCdRequisicaoAnalise,
-	CUBO.nCdRequisicaoFilial,
-	CUBO.nCdLaboratorio,
-	CUBO.cNmLaboratorio,
-	CUBO.iAmostra,
-	CUBO.cCdBarraAmostra,
-	CUBO.nCdMatriz,
-	CUBO.cNmMatriz,
-	CUBO.nCdGrupoAnalise,
-	CUBO.cNmGrupoAnalise,
-	CUBO.nCdMetodologia,
-	CUBO.cCdMetodologia,
-	CUBO.cNmMetodologia,
-	CUBO.dAnaliseCritica,
-	CUBO.dAnaliseInicial,
-	CUBO.dAnaliseFinal,
-	CUBO.dPrevisaoFinalAnalise,
-	CUBO.nCdValorPadrao,
-	CUBO.cSinal1,
-	CUBO.nValor1,
-	CUBO.cOperadorPadrao,
-	CUBO.cSinal2,
-	CUBO.nValor2,
-	CUBO.cRastreabilidadeLaudo,
-	CUBO.Key_Integ,
-	CUBO.cResultadoAnalise,
-	CASE
-		WHEN cResultadoAnalise = '3' THEN 'NC'
-		ELSE 'C'
-	END Conformidade,
-	1 AS AV,
-	CASE
-		WHEN cResultadoAnalise = '3' THEN 1
-		ELSE 0
-	END NC
-	FROM INTEG.CollectionAnaliseLaboratorial CUBO WITH(NOLOCK)
-	LEFT JOIN Empresa E WITH(NOLOCK) ON CUBO.nCdEmpresa = E.nCdEmpresa
-	LEFT JOIN ParCompany C WITH(NOLOCK) ON E.nCdEmpresa = C.CompanyNumber
-	LEFT JOIN ParCompanyXStructure CS WITH(NOLOCK) ON C.ID = CS.ParCompany_Id
-	LEFT JOIN ParStructure S1 WITH(NOLOCK) ON CS.ParStructure_Id = S1.Id
-	LEFT JOIN ParStructure S2 WITH(NOLOCK) ON S1.ParStructureParent_Id = S2.Id
-	LEFT JOIN ParStructure S3 WITH(NOLOCK) ON S2.ParStructureParent_Id = S3.Id
-	LEFT JOIN dbPlanejamentoGestao.dbo.DMProduto P WITH(NOLOCK) ON CUBO.nCdProduto = P.nCdProduto
-
-	WHERE 1 = 1
-	--AND cNmTpColeta NOT IN('Não Destrutivo'/*cc*/,'Destrutiva'/*cc*/,'Exposição de placa'/*ambiente*/)
-	--AND cNmTpColeta = 'Exposição de placa'
-	AND C.IsActive = 1
-	AND CS.Active = 1
-	AND S1.Active = 1
-	AND S2.Active = 1
-	AND S3.Active = 1
-	{ whereDate }";
-
-            var retorno = new List<RelatorioLaboratorioReultSet>();
+            var retorno = new List<JObject>();
 
             using (var db = new Factory("DefaultConnection"))
             {
-                retorno = db.SearchQuery<RelatorioLaboratorioReultSet>(sqlQuery).ToList();
+
+                retorno = db.QueryNinjaADO(sqlQuery + " " + whereDate);
             }
 
             return retorno;
