7a1982d8ab02668db710e9dba8f07b803ed42a95
Lucas Ricci <lucasrafael.ricci@gmail.com>
2021-05-20 16:32:46 -0300

    [GRX-1857] - Adiciona o Edit na API, que retorna os dados referentes ao id solicitado.

--- SgqSystem/Controllers/PlanoDeAcao/Api/AcaoApiController.cs
+++ SgqSystem/Controllers/PlanoDeAcao/Api/AcaoApiController.cs
@@ -1,5 +1,4 @@
-﻿using Dominio;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -43,7 +42,8 @@ SELECT
  PAC.EvidenciaAcaoConcluida,
  PAC.Prioridade,
  PAC.Status,
- PAC.IsActive
+ PAC.IsActive,
+ US.Name AS Responsavel_Name
  FROM Pa.Acao PAC  WITH (NOLOCK)
  LEFT JOIN ParLevel1 PL1  WITH (NOLOCK)
  ON PL1.Id = PAC.ParLevel1_Id
@@ -56,7 +56,10 @@ SELECT
  LEFT JOIN ParDepartment PD  WITH (NOLOCK)
  ON PD.Id = PAC.ParDepartment_Id
  LEFT JOIN ParCargo PCG  WITH (NOLOCK)
- ON PCG.Id = PAC.ParCargo_Id";
+ ON PCG.Id = PAC.ParCargo_Id
+ LEFT JOIN UserSgq US WITH (NOLOCK)
+ ON US.Id = PAC.Responsavel"
+ ;
 
             using (ADOFactory.Factory factory = new ADOFactory.Factory("DefaultConnection"))
             {
@@ -93,6 +96,66 @@ SELECT
             return Ok();
         }*/
 
+        [Route("Edit/{id}")]
+        [HttpGet]
+        public AcaoViewModel Edit(int id)
+        {
+            var query = $@"
+SELECT
+ PAC.Id,
+ PL1.Id AS ParLevel1_Id,
+ PL1.Name AS ParLevel1_Name,
+ PL2.Id AS ParLevel2_Id,
+ PL2.Name AS ParLevel2_Name,
+ PL3.Id AS ParLevel3_Id,
+ PL3.Name AS ParLevel3_Name,
+ PC.Id AS ParCompany_Id,
+ PC.Name AS ParCompany_Name,
+ PD.Id AS ParDepartment_Id,
+ PD.Name AS ParDepartment_Name,
+ PD.Parent_Id AS ParDepartmentParent_Id,
+ PCG.Id AS ParCargo_Id,
+ PCG.Name AS ParCargo_Name,
+ PAC.Acao_Naoconformidade,
+ PAC.AcaoText,
+ PAC.DataEmissao,
+ PAC.DataConclusao,
+ PAC.HoraEmissao,
+ PAC.HoraConclusao,
+ PAC.Referencia,
+ PAC.Responsavel,
+ PAC.Notificar,
+ PAC.EvidenciaNaoConformidade,
+ PAC.EvidenciaAcaoConcluida,
+ PAC.Prioridade,
+ PAC.Status,
+ PAC.IsActive,
+ US.Name AS Responsavel_Name
+ FROM Pa.Acao PAC  WITH (NOLOCK)
+ LEFT JOIN ParLevel1 PL1  WITH (NOLOCK)
+ ON PL1.Id = PAC.ParLevel1_Id
+ LEFT JOIN ParLevel2 PL2  WITH (NOLOCK)
+ ON PL2.Id = PAC.ParLevel2_Id
+ LEFT JOIN ParLevel3 PL3  WITH (NOLOCK)
+ ON PL3.Id = PAC.ParLevel3_Id
+ LEFT JOIN ParCompany PC  WITH (NOLOCK)
+ ON PC.Id = PAC.ParCompany_Id
+ LEFT JOIN ParDepartment PD  WITH (NOLOCK)
+ ON PD.Id = PAC.ParDepartment_Id
+ LEFT JOIN ParCargo PCG  WITH (NOLOCK)
+ ON PCG.Id = PAC.ParCargo_Id
+ LEFT JOIN UserSgq US WITH (NOLOCK)
+ ON US.Id = PAC.Responsavel
+WHERE PAC.Id = {id}
+";
+
+            using (ADOFactory.Factory factory = new ADOFactory.Factory("DefaultConnection"))
+            {
+                var lista = factory.SearchQuery<AcaoViewModel>(query).FirstOrDefault();
+                return lista;
+            }
+        }
+
         public class AcaoViewModel
         {
             public int Id { get; set; }
@@ -123,6 +186,7 @@ SELECT
             public string Prioridade { get; set; }
             public string Status { get; set; }
             public bool IsActive { get; set; }
+            public string Responsavel_Name { get; set; }
         }
     }
 }
