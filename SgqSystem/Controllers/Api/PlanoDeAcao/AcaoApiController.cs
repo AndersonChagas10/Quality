38297e9931429bd184de8ed97b1046d6e6011293
Alyne Gois <alyne0x67@gmail.com>
2021-06-17 11:47:21 -0300

    [GRX-2063] Ajusta Layout da tela e retorna na tela campos da Ação que estavam faltando.

--- SgqSystem/Controllers/Api/PlanoDeAcao/AcaoApiController.cs
+++ SgqSystem/Controllers/Api/PlanoDeAcao/AcaoApiController.cs
@@ -23,53 +23,53 @@ namespace SgqSystem.Controllers.Api.PlanoDeAcao
         [HttpGet]
         public IEnumerable<AcaoViewModel> Get()
         {
-         var query = $@"
-         SELECT
-         PAC.Id,
-         PL1.Id AS ParLevel1_Id,
-         PL1.Name AS ParLevel1_Name,
-         PL2.Id AS ParLevel2_Id,
-         PL2.Name AS ParLevel2_Name,
-         PL3.Id AS ParLevel3_Id,
-         PL3.Name AS ParLevel3_Name,
-         PC.Id AS ParCompany_Id,
-         PC.Name AS ParCompany_Name,
-         PD.Id AS ParDepartment_Id,
-         PD.Name AS ParDepartment_Name,
-         PD.Parent_Id AS ParDepartmentParent_Id,
-         PDS.Name AS ParDepartmentParent_Name,
-         PCG.Id AS ParCargo_Id,
-         PCG.Name AS ParCargo_Name,
-         PAC.Acao_Naoconformidade,
-         PAC.AcaoText,
-         FORMAT(PAC.DataEmissao, 'dd/MM/yyyy') as DataEmissao,
-         PAC.DataConclusao,
-         PAC.HoraEmissao,
-         PAC.HoraConclusao,
-         PAC.Referencia,
-         PAC.Responsavel,
-         PAC.Prioridade,
-         PAC.Status,
-         PAC.IsActive,
-         US.Name AS Responsavel_Name
-         FROM Pa.Acao PAC  WITH (NOLOCK)
-         LEFT JOIN ParLevel1 PL1  WITH (NOLOCK)
-         ON PL1.Id = PAC.ParLevel1_Id
-         LEFT JOIN ParLevel2 PL2  WITH (NOLOCK)
-         ON PL2.Id = PAC.ParLevel2_Id
-         LEFT JOIN ParLevel3 PL3  WITH (NOLOCK)
-         ON PL3.Id = PAC.ParLevel3_Id
-         LEFT JOIN ParCompany PC  WITH (NOLOCK)
-         ON PC.Id = PAC.ParCompany_Id
-         LEFT JOIN ParDepartment PD  WITH (NOLOCK)
-         ON PD.Id = PAC.ParDepartment_Id
-        LEFT JOIN ParDepartment PDS  WITH (NOLOCK)
-         ON PDs.Id = PAC.ParDepartmentParent_Id
-         LEFT JOIN ParCargo PCG  WITH (NOLOCK)
-         ON PCG.Id = PAC.ParCargo_Id
-         LEFT JOIN UserSgq US WITH (NOLOCK)
-         ON US.Id = PAC.Responsavel"
-        ;
+             var query = $@"
+                        SELECT
+                            PAC.Id,
+                            PL1.Id AS ParLevel1_Id,
+                            PL1.Name AS ParLevel1_Name,
+                            PL2.Id AS ParLevel2_Id,
+                            PL2.Name AS ParLevel2_Name,
+                            PL3.Id AS ParLevel3_Id,
+                            PL3.Name AS ParLevel3_Name,
+                            PC.Id AS ParCompany_Id,
+                            PC.Name AS ParCompany_Name,
+                            PD.Id AS ParDepartment_Id,
+                            PD.Name AS ParDepartment_Name,
+                            PD.Parent_Id AS ParDepartmentParent_Id,
+                            PDS.Name AS ParDepartmentParent_Name,
+                            PCG.Id AS ParCargo_Id,
+                            PCG.Name AS ParCargo_Name,
+                            PAC.Acao_Naoconformidade,
+                            PAC.AcaoText,
+                            FORMAT(PAC.DataEmissao, 'dd/MM/yyyy') as DataEmissao,
+                            PAC.DataConclusao,
+                            PAC.HoraEmissao,
+                            PAC.HoraConclusao,
+                            PAC.Referencia,
+                            PAC.Responsavel,
+                            PAC.Emissor,
+                            PAC.Prioridade,
+                            PAC.Status,
+                            PAC.IsActive,
+                            US.FullName AS Responsavel_Name
+                            FROM Pa.Acao PAC  WITH (NOLOCK)
+                            LEFT JOIN ParLevel1 PL1  WITH (NOLOCK)
+                            ON PL1.Id = PAC.ParLevel1_Id
+                            LEFT JOIN ParLevel2 PL2  WITH (NOLOCK)
+                            ON PL2.Id = PAC.ParLevel2_Id
+                            LEFT JOIN ParLevel3 PL3  WITH (NOLOCK)
+                            ON PL3.Id = PAC.ParLevel3_Id
+                            LEFT JOIN ParCompany PC  WITH (NOLOCK)
+                            ON PC.Id = PAC.ParCompany_Id
+                            LEFT JOIN ParDepartment PD  WITH (NOLOCK)
+                            ON PD.Id = PAC.ParDepartment_Id
+                            LEFT JOIN ParDepartment PDS  WITH (NOLOCK)
+                            ON PDs.Id = PAC.ParDepartmentParent_Id
+                            LEFT JOIN ParCargo PCG  WITH (NOLOCK)
+                            ON PCG.Id = PAC.ParCargo_Id
+                            LEFT JOIN UserSgq US WITH (NOLOCK)
+                            ON US.Id = PAC.Responsavel";
 
             using (ADOFactory.Factory factory = new ADOFactory.Factory("DefaultConnection"))
             {
@@ -107,10 +107,11 @@ namespace SgqSystem.Controllers.Api.PlanoDeAcao
                      PAC.HoraConclusao,
                      PAC.Referencia,
                      PAC.Responsavel,
+                     PAC.Emissor,
                      PAC.Prioridade,
                      PAC.Status,
                      PAC.IsActive,
-                     US.Name AS Responsavel_Name
+                     US.FullName AS Responsavel_Name
                      FROM Pa.Acao PAC  WITH (NOLOCK)
                      LEFT JOIN ParLevel1 PL1  WITH (NOLOCK)
                      ON PL1.Id = PAC.ParLevel1_Id
@@ -122,7 +123,7 @@ namespace SgqSystem.Controllers.Api.PlanoDeAcao
                      ON PC.Id = PAC.ParCompany_Id
                      LEFT JOIN ParDepartment PD  WITH (NOLOCK)
                      ON PD.Id = PAC.ParDepartment_Id
-                    LEFT JOIN ParDepartment PDS  WITH (NOLOCK)
+                     LEFT JOIN ParDepartment PDS  WITH (NOLOCK)
                      ON PDs.Id = PAC.ParDepartmentParent_Id
                      LEFT JOIN ParCargo PCG  WITH (NOLOCK)
                      ON PCG.Id = PAC.ParCargo_Id
@@ -543,7 +544,7 @@ namespace SgqSystem.Controllers.Api.PlanoDeAcao
                  PAC.Prioridade,
                  PAC.Status,
                  PAC.IsActive,
-                 US.Name AS Responsavel_Name
+                 US.FullName AS Responsavel_Name
                  FROM Pa.Acao PAC  WITH (NOLOCK)
                  LEFT JOIN ParLevel1 PL1  WITH (NOLOCK)
                  ON PL1.Id = PAC.ParLevel1_Id
@@ -559,8 +560,7 @@ namespace SgqSystem.Controllers.Api.PlanoDeAcao
                  ON PCG.Id = PAC.ParCargo_Id
                  LEFT JOIN UserSgq US WITH (NOLOCK)
                  ON US.Id = PAC.Responsavel
-                WHERE PAC.Id = {id}
-                ";
+                 WHERE PAC.Id = {id}";
 
             using (ADOFactory.Factory factory = new ADOFactory.Factory("DefaultConnection"))
             {
