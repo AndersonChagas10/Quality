ef0d60834f0929b4f0f4499ba0393adc8f68c2cd
GRT Solucoes <devgrtsolucoes@gmail.com>
2021-07-06 15:54:44 -0300

    [FIX] merge version/rhsesmt/v1.10.6

--- SgqSystem/Controllers/V2/Api/ParFrequencyController.cs
+++ SgqSystem/Controllers/V2/Api/ParFrequencyController.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using Dominio;
+using ServiceModel;
 using SgqSystem.Controllers.Api;
 using SgqSystem.ViewModels;
 
