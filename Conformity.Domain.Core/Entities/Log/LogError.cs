fa21294f3d85e2e9bbf5af0117125669b2bb6418
GRT Solucoes <devgrtsolucoes@gmail.com>
2021-07-07 15:51:00 -0300

    [RHSESMT-107] Adicionado arquivos que tinham sido ignorados pelo gitignore

---
+++ Conformity.Domain.Core/Entities/Log/LogError.cs
@@ -0,0 +1,35 @@
+﻿using Conformity.Domain.Core.Enums.Log;
+using Conformity.Domain.Core.Interfaces;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Conformity.Domain.Core.Entities.Log
+{
+    [Table("LogError")]
+    public class LogError
+    {
+        public int Id { get; set; }
+        public string ErrorMessage { get; set; }
+        public int Line { get; set; }
+        public string Method { get; set; }
+        public string Controller { get; set; }
+
+        private string _object;
+
+        public string Object
+        {
+            get { return _object?.Substring(0, _object.Length > 900 ? 900 : _object.Length); }
+            set { _object = value; }
+        }
+
+        private string _stackTrace;
+
+        public string StackTrace
+        {
+            get { return _stackTrace.Substring(0, _stackTrace.Length > 900 ? 900 : _stackTrace.Length); }
+            set { _stackTrace = value; }
+        }
+
+        public DateTime AddDate { get; set; }
+    }
+}
---
+++ Conformity.Infra.Data.Core/Repository/Log/LogErrorRepository.cs
@@ -0,0 +1,53 @@
+﻿using Conformity.Domain.Core.Entities.Log;
+using Conformity.Infra.CrossCutting;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Conformity.Infra.Data.Core.Repository.Log
+{
+    public class LogErrorRepository
+    {
+        private readonly ADOContext _aDOContext;
+
+        public LogErrorRepository(ADOContext aDOContext)
+        {
+            _aDOContext = aDOContext;
+        }
+
+        public void Add(LogError error)
+        {
+            string sql = $@"
+INSERT INTO [dbo].[LogError]
+           ([ErrorMessage]
+           ,[Line]
+           ,[Method]
+           ,[Controller]
+           ,[Object]
+           ,[StackTrace]
+           ,[AddDate])
+     VALUES
+           (@ErrorMessage
+           ,@Line
+           ,@Method
+           ,@Controller
+           ,@Object
+           ,@StackTrace
+           ,@AddDate);
+            SELECT @@IDENTITY AS 'Identity';";
+
+            using (SqlCommand cmd = new SqlCommand(sql, _aDOContext.connection))
+            {
+                cmd.CommandType = CommandType.Text;
+                cmd.AddParameterNullable("@ErrorMessage", error.ErrorMessage);
+                cmd.AddParameterNullable("@Line", error.Line);
+                cmd.AddParameterNullable("@Method", error.Method);
+                cmd.AddParameterNullable("@Controller", error.Controller);
+                cmd.AddParameterNullable("@Object", error.Object);
+                cmd.AddParameterNullable("@StackTrace", error.StackTrace);
+                cmd.AddParameterNullable("@AddDate", error.AddDate);
+                int id = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+        }
+    }
+}
