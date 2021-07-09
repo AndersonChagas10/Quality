8f187f2ce05e257904302b93f0914b627b8cd873
GRT Solucoes <devgrtsolucoes@gmail.com>
2021-07-09 10:48:32 -0300

    [RHSESMT-112] Adicionado repository LOG

---
+++ Conformity.Infra.Data.Core/Repository/Log/LogRepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using Conformity.Domain.Core.Entities;
+using Conformity.Domain.Core.Interfaces;
+
+namespace Conformity.Infra.Data.Core
+{
+    /// <summary>
+    /// Repositório Base, classe de gerencia do Banco de Dados.
+    /// </summary>
+    /// <typeparam name="T">Object reconhecido pelo DataBase: EntityBase</typeparam>
+    public class LogRepositoryBase<T> : RepositoryBase<T>, ILogRepository<T>  where T : BaseModel
+    {
+        public LogRepositoryBase(ParametrizacaoEntityContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
