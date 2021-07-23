15854769adaa4bb0ff7998842956e36fd948142b
Alyne Gois <alyne0x67@gmail.com>
2021-07-23 12:22:52 -0300

    [RHSESMT-96] Cria EntityTrackApi

---
+++ SgqSystem/Controllers/Api/Log/EntityTrackApiController.cs
@@ -0,0 +1,28 @@
+﻿using Conformity.Application.Core.Log;
+using Conformity.Domain.Core.DTOs;
+using Conformity.Infra.CrossCutting;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SgqSystem.Controllers.Api.Log
+{
+    [RoutePrefix("api/EntityTrackApi")]
+
+    public class EntityTrackApiController : BaseAuthenticatedApiController
+    {
+        private readonly EntityTrackService _entityTrackService;
+
+        public EntityTrackApiController(EntityTrackService entityTrackService,
+            ApplicationConfig applicationConfig) : base(applicationConfig)
+        {
+            _entityTrackService = entityTrackService;
+        }
+
+        [Route("ObterLogs/{acaoId}")]
+        [HttpGet]
+        public IEnumerable<EntityTrackViewModel> ObterLogs(int acaoId)
+        {
+            return _entityTrackService.GetAll("Acao", acaoId);
+        }
+    }
+}
